225573e7fb1e2d9b32b3e6594a397b7a754e1e61
Antonio Bulgheroni <bulgheroni>
2007-02-21 14:08:13 +0000

    Added a processor to be used with detectors having a flat pedestal and noise distribution. In this way there is no need to make a real pedestal calculation and probably a simple pedestal update procedure will be needed.

---
+++ include/EUTelAutoPedestalNoiseProcessor.h
@@ -0,0 +1,219 @@
+// -*- C++ -*-
+/*
+ *   This source code is part of the Eutelescope package of Marlin.
+ *   You are free to use this source files for your own development as
+ *   long as it stays in a public research context. You are not
+ *   allowed to use it for commercial purpose. You must put this
+ *   header with author names in all development based on this file.
+ *
+ */
+#ifndef EUTELAUTOPEDESTALNOISEPROCESSOR
+#define EUTELAUTOPEDESTALNOISEPROCESSOR
+
+// eutelescope includes ".h" 
+
+// marlin includes ".h"
+#include "marlin/Processor.h"
+
+// lcio includes <.h> 
+#include <IMPL/LCCollectionVec.h>
+
+// system includes <>
+
+
+
+namespace eutelescope {
+
+  //! Automatic pedestal and noise processor
+  /*! Especially when dealing with self-biased MAPS, having a specific
+   *  pedestal run can be considered useless since all pixels have a
+   *  mean value very close to 0 and the typical noise distribution is
+   *  peaked around a single value.
+   *  
+   *  The general idea behind this process is that, we can start the
+   *  analysis procedure with fictitius pedestal and noise values
+   *  valid for a whole sensor that are going then updated during the
+   *  procedure itself (@see PedestalUpdateProcessor)
+   * 
+   *  The user has to specify the initial pedestal and noise values
+   *  for each detector as a steering parameters via the _initPedestal
+   *  and _initNoise float vector.
+   *
+   *  <h4>Input</h4> 
+   *  
+   *  <b>InitPedestal</b>: Float vector containing the initial value
+   *  for pixel pedestal. The size of this vector should be equal to
+   *  the number of detectors in the telescope <br> <b>InitNoise</b>:
+   *  Float vector containing the initial value for the pixel
+   *  noise. The size of this vector should be equal to the number of
+   *  detector in the telescope <br> <b>PedestalCollectionName</b>:
+   *  this is the name of the output pedestal collection.</br>
+   *  <b>NoiseCollectionName</b>: this is the name of the output noise
+   *  collection. <br> <b>StatusCollectionName</b>: this is the name
+   *  of the output status collection.
+   *
+   *  <h4>Output</h4>
+   *  <b>PedestalCollection</b><br><b>NoiseCollection</b><br><b>StatusCollection</b>
+   *
+   *  @param InitPedestal the initial pedestal value (vector of float)
+   *  @param InitNoise the initial noise value (vector of float)
+   *  @param PedestalCollectionName
+   *  @param NoiseCollectionName
+   *  @param StatusCollectionName
+   *
+   *  @author Antonio Bulgheroni, INFN <mailto:antonio.bulgheroni@gmail.com>
+   *  @version $Id: EUTelAutoPedestalNoiseProcessor.h,v 1.1 2007-02-21 14:08:13 bulgheroni Exp $
+   *
+   *
+   */
+
+  class EUTelAutoPedestalNoiseProcessor : public marlin::Processor {
+
+  public:
+
+     
+    //! Returns a new instance of EUTelAutoPedestalNoiseProcessor
+    /*! This method returns an new instance of the this processor.  It
+     *  is called by Marlin execution framework and it shouldn't be
+     *  called/used by the final user.
+     *  
+     *  @return a new EUTelAutoPedestalNoiseProcessor.
+     */
+    virtual Processor * newProcessor() {
+      return new EUTelAutoPedestalNoiseProcessor;
+    }
+
+    //! Default constructor 
+    EUTelAutoPedestalNoiseProcessor ();
+
+    //! Called at the job beginning.
+    /*! This is executed only once in the whole execution. It prints
+     *  out the processor parameters. 
+     */
+    virtual void init ();
+
+    //! Called for every run.
+    /*! It is called for every run, and consequently the run counter
+     *  is incremented. From the run header the number of detectors is
+     *  retrieved and compared with the size of the init values
+     *  floating vectors. In the case the input vectors are shorter
+     *  than the detector number, they are padded with the last
+     *  entry. In the opposite case, the last exceeding elements are
+     *  removed.
+     * 
+     *  The vectors containing the min and max pixels along the two
+     *  directions are retrieved as well because they are used to set
+     *  the CellID in the CollectionVec.
+     * 
+     *  @param run the LCRunHeader of the this current run
+     */
+    virtual void processRunHeader (LCRunHeader * run);
+
+    //! Called every event
+    /*! This is called for each event in the file. During the first
+     *  event the pedestal / noise / status collections are prepared
+     *  with the initialization values provided by the user. Those
+     *  collections are taken off from the current event, in order to
+     *  have them available to all the events.
+     * 
+     *  @param evt the current LCEvent event as passed by the
+     *  ProcessMgr
+     */
+    virtual void processEvent (LCEvent * evt);
+
+
+    //! Called after data processing.
+    /*! This method is called when the loop on events is
+     *  finished. Since the owner of the local copied collections is
+     *  this processor, it is its own responsibility to delete
+     *  them. This is done in the end() call back.
+     */
+    virtual void end();
+
+
+  protected:
+    
+    //! Pedestal collection name
+    /*! Input pedestal collection name. Default value is pedestal.
+     */
+    std::string _pedestalCollectionName;
+
+    //! Noise collection name
+    /*! Input noise collection name. Default value is noise.
+     */
+    std::string _noiseCollectionName;
+
+    //! Status collection name
+    /*! Input status collection name. Default value is status.
+     */
+    std::string _statusCollectionName;
+
+    //! Initial pedestal value
+    /*! This vector of floats is used to store the initial values of
+     *  pedestal. The size of this vector should match the number of
+     *  detectors in the telescope setup. This vector is provided by
+     *  the user from steering file.
+     */
+    FloatVec _initPedestal;
+
+    //! Initial noise value
+    /*! This vector of floats is used to store the initial values of
+     *  noise. The size of this vector should match the number of
+     *  detectors in the telescope setup. This vector is provided by
+     *  the user from steering file.
+     */
+    FloatVec _initNoise;
+    
+    //! Current run number.
+    /*! This number is used to store the current run number
+     */
+    int _iRun;
+
+    //! Current event number.
+    /*! This number is used to store the current event number NOTE that
+     * events are counted from 0 and on a run base
+     */
+    int _iEvt;
+
+  private:
+
+    //! Pedestal collection
+    IMPL::LCCollectionVec * _pedestalCollectionVec;
+
+    //! Noise collection
+    IMPL::LCCollectionVec * _noiseCollectionVec;
+
+    //! Status collection
+    IMPL::LCCollectionVec * _statusCollectionVec;
+
+    //! First pixel along X
+    /*! This array of int is used to store the number of the first
+     *  pixel along the X direction
+     */
+    IntVec _minX;
+
+    //! Last pixel along X
+    /*! This array of int is used to store the number of the last
+     *  pixel along the X direction
+     */
+    IntVec _maxX;
+
+    //! First pixel along Y
+    /*! This array of int is used to store the number of the first
+     *  pixel along the Y direction
+     */
+    IntVec _minY;
+
+    //! Last pixel along Y
+    /*! This array of int is used to store the number of the last
+     *  pixel along the Y direction
+     */
+    IntVec _maxY;
+    
+  };
+
+  //! A global instance of the processor
+  EUTelAutoPedestalNoiseProcessor gEUTelAutoPedestalNoiseProcessor;      
+
+}
+#endif
---
+++ src/EUTelAutoPedestalNoiseProcessor.cc
@@ -0,0 +1,187 @@
+// Author Antonio Bulgheroni, INFN <mailto:antonio.bulgheroni@gmail.com>
+// Version $Id: EUTelAutoPedestalNoiseProcessor.cc,v 1.1 2007-02-21 14:08:13 bulgheroni Exp $
+/*
+ *   This source code is part of the Eutelescope package of Marlin.
+ *   You are free to use this source files for your own development as
+ *   long as it stays in a public research context. You are not
+ *   allowed to use it for commercial purpose. You must put this
+ *   header with author names in all development based on this file.
+ *
+ */
+
+// eutelescope includes ".h" 
+#include "EUTelAutoPedestalNoiseProcessor.h"
+#include "EUTelRunHeaderImpl.h"
+
+// marlin includes ".h"
+#include "marlin/Processor.h"
+
+// lcio includes <.h> 
+#include <IMPL/TrackerRawDataImpl.h>
+#include <IMPL/TrackerDataImpl.h>
+#include <IMPL/LCCollectionVec.h>
+#include <UTIL/CellIDEncoder.h>
+
+
+// system includes <>
+
+
+using namespace std;
+using namespace marlin;
+using namespace eutelescope;
+
+
+
+EUTelAutoPedestalNoiseProcessor::EUTelAutoPedestalNoiseProcessor () : Processor("EUTelAutoPedestalNoiseProcessor"),
+							    _pedestalCollectionVec(NULL),
+							    _noiseCollectionVec(NULL),
+							    _statusCollectionVec(NULL) {
+
+  // modify processor description
+  _description =
+    "EUTelAutoPedestalNoiseProcessor produces initial pedestal / noise / status with user provided values";
+
+ 
+  registerOutputCollection (LCIO::TRACKERDATA, "PedestalCollectionName",
+			   "Pedestal local collection",
+			   _pedestalCollectionName, string ("pedestal"));
+
+  registerOutputCollection (LCIO::TRACKERDATA, "NoiseCollectionName",
+			   "Noise local collection",
+			   _noiseCollectionName, string("noise"));
+
+  registerOutputCollection (LCIO::TRACKERRAWDATA, "StatusCollectionName",
+			   "Pixel status collection",
+			   _statusCollectionName, string("status"));
+
+  const int nDetectorExample = 6;
+  FloatVec initPedeExample(nDetectorExample, 0.);
+
+  registerOptionalParameter("InitPedestalValue",
+			    "The initial value of pedestal (one value for detector)",
+			    _initPedestal, initPedeExample, initPedeExample.size() );
+
+  FloatVec initNoiseExample(nDetectorExample, 1.);
+
+  registerOptionalParameter("InitNoiseValue",
+			    "The initial value of noise (one value for detector)",
+			    _initNoise, initNoiseExample, initNoiseExample.size() );  
+
+  
+}
+
+
+void EUTelAutoPedestalNoiseProcessor::init () {
+  // this method is called only once even when the rewind is active
+  // usually a good idea to
+  printParameters ();
+
+  // set to zero the run and event counters
+  _iRun = 0;
+  _iEvt = 0;
+
+}
+
+void EUTelAutoPedestalNoiseProcessor::processRunHeader (LCRunHeader * rdr) {
+
+  // increment the run counter
+  ++_iRun;
+
+  EUTelRunHeaderImpl * runHeader = static_cast<EUTelRunHeaderImpl*> (rdr);
+  unsigned int noOfDetector = runHeader->getNoOfDetector();
+
+  if (noOfDetector != _initPedestal.size()) {
+    cerr << "[" << name() << "] Resizing the init pedestal vector " << endl;
+    _initPedestal.resize(noOfDetector, _initPedestal.back());
+  }
+
+  if (noOfDetector != _initNoise.size()) {
+    cerr << "[" << name() << "] Resizing the init noise vector " << endl;
+    _initNoise.resize(noOfDetector, _initNoise.back());
+  }
+
+  // now the four vectors containing the first and the last pixel
+  // along both the directions
+  _minX = runHeader->getMinX();
+  _maxX = runHeader->getMaxX();
+  _minY = runHeader->getMinY();
+  _maxY = runHeader->getMaxY();
+
+
+}
+
+
+void EUTelAutoPedestalNoiseProcessor::processEvent (LCEvent * evt) {
+
+  if (isFirstEvent()) {
+
+    _pedestalCollectionVec = new LCCollectionVec(LCIO::TRACKERDATA);
+    _noiseCollectionVec    = new LCCollectionVec(LCIO::TRACKERDATA);
+    _statusCollectionVec   = new LCCollectionVec(LCIO::TRACKERRAWDATA);
+    
+    for (unsigned int iDetector = 0; iDetector < _initPedestal.size(); iDetector++) {
+
+      int nPixel = ( _maxX[iDetector] - _minX[iDetector] + 1 ) * ( _maxY[iDetector] - _minY[iDetector] + 1 ) ;
+
+      TrackerRawDataImpl * status   = new TrackerRawDataImpl;
+      CellIDEncoder<TrackerRawDataImpl> statusEncoder(EUTELESCOPE::MATRIXDEFAULTENCODING, _statusCollectionVec);
+      statusEncoder["sensorID"] = iDetector;
+      statusEncoder["xMin"]     = _minX[iDetector];
+      statusEncoder["yMin"]     = _minY[iDetector];
+      statusEncoder["xMax"]     = _maxX[iDetector];
+      statusEncoder["yMax"]     = _maxY[iDetector];
+      statusEncoder.setCellID(status);
+      ShortVec statusVec(nPixel, EUTELESCOPE::GOODPIXEL);
+      status->setADCValues(statusVec);
+      _statusCollectionVec->push_back(status);
+
+      TrackerDataImpl * pedestal    = new TrackerDataImpl;
+      CellIDEncoder<TrackerDataImpl>  pedestalEncoder(EUTELESCOPE::MATRIXDEFAULTENCODING, _pedestalCollectionVec);
+      pedestalEncoder["sensorID"] = iDetector;
+      pedestalEncoder["xMin"]     = _minX[iDetector];
+      pedestalEncoder["yMin"]     = _minY[iDetector];
+      pedestalEncoder["xMax"]     = _maxX[iDetector];
+      pedestalEncoder["yMax"]     = _maxY[iDetector];
+      pedestalEncoder.setCellID(pedestal);
+      FloatVec pedestalVec(nPixel, _initPedestal[iDetector]);
+      pedestal->setChargeValues(pedestalVec);
+      _pedestalCollectionVec->push_back(pedestal);
+
+      TrackerDataImpl * noise    = new TrackerDataImpl;
+      CellIDEncoder<TrackerDataImpl>  noiseEncoder(EUTELESCOPE::MATRIXDEFAULTENCODING, _noiseCollectionVec);
+      noiseEncoder["sensorID"] = iDetector;
+      noiseEncoder["xMin"]     = _minX[iDetector];
+      noiseEncoder["yMin"]     = _minY[iDetector];
+      noiseEncoder["xMax"]     = _maxX[iDetector];
+      noiseEncoder["yMax"]     = _maxY[iDetector];
+      noiseEncoder.setCellID(noise);    
+      FloatVec noiseVec(nPixel, _initNoise[iDetector]);
+      noise->setChargeValues(noiseVec);
+      _noiseCollectionVec->push_back(noise);
+    }
+
+    _isFirstEvent = false;
+  }
+
+  evt->addCollection(_pedestalCollectionVec, _pedestalCollectionName);
+  evt->takeCollection(_pedestalCollectionName);
+  evt->addCollection(_noiseCollectionVec, _noiseCollectionName);
+  evt->takeCollection(_noiseCollectionName);
+  evt->addCollection(_statusCollectionVec, _statusCollectionName);
+  evt->takeCollection(_statusCollectionName);
+
+  ++_iEvt;
+  
+}
+  
+
+
+
+void EUTelAutoPedestalNoiseProcessor::end() {
+
+  delete _pedestalCollectionVec;
+  delete _noiseCollectionVec;
+  delete _statusCollectionVec;
+  
+}
+
