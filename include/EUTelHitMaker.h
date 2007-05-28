267d361893ddc5a6805e00aa806d037f600701a7
Antonio Bulgheroni <bulgheroni>
2007-05-28 11:52:45 +0000

    In the middle of the debug phase.

--- include/EUTelHitMaker.h
+++ include/EUTelHitMaker.h
@@ -10,20 +10,26 @@
 #ifndef EUTELHITMAKER_H
 #define EUTELHITMAKER_H
 
+// built only if GEAR is available
+#ifdef USE_GEAR
 // eutelescope includes ".h" 
 
 // marlin includes ".h"
 #include "marlin/Processor.h"
 
 // gear includes <.h>
-#ifdef USE_GEAR
 #include <gear/SiPlanesParameters.h>
 #include <gear/SiPlanesLayerLayout.h>
-#endif
 
 // lcio includes <.h> 
 #include <EVENT/LCRunHeader.h>
 #include <EVENT/LCEvent.h>
+
+// AIDA includes <.h>
+#ifdef MARLIN_USE_AIDA
+#include <AIDA/IBaseHistogram.h>
+#endif
+
 
 // system includes <>
 #include <string>
@@ -53,14 +59,84 @@ namespace eutelescope {
    *  provide the name of a suitable collection containing Eta
    *  information for each of the detector.
    *
-   *  Good programming note: If you have a look at the code, you will
-   *  see that all the parts of code related to GEAR are placed within
-   *  a @code #ifdef USE_GEAR ...  #endif @endcode even if the use of
-   *  such a processor is completly meaning less without the use of
-   *  GEAR. This is true, but if someone wants to use EUTelescope just
-   *  to calculate pedestals and to find clusters then GEAR is not
-   *  needed. Those #ifdef/#endif blocks are assuring no compilation
-   *  errors also for those kind of users.
+   *  <h4>Frame of reference conversion</h4>
+   *
+   *  Data coming from the DAQ and from the previous analysis steps
+   *  are organized into Tracker(Raw)Data objects with pixel signals
+   *  saved into a 1D array in a row by row arrangement. Which is the
+   *  first pixel to be readout depends on the hardware. For the
+   *  MimoTel, the first read pixel is the one occupying the top left
+   *  most position as shown in the picture here below. This means
+   *  that the last pixel will be the one on the bottom right
+   *  corner. Generally speaking this local frame of reference has to
+   *  be converted in the telescope frame of reference that is
+   *  right-handed with the z axis parallel to the beam directions,
+   *  the y axis going from the bottom to the top side and the x axis
+   *  from the right to the left hand side.
+   *
+   *  \image html frameOfReference.png "Frame of references: the
+   *  detector on the left and the telescope one on the right."
+   *
+   *  Moreover, the detector FoR can be rotated with respect to the
+   *  telescope one. This rotation in the x-y plane is expressed by
+   *  the two 2D vectors  xPointing[2], yPointing[2].
+   *
+   *  Some examples:
+   *
+   *  \li The two FoRs are perfectly aligned:
+   *     xPointing = {  1 ,  0 }
+   *     yPointing = {  0 ,  1 }
+   * 
+   *  \li The detector is oriented as in the figure above with the x
+   *  axis horizontal going left to right and the y axis vertical
+   *  going top to bottom.
+   *     xPointing = { -1 ,  0 }
+   *     yPointing = {  0 , -1 }
+   *
+   *  Among all possible rotations, four are particularly important
+   *  because of the way the sensor support can be inserted into the
+   *  telescope mechanics. Those are described into the figure here
+   *  below:
+   *  \image html orientation.png "Four important orientations"
+   * 
+   *  <b>Orientation 1</b> represents the case in which the sensor is
+   *  inserted up right in the telescope setup and the beam is hitting
+   *  the front surface of the sensor. This configuration corresponds
+   *  to:<br>
+   *      xPointing = { -1 ,  0 }
+   *      yPointing = {  0 , -1 }
+   *  
+   *  <b>Orientation 2</b> represents the case the sensor is inserted
+   *  up side down facing the beam. This is coded as:<br>
+   *      xPointing = {  1 ,  0 }
+   *      yPointing = {  0 ,  1 }
+   *
+   *  <b>Orientation 3</b> represents the case of the sensor in up
+   *  right position but back illuminated. This is coded as:<br>
+   *      xPointing = {  1 ,  0 }
+   *      yPointing = {  0 , -1 }
+   *
+   *  <b>Orientation 4</b> represents the sensor up side down and back
+   *  illuminated. This is obtained with:<br> 
+   *      xPointing = { -1 ,  0 }
+   *      yPointing = {  0 ,  1 }
+   *
+   *  Other possible rotations are not currently available in the
+   *  geometry description.
+   *
+   *  <h4>Good programming note</h4>
+   *  
+   *  This processor is useful if and only if it has access to
+   *  geometry information through a GEAR implementation. Not all
+   *  EUTelescope users will build Marlin using GEAR, especially who
+   *  is interested to perform only single detector
+   *  characterization. So to avoid compilation error and complains,
+   *  this class is enclosed within a 
+   *  \code 
+   *  \#ifdef USE_GEAR 
+   *     ....
+   *  \#endif 
+   *  \endcode  block.
    *
    *  <h4>Input</h4> 
    *  
@@ -89,7 +165,7 @@ namespace eutelescope {
    *  the eta collection along x and y.
    *
    *  @author Antonio Bulgheroni, INFN <mailto:antonio.bulgheroni@gmail.com>
-   *  @version $Id: EUTelHitMaker.h,v 1.1 2007-05-25 05:16:55 bulgheroni Exp $
+   *  @version $Id: EUTelHitMaker.h,v 1.2 2007-05-28 11:52:45 bulgheroni Exp $
    *
    *
    */
@@ -142,6 +218,8 @@ namespace eutelescope {
      *  sophisticated non linear eta function. This behaviour is
      *  regulated by the user from the steering file.
      * 
+     *  @throw UnknownDataTypeException if the cluster type is unknown
+     *
      *  @param evt the current LCEvent event as passed by the
      *  ProcessMgr
      */
@@ -153,6 +231,16 @@ namespace eutelescope {
      *  finished. 
      */
     virtual void end();
+
+
+    //! Histogram booking
+    /*! Some control histograms are filled during this procedure in
+     *  order to be able to perform easy check on the quality of the
+     *  output hits and also to understand if the frame of reference
+     *  conversion has been properly done. Of course this method is
+     *  effectively doing something only in the case MARLIN_USE_AIDA.
+     */ 
+    void bookHistos();
 
 
   protected:
@@ -208,8 +296,6 @@ namespace eutelescope {
      */ 
     std::map< int, int > _conversionIdMap;
 
-
-#ifdef USE_GEAR
     //! Silicon planes parameters as described in GEAR
     /*! This structure actually contains the following:
      *  @li A reference to the telescope geoemtry and layout
@@ -231,8 +317,34 @@ namespace eutelescope {
      *  init() phase and stored for local use
      */ 
     gear::SiPlanesLayerLayout * _siPlanesLayerLayout;
-#endif
     
+#ifdef MARLIN_USE_AIDA
+    //! AIDA histogram map
+    /*! Instead of putting several pointers to AIDA histograms as
+     *  class members, histograms are booked in the init() method and
+     *  their pointers are inserted into this map keyed by their
+     *  names. 
+     *  The histogram filling can proceed recalling an object through
+     *  its name
+     */ 
+    std::map<std::string, AIDA::IBaseHistogram * > _aidaHistoMap;
+
+    //! Name of the local hit map cloud
+    /*! The histogram pointed by this name is a 2D cloud. The x and y
+     *  axes correspond to the pixel detector axes in its own local
+     *  frame of reference already converted in millimeter. There is
+     *  a cloud like this for each detector in the geometry.
+     */ 
+    static std::string _hitCloudLocalName;
+
+    //! Name of the hit map cloud
+    /*! The histogram pointed by this name is a 2D cloud. The x and y
+     *  axes correspond to the pixel detector axes already in the
+     *  telescope frame of reference. There is a cloud like this for
+     *  each detector in the geometry.
+     */ 
+    static std::string _hitCloudTelescopeName;
+#endif
 
   };
 
@@ -240,4 +352,5 @@ namespace eutelescope {
   EUTelHitMaker gEUTelHitMaker;      
 
 }
+#endif
 #endif
--- src/EUTelHitMaker.cc
+++ src/EUTelHitMaker.cc
@@ -1,6 +1,6 @@
 // -*- mode: c++; mode: auto-fill; mode: flyspell-prog; -*-
 // Author Antonio Bulgheroni, INFN <mailto:antonio.bulgheroni@gmail.com>
-// Version $Id: EUTelHitMaker.cc,v 1.1 2007-05-25 05:16:55 bulgheroni Exp $
+// Version $Id: EUTelHitMaker.cc,v 1.2 2007-05-28 11:52:45 bulgheroni Exp $
 /*
  *   This source code is part of the Eutelescope package of Marlin.
  *   You are free to use this source files for your own development as
@@ -10,27 +10,41 @@
  *
  */
 
+// built only if GEAR is used
+#ifdef USE_GEAR
+
 // eutelescope includes ".h" 
 #include "EUTelHitMaker.h"
 #include "EUTelRunHeaderImpl.h"
 #include "EUTelEventImpl.h"
 #include "EUTELESCOPE.h"
 #include "EUTelVirtualCluster.h"
+#include "EUTelFFClusterImpl.h"
+#include "EUTelExceptions.h"
 #include "EUTelEtaFunctionImpl.h"
+
 
 // marlin includes ".h"
 #include "marlin/Processor.h"
 #include "marlin/Global.h"
 
 // gear includes <.h>
-#ifdef USE_GEAR
 #include <gear/GearMgr.h>
 #include <gear/SiPlanesParameters.h>
+
+// aida includes <.h>
+#ifdef MARLIN_USE_AIDA
+#include <marlin/AIDAProcessor.h>
+#include <AIDA/IHistogramFactory.h>
+#include <AIDA/ICloud2D.h>
+#include <AIDA/ITree.h>
 #endif
 
 // lcio includes <.h> 
 #include <IMPL/LCCollectionVec.h>
 #include <IMPL/TrackerPulseImpl.h>
+#include <IMPL/TrackerHitImpl.h>
+#include <UTIL/CellIDDecoder.h>
 
 // system includes <>
 #include <string>
@@ -42,6 +56,11 @@ using namespace marlin;
 using namespace gear;
 using namespace eutelescope;
 
+// definition of static members mainly used to name histograms
+#ifdef MARLIN_USE_AIDA
+std::string EUTelHitMaker::_hitCloudLocalName     = "HitCloudLocal";
+std::string EUTelHitMaker::_hitCloudTelescopeName = "HitCloudTelescope";
+#endif
 
 
 EUTelHitMaker::EUTelHitMaker () : Processor("EUTelHitMaker") {
@@ -74,7 +93,7 @@ EUTelHitMaker::EUTelHitMaker () : Processor("EUTelHitMaker") {
 }
 
 
-void EUTelHitMaker::init () {
+void EUTelHitMaker::init() {
   // this method is called only once even when the rewind is active
   // usually a good idea to
   printParameters ();
@@ -105,14 +124,15 @@ void EUTelHitMaker::init () {
   _siPlanesParameters  = const_cast<SiPlanesParameters* > (&(Global::GEAR->getSiPlanesParameters()));
   _siPlanesLayerLayout = const_cast<SiPlanesLayerLayout*> ( &(_siPlanesParameters->getSiPlanesLayerLayout() ));
 
-  
 #endif
+
+  // now book histograms plz...
+  bookHistos();
 }
 
 void EUTelHitMaker::processRunHeader (LCRunHeader * rdr) {
 
 
-#ifdef USE_GEAR
   EUTelRunHeaderImpl * header = static_cast<EUTelRunHeaderImpl*> (rdr);
 
   // the run header contains the number of detectors. This number
@@ -153,8 +173,6 @@ void EUTelHitMaker::processRunHeader (LCRunHeader * rdr) {
   //   }
 	
   
-  
-#endif 
 
   // increment the run counter
   ++_iRun;
@@ -166,7 +184,6 @@ void EUTelHitMaker::processRunHeader (LCRunHeader * rdr) {
 
 void EUTelHitMaker::processEvent (LCEvent * event) {
 
-#ifdef USE_GEAR
 
   EUTelEventImpl * evt = static_cast<EUTelEventImpl*> (event) ;
   
@@ -176,8 +193,13 @@ void EUTelHitMaker::processEvent (LCEvent * event) {
   }
 
   
-  LCCollectionVec * pulseCollection = static_cast<LCCollectionVec*> (event->getCollection( _pulseCollectionName ));
+  LCCollectionVec * pulseCollection   = static_cast<LCCollectionVec*> (event->getCollection( _pulseCollectionName ));
+  LCCollectionVec * clusterCollection = static_cast<LCCollectionVec*> (event->getCollection("original_data"));
   LCCollectionVec * xEtaCollection, * yEtaCollection;
+  LCCollectionVec * hitCollection   = new LCCollectionVec(LCIO::TRACKERHIT);
+
+  CellIDDecoder<TrackerPulseImpl>  pulseCellDecoder(pulseCollection);
+  CellIDDecoder<TrackerDataImpl>   clusterCellDecoder(clusterCollection);
 
   if ( _etaCorrection == 1 ) {
     // this means that the user wants to apply the eta correction to
@@ -199,86 +221,208 @@ void EUTelHitMaker::processEvent (LCEvent * event) {
       message<ERROR> ( log() << "Continuing without eta correction " ) ;
       _etaCorrection = 0;
     }
-
-
-    if ( isFirstEvent() ) {
-      if ( ( xEtaCollection->getNumberOfElements() != pulseCollection->getNumberOfElements() ) ||
-	   ( yEtaCollection->getNumberOfElements() != pulseCollection->getNumberOfElements() ) ) {
-	message<ERROR> ( log() 
-			 <<  "The eta collections contain a different number of elements wrt to "
-			 << _siPlanesParameters->getSiPlanesNumber() );
-	message<ERROR> ( log() << "Continuing without eta correction " );
-	_etaCorrection = 0;
-      }
-    }
-
-			 
-
   }
 
- 
+  if ( isFirstEvent() && _etaCorrection == 1) {
+    if ( ( xEtaCollection->getNumberOfElements() != pulseCollection->getNumberOfElements() ) ||
+	 ( yEtaCollection->getNumberOfElements() != pulseCollection->getNumberOfElements() ) ) {
+      message<ERROR> ( log() 
+		       <<  "The eta collections contain a different number of elements wrt to "
+		       << _siPlanesParameters->getSiPlanesNumber() );
+      message<ERROR> ( log() << "Continuing without eta correction " );
+      _etaCorrection = 0;
+    }
+  }
+
+  int detectorID    = -99; // it's a non sense
+  int oldDetectorID = -100;
+
+  int    layerIndex;
+  double xZero, yZero, zZero;
+  double zThickness;
+  double xPitch, yPitch;
+  int    xPointing[2], yPointing[2];
+
   for ( int iPulse = 0; iPulse < pulseCollection->getNumberOfElements(); iPulse++ ) {
     
     TrackerPulseImpl     * pulse   = static_cast<TrackerPulseImpl*> ( pulseCollection->getElementAt(iPulse) );
-    EUTelVirtualCluster  * cluster = static_cast<EUTelVirtualCluster*> ( pulse->getTrackerData() );
+    EUTelVirtualCluster  * cluster;
+    ClusterType type = static_cast<ClusterType>(static_cast<int>((pulseCellDecoder(pulse)["type"])));    
+
+    if ( type == kEUTelFFClusterImpl ) {
+      cluster = static_cast<EUTelFFClusterImpl*> ( pulse->getTrackerData() );
+    } else {
+      message<ERROR> ( "Unknown cluster type. Sorry for quitting" );
+      throw UnknownDataTypeException("Cluster type unknown");
+    }
     
-    int detectorID = cluster->getDetectorID();
-    
-    if ( _conversionIdMap.size() != (unsigned) _siPlanesParameters->getSiPlanesNumber() ) {
-      // first of all try to see if this detectorID already belong to 
-      if ( _conversionIdMap.find( detectorID ) == _conversionIdMap.end() ) {
-	// this means that this detector ID was not already inserted,
-	// so this is the right place to do that
-	for ( int iLayer = 0; iLayer < _siPlanesLayerLayout->getNLayers(); iLayer++ ) {
-	  if ( _siPlanesLayerLayout->getID(iLayer) == detectorID ) {
-	    _conversionIdMap.insert( make_pair( detectorID, iLayer ) );
-	    break;
+    // there could be several clusters belonging to the same
+    // detector. So update the geometry information only if this new
+    // cluster belongs to a different detector.
+    detectorID = clusterCellDecoder(cluster)["sensorID"];
+
+    if ( detectorID != oldDetectorID ) {
+      oldDetectorID = detectorID;
+      
+      if ( _conversionIdMap.size() != (unsigned) _siPlanesParameters->getSiPlanesNumber() ) {
+	// first of all try to see if this detectorID already belong to 
+	if ( _conversionIdMap.find( detectorID ) == _conversionIdMap.end() ) {
+	  // this means that this detector ID was not already inserted,
+	  // so this is the right place to do that
+	  for ( int iLayer = 0; iLayer < _siPlanesLayerLayout->getNLayers(); iLayer++ ) {
+	    if ( _siPlanesLayerLayout->getID(iLayer) == detectorID ) {
+	      _conversionIdMap.insert( make_pair( detectorID, iLayer ) );
+	      break;
+	    }
 	  }
 	}
       }
+      
+      layerIndex = _conversionIdMap[detectorID];
+      xZero      = _siPlanesLayerLayout->getSensitivePositionX(layerIndex); // mm
+      yZero      = _siPlanesLayerLayout->getSensitivePositionY(layerIndex); // mm
+      zZero      = _siPlanesLayerLayout->getSensitivePositionZ(layerIndex); // mm
+      zThickness = _siPlanesLayerLayout->getSensitiveThickness(layerIndex); //  ??? 
+      
+      xPitch  = 30 /* um */ * 0.001 ; // mm
+      yPitch  = 30 /* um */ * 0.001 ; // mm
+      
+      xPointing[0] = -1 ;       xPointing[1] =  0 ;
+      yPointing[0] =  0 ;       yPointing[1] = -1 ;
+
+      
+      if ( isFirstEvent() && (iPulse == 0) ) message<WARNING> ( "Using hardcoded values for the pitches and for the orientation." );
     }
-      
-    int layerIndex = _conversionIdMap[detectorID];
-    double xZero   = _siPlanesLayerLayout->getSensitivePositionX(layerIndex); // mm
-    double yZero   = _siPlanesLayerLayout->getSensitivePositionY(layerIndex); // mm
-    double zZero   = _siPlanesLayerLayout->getSensitivePositionZ(layerIndex); // mm
     
-    double xPitch  = 20 /* um */ * 0.001 ; // mm
-    double yPitch  = 20 /* um */ * 0.001 ; // mm
-      
+    // get the position of the seed pixel. This is in pixel number.
     int xCluCenter, yCluCenter;
     cluster->getSeedCoord( xCluCenter, yCluCenter );
     
-    float xShift, yShift;
-    cluster->getCenterOfGravityShift( xShift, yShift );
-
-    double xCorrection = 0, yCorrection = 0;
-
-    if ( _etaCorrection == 1 ) {
-      
-      EUTelEtaFunctionImpl * xEtaFunc = static_cast<EUTelEtaFunctionImpl*> ( xEtaCollection->getElementAt(detectorID) );
-      EUTelEtaFunctionImpl * yEtaFunc = static_cast<EUTelEtaFunctionImpl*> ( yEtaCollection->getElementAt(detectorID) );
-
-      xCorrection = xEtaFunc->getEtaFromCoG( xShift );
-      yCorrection = yEtaFunc->getEtaFromCoG( yShift );
-
-    }
+    // with the charge center of gravity calculation, we get a shift
+    // from the seed pixel center due to the charge distribution. Those
+    // two numbers are the correction values in the case the Eta
+    // correction is not applied.
+//     float xShift, yShift;
+//     cluster->getCenterOfGravityShift( xShift, yShift );
+//     double xCorrection = static_cast<double> (xShift) ;
+//     double yCorrection = static_cast<double> (yShift) ;
 
     
+//     if ( _etaCorrection == 1 ) {
+      
+//       EUTelEtaFunctionImpl * xEtaFunc = static_cast<EUTelEtaFunctionImpl*> ( xEtaCollection->getElementAt(detectorID) );
+//       EUTelEtaFunctionImpl * yEtaFunc = static_cast<EUTelEtaFunctionImpl*> ( yEtaCollection->getElementAt(detectorID) );
+
+//       xCorrection = xEtaFunc->getEtaFromCoG( xShift );
+//       yCorrection = yEtaFunc->getEtaFromCoG( yShift );
+      
+//     }
+
+//     // rescale the pixel number in millimeter
+//     double xDet = ( static_cast<double> (xCluCenter) + xCorrection + 0.5 ) * xPitch ;
+//     double yDet = ( static_cast<double> (yCluCenter) + yCorrection + 0.5 ) * yPitch ;
+
+// #ifdef MARLIN_USE_AIDA
+//     string tempHistoName;
+//     {
+//       stringstream ss; 
+//       ss << _hitCloudLocalName << "-" << detectorID ;
+//       tempHistoName = ss.str();
+//     }
+//     (dynamic_cast<AIDA::ICloud2D*>(_aidaHistoMap[ tempHistoName ]))->fill(xDet,yDet);
+
+// #endif 
+
+//     // now perform the rotation of the frame of references and put the
+//     // results already into a 3D array of double to be ready for the
+//     // setPosition method of TrackerHit
+//     double telPos[3];
+//     telPos[0] = xPointing[0] * xDet + xPointing[1] * yDet;
+//     telPos[1] = yPointing[0] * xDet + yPointing[1] * yDet;
+
+//     // now the translation
+//     telPos[0] -= xZero;
+//     telPos[1] -= yZero;
+//     telPos[2] = zZero + 0.5 * zThickness;
+
+// #ifdef MARLIN_USE_AIDA
+//     {
+//       stringstream ss;
+//       ss << _hitCloudTelescopeName << "-" << detectorID ;
+//       tempHistoName = ss.str();
+//     }
+//     (dynamic_cast<AIDA::ICloud2D*> (_aidaHistoMap[ tempHistoName ] ))->fill( telPos[0], telPos[1] );
+// #endif
+
+//     // create the new hit
+//     TrackerHitImpl * hit = new TrackerHitImpl;
+//     hit->setPosition( &telPos[0] );
+//     hit->setType( pulseCellDecoder(pulse)["type"] );
+    
+//     // prepare a LCObjectVec to store the current cluster
+//     LCObjectVec clusterVec;
+//     clusterVec.push_back( cluster );
+        
+//     // add the clusterVec to the hit
+//     hit->rawHits() = clusterVec;
+
+//     // add the new hit to the hit collection
+//     hitCollection->push_back( hit );
+
   }
   ++_iEvt;
+  //   evt->addCollection( hitCollection, _hitCollectionName );
   
-#endif 
-  
+  if ( isFirstEvent() ) _isFirstEvent = false;
+
 }
-  
-
-
 
 void EUTelHitMaker::end() {
-
-
 
   message<MESSAGE> ( log() << "Successfully finished" ) ;  
 }
 
+void EUTelHitMaker::bookHistos() {
+  
+#ifdef MARLIN_USE_AIDA
+  message<MESSAGE> ( "Booking histograms" );
+
+  string tempHistoName;
+
+  // histograms are grouped into folders named after the
+  // detector. This requires to loop on detector now.
+  for (int iDet = 0 ; iDet < _siPlanesParameters->getSiPlanesNumber(); iDet++) {
+
+    string basePath;
+    {
+      stringstream ss ;
+      ss << "plane-" << iDet;
+      basePath = ss.str();
+    }
+    AIDAProcessor::tree(this)->mkdir(basePath.c_str());
+    basePath = basePath + "/";
+    
+    {
+      stringstream ss ;
+      ss <<  _hitCloudLocalName << "-" << iDet ;
+      tempHistoName = ss.str();
+    }
+    AIDA::ICloud2D * hitCloudLocal = AIDAProcessor::histogramFactory(this)->createCloud2D( (basePath + tempHistoName).c_str() );
+    _aidaHistoMap.insert( make_pair( tempHistoName, hitCloudLocal ) );
+
+    {
+      stringstream ss ;
+      ss <<  _hitCloudTelescopeName << "-" << iDet ;
+      tempHistoName = ss.str();
+    }
+    AIDA::ICloud2D * hitCloudTelescope = AIDAProcessor::histogramFactory(this)->createCloud2D( ( basePath + tempHistoName ).c_str() );
+    _aidaHistoMap.insert( make_pair ( tempHistoName, hitCloudTelescope ) );
+											   
+  }
+
+
+  
+#endif
+}
+
+#endif
