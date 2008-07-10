0f91fe54f04bea17875181733dced541c7be1b1b
Antonio Bulgheroni <bulgheroni>
2008-07-10 16:09:34 +0000

    Added this new processor to apply the alignment corrections. Still to be deeply tested.

---
+++ include/EUTelApplyAlignmentProcessor.h
@@ -0,0 +1,174 @@
+// -*- mode: c++; mode: auto-fill; mode: flyspell-prog; -*-
+/*
+ *   This source code is part of the Eutelescope package of Marlin.
+ *   You are free to use this source files for your own development as
+ *   long as it stays in a public research context. You are not
+ *   allowed to use it for commercial purpose. You must put this
+ *   header with author names in all development based on this file.
+ *
+ */
+#ifndef EUTELAPPLYALIGNMENTPROCESSOR_H
+#define EUTELAPPLYALIGNMENTPROCESSOR_H 1
+
+// eutelescope includes ".h" 
+
+// marlin includes ".h"
+#include "marlin/Processor.h"
+
+// lcio includes <.h> 
+
+// system includes <>
+#include <iostream>
+
+namespace eutelescope {
+
+  //! Apply the alignment correction to the input hit collection
+  /*! The main goal of this processor is to apply the alignment
+   *  correction to the input hit collection. 
+   *
+   *  The alignment constants are, as well, contained into collections
+   *  of EUTelAlignmentConstant objects and can be loaded from an
+   *  external LCIO file or from a DB query. 
+   *
+   *  <h4>Input collections</h4>
+   *  <br><b>InputHit</b>. 
+   *  This is a collection of TrackerHit containing all the hits that
+   *  need to be corrected for misalignment.
+   *  
+   *  <br><b>AlignmentConstant</b>. 
+   *  This is a collection of EUTelAlignmentConstant. This is a
+   *  LCGenericObject  containing all the needed alignment constants
+   *  calculated by previous processors.
+   *  
+   *  <h4>Output</h4>
+   *  
+   *  <br><b>OutputHit</b>. 
+   *  This is a collection of TrackerHit with the correct hits.
+   *
+   *  @param CorrectionMethod There are actually several different
+   *  methods to apply the alignment constants. Here below a list of
+   *  available methods:
+   *     0. <b>Shifts only</b> No rotations angle will be taken into
+   *  account.
+   *     1. <b>Rotation first</b> The rotational matrix is applied
+   *  before the translation.
+   *     2. <b>Translation first</b> The hits are first shifted and
+   *  then rotated. 
+   *
+   *  @author Antonio Bulgheroni, INFN <mailto:antonio.bulgheroni@gmail.com>
+   *  @version $Id: EUTelApplyAlignmentProcessor.h,v 1.1 2008-07-10 16:09:34 bulgheroni Exp $
+   *
+   *
+   */
+
+  class EUTelApplyAlignmentProcessor:public marlin::Processor {
+
+  public:
+
+     
+    //! Returns a new instance of EUTelApplyAlignmentProcessor
+    /*! This method returns an new instance of the this processor.  It
+     *  is called by Marlin execution framework and it shouldn't be
+     *  called/used by the final user.
+     *  
+     *  @return a new EUTelApplyAlignmentProcessor.
+     */
+    virtual Processor * newProcessor() {
+      return new EUTelApplyAlignmentProcessor;
+    }
+
+    //! Default constructor 
+    EUTelApplyAlignmentProcessor ();
+
+    //! Called at the job beginning.
+    /*! This is executed only once in the whole execution. It prints
+     *  out the processor parameters and reset all needed data
+     *  members. 
+     */
+    virtual void init ();
+
+    //! Called for every run.
+    /*! It is called for every run, and consequently the run counter
+     *  is incremented.
+     * 
+     *  @param run the LCRunHeader of the this current run
+     */
+    virtual void processRunHeader (LCRunHeader * run);
+
+    //! Called every event
+    /*! This is called for each event in the file. A few consistency
+     *  checks are done on the first event to see if the alignment
+     *  constants and the input collection are compatible.
+     * 
+     *  @param evt the current LCEvent event as passed by the
+     *  ProcessMgr
+     */
+    virtual void processEvent (LCEvent * evt);
+
+
+    //! Check event method
+    /*! This method is called by the Marlin execution framework as
+     *  soon as the processEvent is over. For the time being there is
+     *  nothing to check and do in this slot.
+     * 
+     *  @param evt The LCEvent event as passed by the ProcessMgr
+     */
+    virtual void check (LCEvent * evt);
+
+    //! Called after data processing.
+    /*! This method is called when the loop on events is finished. It
+     *  prints only a goodbye message
+     */
+    virtual void end();
+
+
+  protected:
+
+    //! Input collection name.
+    /*! This is the name of the input hit collection.
+     */
+    std::string _inputHitCollectionName;
+
+    //! Alignment constant collection name
+    /*! This is the name of the collection containing the alignment
+     *  constants. This should be the results of the execution of a
+     *  EUTelMille and pede.
+     */
+    std::string _alignmentCollectionName;
+
+    //! Output collection name.
+    /*! This is the name of the output hit collection.
+     */
+    std::string _outputHitCollectionName;
+
+    //! Correction method
+    /*! There are actually several different
+     *  methods to apply the alignment constants. Here below a list of
+     *  available methods:
+     *     0. <b>Shifts only</b> No rotations angle will be taken into
+     *  account.
+     *     1. <b>Rotation first</b> The rotational matrix is applied
+     *  before the translation.
+     *     2. <b>Translation first</b> The hits are first shifted and
+     *  then rotated. 
+     */
+    int _correctionMethod;
+
+    //! Current run number.
+    /*! This number is used to store the current run number
+     */
+    int _iRun;
+
+    //! Current event number.
+    /*! This number is used to store the current event number NOTE that
+     * events are counted from 0 and on a run base
+     */
+    int _iEvt;
+
+  };
+
+  //! A global instance of the processor
+  EUTelApplyAlignmentProcessor gEUTelApplyAlignmentProcessor;      
+
+}
+#endif
---
+++ src/EUTelApplyAlignmentProcessor.cc
@@ -0,0 +1,225 @@
+// -*- mode: c++; mode: auto-fill; mode: flyspell-prog; -*-
+// Author Antonio Bulgheroni, INFN <mailto:antonio.bulgheroni@gmail.com>
+// Version $Id: EUTelApplyAlignmentProcessor.cc,v 1.1 2008-07-10 16:09:34 bulgheroni Exp $
+/*
+ *   This source code is part of the Eutelescope package of Marlin.
+ *   You are free to use this source files for your own development as
+ *   long as it stays in a public research context. You are not
+ *   allowed to use it for commercial purpose. You must put this
+ *   header with author names in all development based on this file.
+ *
+ */
+
+// eutelescope includes ".h" 
+#include "EUTelApplyAlignmentProcessor.h"
+#include "EUTelEventImpl.h"
+#include "EUTelAlignmentConstant.h"
+
+// marlin includes ".h"
+#include "marlin/Processor.h"
+#include "marlin/Exceptions.h"
+
+// lcio includes <.h> 
+#include <IMPL/TrackerHitImpl.h>
+#include <IMPL/TrackerDataImpl.h>
+#include <IMPL/LCCollectionVec.h>
+#include <UTIL/CellIDDecoder.h>
+#include <UTIL/CellIDEncoder.h>
+
+// system includes <>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+using namespace lcio;
+using namespace marlin;
+using namespace eutelescope;
+
+
+
+EUTelApplyAlignmentProcessor::EUTelApplyAlignmentProcessor () :Processor("EUTelApplyAlignmentProcessor") {
+
+  // modify processor description
+  _description =
+    "Apply to the input hit the alignment corrections";
+
+  // first of all we need to register the input collection
+  registerInputCollection (LCIO::TRACKERHIT, "InputHitCollectionName",
+			   "The name of the input hit collection",
+			   _inputHitCollectionName, string ("hit"));
+
+  registerInputCollection (LCIO::LCGENERICOBJECT, "AlignmentConstantName",
+			   "Alignment constant from the condition file",
+			   _alignmentCollectionName, string ("alignment"));
+
+  registerOutputCollection (LCIO::TRACKERHIT, "OutputHitCollectionName",
+			   "The name of the output hit collection",
+			   _outputHitCollectionName, string("correctedHit"));
+
+
+  // now the optional parameters
+  registerProcessorParameter ("CorrectionMethod",
+			      "Available methods are:\n"
+			      " 0 --> shift only \n"
+			      " 1 --> rotation first \n"
+			      " 2 --> shift first ",
+			      _correctionMethod, static_cast<int > (1));
+  
+
+}
+
+
+void EUTelApplyAlignmentProcessor::init () {
+  // this method is called only once even when the rewind is active
+  // usually a good idea to
+  printParameters ();
+
+  // set to zero the run and event counters
+  _iRun = 0;
+  _iEvt = 0;
+
+}
+
+void EUTelApplyAlignmentProcessor::processRunHeader (LCRunHeader * rdr) {
+
+  // increment the run counter
+  ++_iRun;
+
+}
+
+
+void EUTelApplyAlignmentProcessor::processEvent (LCEvent * event) {
+
+  if ( _iEvt % 10 == 0 ) 
+    streamlog_out ( MESSAGE4 ) << "Processing event " 
+			       << setw(6) << setiosflags(ios::right) << event->getEventNumber() << " in run "
+			       << setw(6) << setiosflags(ios::right) << setfill('0')  << event->getRunNumber() << setfill(' ')
+			       << " (Total = " << setw(10) << _iEvt << ")" << resetiosflags(ios::left) << endl;
+  ++_iEvt;
+
+
+  EUTelEventImpl * evt = static_cast<EUTelEventImpl*> (event);
+
+  if ( evt->getEventType() == kEORE ) {
+    streamlog_out ( DEBUG4 ) << "EORE found: nothing else to do." << endl;
+    return;
+  } else if ( evt->getEventType() == kUNKNOWN ) {
+    streamlog_out ( WARNING2 ) << "Event number " << evt->getEventNumber() << " in run " << evt->getRunNumber()
+			       << " is of unknown type. Continue considering it as a normal Data Event." << endl;
+  }
+
+
+
+
+    
+  try {
+    
+    LCCollectionVec * inputCollectionVec         = dynamic_cast < LCCollectionVec * > (evt->getCollection(_inputHitCollectionName));
+    LCCollectionVec * alignmentCollectionVec     = dynamic_cast < LCCollectionVec * > (evt->getCollection(_alignmentCollectionName));
+
+    // I also need the original data collection
+    LCCollectionVec * originalDataCollectionVec  = dynamic_cast < LCCollectionVec * > (evt->getCollection( "original_data" ) );
+    
+    // the cell decoder to get the sensor ID
+    CellIDDecoder<TrackerDataImpl> clusterCellDecoder( originalDataCollectionVec );
+    
+
+    if (isFirstEvent()) {
+      
+      _isFirstEvent = false;
+    }
+  
+
+    LCCollectionVec * outputCollectionVec = new LCCollectionVec(LCIO::TRACKERHIT);
+    
+    for (size_t iHit = 0; iHit < inputCollectionVec->size(); iHit++) {
+      
+      TrackerHitImpl   * inputHit   = dynamic_cast< TrackerHitImpl * >  ( inputCollectionVec->getElementAt( iHit ) ) ;
+      
+      // now we have to understand which layer this hit belongs to. 
+      LCObjectVec        clusterVec = inputHit->getRawHits();
+      TrackerDataImpl  * cluster    = dynamic_cast< TrackerDataImpl *>  ( clusterVec[0] );
+    
+      int sensorID = clusterCellDecoder( cluster ) ["sensorID"]   ;
+      
+      // now that we know at which sensor the hit belongs to, we can
+      // get the corresponding alignment constants
+      EUTelAlignmentConstant * alignment = static_cast< EUTelAlignmentConstant * > ( alignmentCollectionVec->getElementAt( sensorID ) );
+
+      if ( alignment == 0x0 ) cerr << "cazzo! " << endl;
+
+
+      TrackerHitImpl   * outputHit  = new TrackerHitImpl;
+      outputHit->setType( inputHit->getType() );
+      outputHit->rawHits() = clusterVec;
+      
+
+      double * inputPosition      = const_cast< double * > ( inputHit->getPosition() ) ;
+      double   outputPosition[3]  = { 0., 0., 0. };
+
+      if ( _correctionMethod == 0 ) {
+	
+	// this is the shift only case 
+	
+	outputPosition[0] = inputPosition[0] - alignment->getXOffset();
+	outputPosition[1] = inputPosition[1] - alignment->getYOffset();
+	outputPosition[2] = inputPosition[2] - alignment->getZOffset();
+
+      } else if ( _correctionMethod == 1 ) {
+	
+	// this is the rotation first
+	
+	// first the rotation
+	outputPosition[0] = inputPosition[0] + alignment->getGamma() * inputPosition[1] + alignment->getBeta() * inputPosition[2] ;
+	outputPosition[1] = -1 * alignment->getGamma() * inputPosition[0] + inputPosition[1] + alignment->getAlpha() * inputPosition[2];
+	outputPosition[2] = -1 * alignment->getBeta()  * inputPosition[0] + alignment->getAlpha() * inputPosition[1] + inputPosition[2];
+
+	// second the shift
+	outputPosition[0] -= alignment->getXOffset();
+	outputPosition[1] -= alignment->getYOffset();
+	outputPosition[2] -= alignment->getZOffset();
+
+      } else if ( _correctionMethod == 2 ) {
+	
+	// this is the translation first
+	
+	// first the shifts
+	inputPosition[0] -= alignment->getXOffset();
+	inputPosition[1] -= alignment->getYOffset();
+	inputPosition[2] -= alignment->getZOffset();
+	
+	// second the rotation
+	outputPosition[0] = inputPosition[0] + alignment->getGamma() * inputPosition[1] + alignment->getBeta() * inputPosition[2] ;
+	outputPosition[1] = -1 * alignment->getGamma() * inputPosition[0] + inputPosition[1] + alignment->getAlpha() * inputPosition[2];
+	outputPosition[2] = -1 * alignment->getBeta()  * inputPosition[0] + alignment->getAlpha() * inputPosition[1] + inputPosition[2];
+
+      }
+      
+      outputHit->setPosition( outputPosition ) ;
+      outputCollectionVec->push_back( outputHit );  
+
+    }
+    
+
+    evt->addCollection( outputCollectionVec, _outputHitCollectionName );
+  
+  
+  } catch (DataNotAvailableException& e) {
+    streamlog_out  ( WARNING2 ) <<  "No input collection found on event " << event->getEventNumber() 
+				<< " in run " << event->getRunNumber() << endl;
+  }
+  
+}
+  
+
+
+void EUTelApplyAlignmentProcessor::check (LCEvent * evt) {
+  // nothing to check here - could be used to fill check plots in reconstruction processor
+}
+
+
+void EUTelApplyAlignmentProcessor::end() {
+  streamlog_out ( MESSAGE2 ) <<  "Successfully finished" << endl;
+
+}
+
