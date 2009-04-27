b4077cebc7b04b91254d01c5315002b1d81c3590
Antonio Bulgheroni <bulgheroni>
2009-04-27 09:43:09 +0000

    Added this new processor to make correlation between the telescope planes and the DUT. It requires two input hit collection (one from the telescope and one from the DUT) and also needs the DUT to be properly described in GEAR.
    This is a preliminary still to be test version. Not use for production please...

---
+++ include/EUTelXCorrelator.h
@@ -0,0 +1,198 @@
+// -*- mode: c++; mode: auto-fill; mode: flyspell-prog; -*-
+/*
+ *   This source code is part of the Eutelescope package of Marlin.
+ *   You are free to use this source files for your own development as
+ *   long as it stays in a public research context. You are not
+ *   allowed to use it for commercial purpose. You must put this
+ *   header with author names in all development based on this file.
+ *
+ */
+
+#ifdef USE_GEAR
+
+#ifndef EUTELXCORRELATOR_H
+#define EUTELXCORRELATOR_H
+
+// eutelescope includes ".h"
+
+// marlin includes ".h"
+#include "marlin/Processor.h"
+
+// gear includes <.h>
+#include <gear/SiPlanesParameters.h>
+#include <gear/SiPlanesLayerLayout.h>
+
+// lcio includes <.h>
+#include <EVENT/LCRunHeader.h>
+#include <EVENT/LCEvent.h>
+#include <IMPL/TrackerHitImpl.h>
+
+// AIDA includes <.h>
+#if defined(USE_AIDA) || defined(MARLIN_USE_AIDA)
+#include <AIDA/IBaseHistogram.h>
+#include <AIDA/IHistogram2D.h>
+#endif
+
+
+// system includes <>
+#include <string>
+#include <vector>
+#include <map>
+
+namespace eutelescope {
+
+  //! Hit cross correlator
+  /*! This processor can be used to study the correlation among the
+   *  telescope hits and the one detected by the DUT.
+   *
+   *  The user can select if to make the correlation among all
+   *  possible planes in the telescope and the DUT or just among a
+   *  specified plane and the DUT.
+   *
+   *  <h4>Input collections</h4>
+   *
+   *  <b>Telescope hit collection</b>: A collection containing the
+   *  telescope hits
+   *
+   *  <b>DUT hit collection</b>: A collection containing the DUT hits
+   *
+   *
+   *  @author Antonio Bulgheroni, INFN <mailto:antonio.bulgheroni@gmail.com>
+   *  @version $Id: EUTelXCorrelator.h,v 1.1 2009-04-27 09:43:09 bulgheroni Exp $
+   *
+   */
+  class EUTelXCorrelator : public marlin::Processor {
+
+  public:
+
+
+    //! Returns a new instance of EUTelXCorrelator
+    /*! This method returns a new instance of this processor.  It is
+     *  called by Marlin execution framework and it shouldn't be
+     *  called/used by the final user.
+     *
+     *  @return a new EUTelXCorrelator.
+     */
+    virtual Processor * newProcessor() {
+      return new EUTelXCorrelator;
+    }
+
+    //! Default constructor
+    EUTelXCorrelator ();
+
+    //! Called at the job beginning.
+    /*! This is executed only once in the whole execution. It prints
+     *  out the processor parameters
+     */
+    virtual void init ();
+
+    //! Called for every run.
+    /*! It is called for every run, and consequently the run counter
+     *  is incremented.
+     *
+     *  @param run the LCRunHeader of the this current run
+     */
+    virtual void processRunHeader (LCRunHeader * run);
+
+    //! Called every event
+    /*! For each event, we loop over both the telescope and DUT input
+     *  hit collections and for each clusters we will be filling in a
+     *  2D correlation histogram.
+     *
+     *  Histograms are booked in the first event.
+     *
+     *  @param evt the current LCEvent event as passed by the
+     *  ProcessMgr
+     */
+    virtual void processEvent (LCEvent * evt);
+
+
+    //! Called after data processing.
+    /*! This method is called when the loop on events is
+     *  finished.
+     */
+    virtual void end();
+
+
+    //! Histogram booking
+    /*! This method is used to book all the correlation histograms. It
+     *  is called by processEvent when processing the first event.
+     */
+    void bookHistos();
+
+
+  protected:
+
+    //! Telescope input hit collection name
+    std::string _inputTelescopeCollectionName;
+
+    //! DUT input hit collection name
+    std::string _inputDUTCollectionName;
+
+  private:
+
+    //! Run number
+    int _iRun;
+
+    //! Event number
+    int _iEvt;
+
+    //! Silicon planes parameters as described in GEAR
+    /*! This structure actually contains the following:
+     *  @li A reference to the telescope geoemtry and layout
+     *  @li An integer number saying if the telescope is w/ or w/o DUT
+     *  @li An integer number saying the number of planes in the
+     *  telescope.
+     *
+     *  This object is provided by GEAR during the init() phase and
+     *  stored here for local use.
+     */
+    gear::SiPlanesParameters * _siPlanesParameters;
+
+    //! Silicon plane layer layout
+    /*! This is the real geometry description. For each layer
+     *  composing the telescope the relevant information are
+     *  available.
+     *
+     *  This object is taken from the _siPlanesParameters during the
+     *  init() phase and stored for local use
+     */
+    gear::SiPlanesLayerLayout * _siPlanesLayerLayout;
+
+    //! An array with the Z position of planes
+    double * _siPlaneZPosition;
+
+#if defined(USE_AIDA) || defined(MARLIN_USE_AIDA)
+
+    //! AIDA histogram map
+    /*! Instead of putting several pointers to AIDA histograms as
+     *  class members, histograms are booked in the processEvent
+     *  during the first event and
+     *  their pointers are inserted into this map keyed by their
+     *  names.
+     *  The histogram filling can proceed recalling an object through
+     *  its name
+     */
+    std::map<std::string, AIDA::IBaseHistogram * > _aidaHistoMap;
+
+    //! Correlation histogram matrix
+    /*! This is used to store the pointers of each histogram
+     */
+    std::map< unsigned int , AIDA::IHistogram2D* >  _hitXCorrelationMatrix;
+    std::map< unsigned int , AIDA::IHistogram2D* >  _hitYCorrelationMatrix;
+
+    //! Base name of the correlation histogram
+    static std::string _hitXCorrelationHistoName;
+    static std::string _hitYCorrelationHistoName;
+#endif
+
+  };
+
+  //! A global instance of the processor
+  EUTelXCorrelator gEUTelXCorrelator;
+
+
+}
+
+#endif
+#endif
---
+++ src/EUTelXCorrelator.cc
@@ -0,0 +1,367 @@
+// -*- mode: c++; mode: auto-fill; mode: flyspell-prog; -*-
+// Author Antonio Bulgheroni, INFN <mailto:antonio.bulgheroni@gmail.com>
+// Version $Id: EUTelXCorrelator.cc,v 1.1 2009-04-27 09:43:09 bulgheroni Exp $
+/*
+ *   This source code is part of the Eutelescope package of Marlin.
+ *   You are free to use this source files for your own development as
+ *   long as it stays in a public research context. You are not
+ *   allowed to use it for commercial purpose. You must put this
+ *   header with author names in all development based on this file.
+ *
+ */
+
+#ifdef USE_GEAR
+
+// eutelescope includes ".h"
+#include "EUTelXCorrelator.h"
+#include "EUTelRunHeaderImpl.h"
+#include "EUTelEventImpl.h"
+#include "EUTELESCOPE.h"
+#include "EUTelExceptions.h"
+
+// marlin includes ".h"
+#include "marlin/Processor.h"
+#include "marlin/Global.h"
+
+// aida includes <.h>
+#if defined(USE_AIDA) || defined(MARLIN_USE_AIDA)
+#include <marlin/AIDAProcessor.h>
+#include <AIDA/IHistogramFactory.h>
+#include <AIDA/IHistogram2D.h>
+#include <AIDA/ITree.h>
+#endif
+
+// gear includes <.h>
+#include <gear/GearMgr.h>
+#include <gear/SiPlanesParameters.h>
+#include <gear/SiPlanesLayerLayout.h>
+
+// lcio includes <.h>
+#include <IMPL/LCCollectionVec.h>
+#include <IMPL/TrackerPulseImpl.h>
+#include <IMPL/TrackerHitImpl.h>
+#include <UTIL/CellIDDecoder.h>
+
+// system includes <>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+using namespace marlin;
+using namespace eutelescope;
+
+// definition of static members mainly used to name histograms
+#if defined(USE_AIDA) || defined(MARLIN_USE_AIDA)
+std::string EUTelXCorrelator::_hitXCorrelationHistoName       = "HitXCorrelatioHisto";
+std::string EUTelXCorrelator::_hitYCorrelationHistoName       = "HitYCorrelationHisto";
+#endif
+
+EUTelXCorrelator::EUTelXCorrelator () : Processor("EUTelXCorrelator") {
+
+  // modify processor description
+  _description =
+    "EUTelXCorrelator fills histograms with correlation plots between the telescope hits and the DUT ones";
+
+  registerInputCollection(LCIO::TRACKERHIT,"TelescopeHitCollection",
+                          "Telescope hit collection name",
+                          _inputTelescopeCollectionName, string ( "hit" ));
+
+  registerInputCollection(LCIO::TRACKERHIT,"DUTHitCollection",
+                          "DUT hit collection name",
+                          _inputDUTCollectionName, string ( "hit_DUT" ));
+
+}
+
+
+void EUTelXCorrelator::init() {
+  // this method is called only once even when the rewind is active
+  // usually a good idea to
+  printParameters ();
+
+  // set to zero the run and event counters
+  _iRun = 0;
+  _iEvt = 0;
+
+  // check if the GEAR manager pointer is not null!
+  if ( Global::GEAR == 0x0 ) {
+    streamlog_out ( ERROR4 ) <<  "The GearMgr is not available, for an unknown reason." << endl;
+    exit(-1);
+  }
+
+  _siPlanesParameters  = const_cast<gear::SiPlanesParameters* > (&(Global::GEAR->getSiPlanesParameters()));
+  _siPlanesLayerLayout = const_cast<gear::SiPlanesLayerLayout*>
+    ( &(_siPlanesParameters->getSiPlanesLayerLayout() ));
+
+  _siPlaneZPosition = new double[ _siPlanesLayerLayout->getNLayers() ];
+  for ( int iPlane = 0 ; iPlane < _siPlanesLayerLayout->getNLayers(); iPlane++ ) {
+    _siPlaneZPosition[ iPlane ] = _siPlanesLayerLayout->getLayerPositionZ(iPlane);
+  }
+
+  // verify that the telescope has a DUT in the steering file!
+  if ( _siPlanesParameters->getSiPlanesType() != gear::SiPlanesParameters::TelescopeWithDUT ) {
+    throw InvalidGeometryException( this->name() + " needs to have a telescope with DUT!" );
+  }
+
+}
+
+void EUTelXCorrelator::processRunHeader (LCRunHeader * rdr) {
+
+
+  EUTelRunHeaderImpl * runHeader = new EUTelRunHeaderImpl( rdr ) ;
+
+  // perform some consistency check in case we have GEAR support
+
+  // the run header contains the number of detectors. This number
+  // should be in principle be the same as the number of layers in the
+  // geometry description
+  if ( runHeader->getNoOfDetector() != _siPlanesParameters->getSiPlanesNumber() ) {
+    streamlog_out ( ERROR4 ) << "Error during the geometry consistency check: " << endl
+                             << "The run header says there are " << runHeader->getNoOfDetector()
+                             << " silicon detectors " << endl
+                             << "The GEAR description says     " << _siPlanesParameters->getSiPlanesNumber()
+                             << " silicon planes" << endl;
+    exit(-1);
+  }
+
+  // this is the right place also to check the geometry ID. This is a
+  // unique number identifying each different geometry used at the
+  // beam test. The same number should be saved in the run header and
+  // in the xml file. If the numbers are different, instead of barely
+  // quitting ask the user what to do.
+
+  if ( runHeader->getGeoID() == 0 )
+    streamlog_out ( WARNING0 ) <<  "The geometry ID in the run header is set to zero." << endl
+                               <<  "This may mean that the GeoID parameter was not set" << endl;
+
+
+  if ( runHeader->getGeoID() != _siPlanesParameters->getSiPlanesID() ) {
+    streamlog_out ( ERROR1 ) <<  "Error during the geometry consistency check: " << endl
+                             << "The run header says the GeoID is " << runHeader->getGeoID() << endl
+                             << "The GEAR description says is     " << _siPlanesParameters->getSiPlanesID()
+                             << endl;
+
+#ifdef EUTEL_INTERACTIVE
+    string answer;
+    while (true) {
+      streamlog_out ( ERROR1 ) << "Type Q to quit now or C to continue using the actual GEAR description anyway [Q/C]" << endl;
+      cin >> answer;
+      // put the answer in lower case before making the comparison.
+      transform( answer.begin(), answer.end(), answer.begin(), ::tolower );
+      if ( answer == "q" ) {
+        exit(-1);
+      } else if ( answer == "c" ) {
+        break;
+      }
+    }
+#endif
+  }
+
+  delete runHeader;
+
+  // increment the run counter
+  ++_iRun;
+}
+
+
+void EUTelXCorrelator::processEvent (LCEvent * event) {
+
+#if defined(USE_AIDA) || defined(MARLIN_USE_AIDA)
+
+  if (_iEvt % 10 == 0)
+    streamlog_out( MESSAGE4 ) << "Processing event "
+                              << setw(6) << setiosflags(ios::right) << event->getEventNumber() << " in run "
+                              << setw(6) << setiosflags(ios::right) << setfill('0')  << event->getRunNumber()
+                              << setfill(' ') << " (Total = " << setw(10) << _iEvt << ")"
+                              << resetiosflags(ios::left) << endl;
+  ++_iEvt;
+
+
+  EUTelEventImpl * evt = static_cast<EUTelEventImpl*> (event) ;
+
+  if ( evt->getEventType() == kEORE ) {
+    streamlog_out ( DEBUG4 ) << "EORE found: nothing else to do." << endl;
+    return;
+  } else if ( evt->getEventType() == kUNKNOWN ) {
+    streamlog_out ( WARNING2 ) << "Event number " << evt->getEventNumber() << " in run " << evt->getRunNumber()
+                               << " is of unknown type. Continue considering it as a normal Data Event."
+                               << endl;
+  }
+
+  if ( isFirstEvent() ) {
+
+    // need to book histograms
+    bookHistos();
+
+    _isFirstEvent = false;
+
+  }
+
+  try {
+    LCCollectionVec * inputTelescopeCollection = static_cast< LCCollectionVec *>
+      ( event->getCollection( _inputTelescopeCollectionName )) ;
+
+    LCCollectionVec * inputDUTCollection = static_cast< LCCollectionVec *>
+      ( event->getCollection( _inputDUTCollectionName ));
+
+    // loop over all the DUT hit first
+    for ( size_t iDUT = 0 ; iDUT < inputDUTCollection->size(); ++iDUT ) {
+
+      // get the current DUT hit
+      TrackerHitImpl * dutHit = static_cast< TrackerHitImpl * >
+        ( inputDUTCollection->getElementAt ( iDUT ) );
+
+      // get the x,y,z position of the DUT hit
+      const double * dutHitPosition =  dutHit->getPosition() ;
+
+      // now loop over all the telescope hit
+      for ( size_t iTel = 0; iTel < inputTelescopeCollection->size(); ++iTel ) {
+
+        // get the current telescope hit
+        TrackerHitImpl * telHit = static_cast< TrackerHitImpl * >
+          ( inputTelescopeCollection->getElementAt( iTel ) ) ;
+
+        // get the position of the telescope plane
+        const double * telHitPosition = telHit->getPosition();
+
+        // fill the correlation histograms
+        _hitXCorrelationMatrix[ iTel ]->fill ( telHitPosition[0], dutHitPosition[0] ) ;
+        _hitYCorrelationMatrix[ iTel ]->fill ( telHitPosition[1], dutHitPosition[1] );
+
+      } // loop over of the tel hits
+
+
+    } // loop over the DUT hits
+
+
+  } catch (DataNotAvailableException& e  ) {
+
+    streamlog_out  ( WARNING2 ) <<  "No input collection found on event " << event->getEventNumber()
+                                << " in run " << event->getRunNumber() << endl;
+  }
+
+#endif
+
+}
+
+void EUTelXCorrelator::end() {
+
+  streamlog_out ( MESSAGE4 )  << "Successfully finished" << endl;
+  delete [] _siPlaneZPosition;
+
+}
+
+void EUTelXCorrelator::bookHistos() {
+
+
+#if defined(USE_AIDA) || defined(MARLIN_USE_AIDA)
+
+  try {
+
+    streamlog_out ( MESSAGE4 ) <<  "Booking histograms" << endl;
+
+    // create all the directories first
+    vector< string > dirNames;
+    dirNames.push_back ("HitX");
+    dirNames.push_back ("HitY");
+
+    for ( size_t iPos = 0 ; iPos < dirNames.size() ; iPos++ ) {
+
+      AIDAProcessor::tree(this)->mkdir( dirNames[iPos].c_str() ) ;
+
+    }
+
+    string tempHistoName;
+    string tempHistoTitle ;
+
+    // get the number of detectors from GEAR.
+    int nTelDetector = _siPlanesLayerLayout->getNLayers();
+
+    // the boundaries of the histos can be read from the GEAR
+    // description and for safety multiplied by a safety factor
+    // to take into account possible misalignment.
+    //
+    // 2 should be enough because it
+    // means that the sensor is wrong
+    // by all its size.
+    double safetyFactor = 2.0;
+
+    // get the boundaries of the DUT detector
+    double dutXMin  = safetyFactor * ( _siPlanesLayerLayout->getDUTSensitivePositionX() -
+                                       ( 0.5 * _siPlanesLayerLayout->getDUTSensitiveSizeX() ));
+    double dutXMax  = safetyFactor * ( _siPlanesLayerLayout->getDUTSensitivePositionX() +
+                                       ( 0.5 * _siPlanesLayerLayout->getDUTSensitiveSizeX() ));
+    int    dutXNBin = static_cast< int > ( safetyFactor ) * _siPlanesLayerLayout->getDUTSensitiveNpixelX();
+
+
+    double dutYMin = safetyFactor * ( _siPlanesLayerLayout->getDUTSensitivePositionY() -
+                                      ( 0.5 * _siPlanesLayerLayout->getDUTSensitiveSizeY() ));
+    double dutYMax = safetyFactor * ( _siPlanesLayerLayout->getDUTSensitivePositionY() +
+                                      ( 0.5 * _siPlanesLayerLayout->getDUTSensitiveSizeY() ));
+    int    dutYNBin = static_cast< int > ( safetyFactor ) * _siPlanesLayerLayout->getDUTSensitiveNpixelY();
+
+    // loop over all telescope planes
+    for ( int iTel = 0 ; iTel < nTelDetector; ++iTel ) {
+
+      // as declared in the gear description
+      int sensorID = _siPlanesLayerLayout->getID( iTel );
+
+      double telXMin = safetyFactor * ( _siPlanesLayerLayout->getSensitivePositionX( iTel ) -
+                                        ( 0.5 * _siPlanesLayerLayout->getSensitiveSizeX ( iTel ) ));
+      double telXMax = safetyFactor * ( _siPlanesLayerLayout->getSensitivePositionX( iTel ) +
+                                        ( 0.5 * _siPlanesLayerLayout->getSensitiveSizeX ( iTel )));
+      int    telXNBin = static_cast< int > ( safetyFactor ) * _siPlanesLayerLayout->getSensitiveNpixelX( iTel );
+
+
+      double telYMin = safetyFactor * ( _siPlanesLayerLayout->getSensitivePositionY( iTel ) -
+                                        ( 0.5 * _siPlanesLayerLayout->getSensitiveSizeY ( iTel ) ));
+      double telYMax = safetyFactor * ( _siPlanesLayerLayout->getSensitivePositionY( iTel ) +
+                                        ( 0.5 * _siPlanesLayerLayout->getSensitiveSizeY ( iTel )));
+      int    telYNBin = static_cast< int > ( safetyFactor ) * _siPlanesLayerLayout->getSensitiveNpixelY( iTel );
+
+      tempHistoName = _hitXCorrelationHistoName + to_string( sensorID );
+      streamlog_out( DEBUG ) << "Booking histo " << tempHistoName ;
+
+      tempHistoTitle = "Correlation of the DUT and the X detector " + to_string( sensorID );
+
+      // book the histogram
+      AIDA::IHistogram2D * histo2D =
+        AIDAProcessor::histogramFactory( this )->createHistogram2D( tempHistoName.c_str(), telXNBin, telXMin, telXMax,
+                                                                    dutXNBin, dutXMin, dutXMax);
+      histo2D->setTitle( tempHistoTitle.c_str() );
+
+      // add it to the associative map. I'm using iTel instead of
+      // sensorID because it is way too practical
+      _hitXCorrelationMatrix[ iTel ] = histo2D;
+
+      // repeat for the y direction
+      tempHistoName = _hitYCorrelationHistoName + to_string( sensorID );
+      streamlog_out( DEBUG ) << "Booking histo " << tempHistoName ;
+
+      tempHistoTitle = "Correlation of the DUT and the Y detector " + to_string( sensorID );
+
+      // book the histogram
+      histo2D = AIDAProcessor::histogramFactory( this )->createHistogram2D( tempHistoName.c_str(), telYNBin, telYMin, telYMax,
+                                                                            dutYNBin, dutYMin, dutYMax);
+      histo2D->setTitle( tempHistoTitle.c_str() );
+
+      // add it to the associative map. I'm using iTel instead of
+      // sensorID because it is way too practical
+      _hitYCorrelationMatrix[ iTel ] = histo2D;
+
+
+    }
+
+  } catch (lcio::Exception& e ) {
+
+    streamlog_out ( ERROR1 ) << "No AIDAProcessor initialized. Sorry for quitting..." << endl;
+    exit( -1 );
+
+  }
+#endif
+}
+
+
+#endif // USE_GEAR
