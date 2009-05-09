d7c0635f8b4f0b60e6e80fe8b5504fd753c0c37d
Antonio Bulgheroni <bulgheroni>
2009-05-09 20:03:45 +0000

    Added two test options

--- pysub/submit-test.py
+++ pysub/submit-test.py
@@ -5,6 +5,7 @@ import commands
 from optparse import OptionParser
 from pysub import *
 
+
 def main() :
 
     usage = "usage: %prog [options] run-list"
@@ -12,6 +13,8 @@ def main() :
     parser = OptionParser( usage = usage )
     parser.add_option( "--config-file", action="store", dest="config_file",
                        help="Specify the configuration file to be used" )
+    parser.add_option( "--plus",  action="store_true", dest="plus",  help="add a plus sign" )
+    parser.add_option( "--minus", action="store_true", dest="minus", help="add a minus sign" )
 
     # create the new submitter, this will automatically configure it
     submitTest = SubmitTest( parser )
