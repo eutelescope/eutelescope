001ac3b76fe17c1a1768115474398a905344950a
Antonio Bulgheroni <bulgheroni>
2009-05-12 16:48:20 +0000

    Defined three functions: -> doCommand to print the command line on the stdout before really doing it -> getFromGRID to replace the get_from_grid external script -> putOnGRID as above

--- pysub/grid/runjob-converter-tmp.sh
+++ pysub/grid/runjob-converter-tmp.sh
@@ -3,8 +3,55 @@
 # A template of converter job
 #
 # @author Antonio Bulgheroni <mailto:antonio.bulgheroni@gmail.com>
-# @version $Id: runjob-converter-tmp.sh,v 1.1 2009-05-11 17:16:17 bulgheroni Exp $
+# @version $Id: runjob-converter-tmp.sh,v 1.2 2009-05-12 16:48:20 bulgheroni Exp $
 #
+
+#############
+# Defining a function to output a command line message
+# 
+# It takes exactly one string
+doCommand() {
+    echo "> $1 "
+    $1
+    return $?
+}
+
+
+#############
+# defining a function to get files from GRID
+#
+# It takes exactly two arguments:
+#
+# $1: the LFN
+# $2: the local file name
+#
+getFromGRID(){
+    echo "Copying $1 in $2 "
+
+    doCommand "lcg-cp -v lfn:$1 file:$2"
+    return $?
+
+}
+
+#############
+# Defining a function to put and register files on GRID
+#
+# It takes exactly three arguments
+#
+# $1: the local file name
+# $2: the LFN
+# $3: the SE
+#
+putOnGRID() {
+    echo "Copying and registering $1 in $2 on $3"
+
+    doCommand "lcg-cr -v -d $3 -l lfn:$2 file:$1"
+    return $?
+}
+
+
+# This is the begin!
+echo "Starting universal-$RunString at `date `"
 
 # To be replaced with the runString in the format %(run)06d
 RunString="@RunString@"
@@ -14,6 +61,7 @@ GRIDCE="@GRIDCE@"
 GRIDSE="@GRIDSE@"
 GRIDStoreProtocol="@GRIDStoreProtocol@"
 GRIDVO="@GRIDVO@"
+LFC_HOME="/grid/ilc/eudet-jra1"
 GRIDFolderBase="@GRIDFolderBase@"
 GRIDFolderNative="@GRIDFolderNative@"
 GRIDFolderLcioRaw="@GRIDFolderLcioRaw@"
@@ -32,49 +80,33 @@ OutputJoboutputLocal=$PWD/log/universal-$RunString.tar.gz
 SteeringFile=universal-$RunString.xml
 LogFile=universal-$RunString.log
 
+
 # prepare the directory structure as local
 echo "Preparing the directory structure..."
-mkdir native
-mkdir lcio-raw
-mkdir results
-mkdir histo
-mkdir pics
-mkdir db
-mkdir log
-
+doCommand "mkdir native"
+doCommand "mkdir lcio-raw"
+doCommand "mkdir results"
+doCommand "mkdir histo"
+doCommand "mkdir pics"
+doCommand "mkdir db"
+doCommand "mkdir log"
 
 # unpack the library
-echo "> tar xzvf $GRIDLibraryTarball"
-tar xzvf $GRIDLibraryTarball
+doCommand "tar xzvf $GRIDLibraryTarball"
 
 # rename the simjob.slcio because otherwise it gets delete
-echo "> mv simjob.slcio simjob.slcio.keepme "
-mv simjob.slcio simjob.slcio.keepme
-echo " "
+doCommand "mv simjob.slcio simjob.slcio.keepme"
 
 # from now on doing things to get access to ESA
-echo "> source ./ilc-grid-config.sh"
-source ./ilc-grid-config.sh
-echo " " 
-
-echo "> $BASH ./ilc-grid-test-sys.sh || abort \"system tests failed!\""
-$BASH ./ilc-grid-test-sys.sh || abort "system tests failed!"
-echo " " 
-
-echo "> . new $VO_ILC_SW_DIR/initILCSOFT.sh $GRIDILCSoftVersion "
-. $VO_ILC_SW_DIR/initILCSOFT.sh $GRIDILCSoftVersion
-echo " "
-
-
-echo "> $BASH ./ilc-grid-test-sw.sh ; r=$?"
-$BASH ./ilc-grid-test-sw.sh ; r=$?
+doCommand "source ./ilc-grid-config.sh"
+doCommand "$BASH ./ilc-grid-test-sys.sh || abort \"system tests failed!\" "
+doCommand ". $VO_ILC_SW_DIR/initILCSOFT.sh $GRIDILCSoftVersion"
+doCommand "$BASH ./ilc-grid-test-sw.sh"
+r=$?
 if [ $r -ne 0 ] ; then 
     echo "ERROR: " ; cat ./ilc-grid-test-sw.log
     abort "software tests failed!"
 fi
-echo " "
-
-
 # ILCSOFT ready
 echo " "
 echo " ILCSOFT ready to use "
@@ -82,85 +114,50 @@ echo " ------------------------------------------"
 echo " "
 
 # now it's safe to rename the simjob to the original
-echo "> mv simjob.slcio.keepme simjob.slcio"
-mv simjob.slcio.keepme simjob.slcio
-echo " "
+doCommand "mv simjob.slcio.keepme simjob.slcio"
 
-# set the list of Marlin plugins
-c="export MARLIN_DLL=$PWD/libEutelescope.so.0.0.8"
-echo "> $c"
-$c
-echo " "
+# set the list of Marlin plugins and the LD_LIBRARY_PATH
+doCommand "export MARLIN_DLL=$PWD/libEutelescope.so.0.0.8"
+doCommand "export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH"
 
-echo "> export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH"
-export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
-echo " "
-
-# setting permissions for scripts.
-echo "> chmod a+x ./get_file_by_lfn.sh"
-chmod  a+x ./get_file_by_lfn.sh
-echo " "
-
-echo "> chmod a+x ./put_file.sh"
-chmod  a+x ./put_file.sh
-echo " "
-
-return 0
-
-echo "> ./get_file_by_lfn.sh ${InputRawLFN} ${InputRawLocal}"
-./get_file_by_lfn.sh ${InputRawLFN} ${InputRawLocal}"
-rc=$?
-if [ $[rc!=0] = "1" ]; then
-  echo "##### Copying '${InputRawLFN}' failed. Exiting with error."
-  exit 0
-else
-  echo "+++++ Copying '${InputRawLFN}' succeeded. Continuing."
+# get the input raw file
+doCommand "getFromGRID ${InputRawLFN} ${InputRawLocal}"
+r=$?
+if [ $r -ne 0 ] ; then
+    echo "Problem copying ${InputRawLFN}. Exiting with error."
+    exit 1
 fi
 
 # list all the files available
-echo "> ls -l"
-ls -al
-echo " "
-
-echo "> ls -l"
-ls -al
-echo " "
+doCommand "ls -al"
 
 # ready to run marlin
 echo "Starting Marlin `date`"
-echo " "
-
-# c="Marlin $SteeringFile | tee $LogFile "
 c="Marlin $SteeringFile | tee $LogFile"
-echo "> $c"
-$c
-echo " "
+doCommand $c
 
 # remove the input file
-echo "> rm ${InputRawLocal}"
-rm ${InputRawLocal}
+doCommand "rm ${InputRawLocal}"
+
+# put back the files to the GRID
+echo "Copying and registering the output file to SE"
+doCommand "putOnGRID ${OutputLcioLocal} ${OutputLcioLFN} ${GRIDSE}"
+r=$?
+if [ $r -ne 0 ] ; then
+    echo "Problem copying the ${OutputLcioLocal} to the GRID"
+    exit 2
+fi
 
 echo "Preparing the joboutput tarball"
-tar czvf ${OutputJoboutputLocal} *.log *.xml 
+doCommand "tar czvf ${OutputJoboutputLocal} *.log *.xml"
 
 echo "Copying and registering the tarball to SE"
-./put_file.sh ${OutputJoboutputLNF} ${OutputJoboutputLocal} ${GRIDSE}
-if [ $[rc!=0] = "1" ]; then
-    echo "##### Copying '$LFN' failed. No output tar-ball written."
-    exit 0
-else
-    echo "+++++ Copying '$LFN' succeeded. Output tar-ball written."
+doCommand "putOnGRID  ${OutputJoboutputLocal} ${OutputJoboutputLNF} ${GRIDSE}"
+r=$?
+if [ $r -ne 0 ] ; then
+    echo "Problem copying the ${OutputJoboutputLocal} to the GRID"
+    exit 3
 fi
-
-echo "Copying and registering the output file to SE"
-./put_file.sh ${OutputLcioLFN} ${OutputLcioLocal} ${GRIDSE}
-if [ $[rc!=0] = "1" ]; then
-    echo "##### Copying '$LFN' failed."
-    exit 0
-else
-    echo "+++++ Copying '$LFN' succeeded."
-fi
-
 
 # Job finished
 echo "Job finished at `date`"
