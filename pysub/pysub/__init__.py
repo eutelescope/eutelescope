aabf618164ea299d4dde87b176a53f207f6ec291
Antonio Bulgheroni <bulgheroni>
2009-05-13 09:15:04 +0000

    Added the SubmitNativeCopy to the package

--- pysub/pysub/__init__.py
+++ pysub/pysub/__init__.py
@@ -1,4 +1,6 @@
 from submitbase import SubmitBase
 from submitconverter import SubmitConverter
 from submittest import SubmitTest
+from submitnativecopy import SubmitNativeCopy
 from error import *
+from grid import *
