bfb91b5716e70eea413827d1439378a779a16baf
Antonio Bulgheroni <bulgheroni>
2009-05-30 15:40:35 +0000

    Added a patch to fix the histogram file (hadd with empty.root)

--- pysub/grid/runjob-align-tmp.sh
+++ pysub/grid/runjob-align-tmp.sh
@@ -2,7 +2,7 @@
 # A template of alignment job
 #
 # @author Antonio Bulgheroni <mailto:antonio.bulgheroni@gmail.com>
-# @version $Id: runjob-align-tmp.sh,v 1.2 2009-05-21 12:26:00 bulgheroni Exp $
+# @version $Id: runjob-align-tmp.sh,v 1.3 2009-05-30 15:40:35 bulgheroni Exp $
 #
 # errno  0: No error.
 # errno  1: Unable to get the input file from the SE.
@@ -232,7 +232,9 @@ for file in $InputFileList; do
     doCommand "rm ${InputLocal}"
 done
 
-
+# fixing the problem with the histogram file
+doCommand "hadd -f temp.root empty.root ${OutputHistoLocal}"
+doCommand "mv temp.root ${OutputHistoLocal}"
 
 # put back the files to the GRID
 if [ $RunPede == "yes" ] ; then
