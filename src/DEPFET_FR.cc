5848fc6d71cc4be05d79e568596a394203adb3ca
Antonio Bulgheroni <bulgheroni>
2009-07-01 17:32:10 +0000

    Julia changes.

--- src/DEPFET_FR.cc
+++ src/DEPFET_FR.cc
@@ -86,6 +86,19 @@ int DEPFET_FR::READ_EVENT_HEADER(int iprint) {
 
     break;
 //....................................................
+  case (0x3): // DEPFET Event 128x128
+    //printf("DEPFET event \n");
+    if(header.EventType==0x0) { 
+       printf("Mod %d BORE\n",header.ModuleNo);}
+      
+    else if(header.EventType==0x1){
+       printf("Mod %d EORE\n",header.ModuleNo); return 1;}
+    else if(header.EventType==0x2){
+	
+         if(iprint) printf("Mod %d data\n",header.ModuleNo); return 2;}
+    break;
+
+//....................................................
   case(0xd):// TLU event
     if(header.EventType==0x2 ) { printf("TLU EVENT\n");
     fseek(DF,(header.EventSize-2)*4,SEEK_CUR);
@@ -117,21 +130,35 @@ int DEPFET_FR::READ_EVENT_HEADER(int iprint) {
 int  DEPFET_FR::READ_DEPFET_EVENT(int iprint,int SkipStartGate,int DATA1[NUM_COL][NUM_ROW] ){
   int rc,i;
   int icol,irow,startgate = 0;
-  rc=fread(&infoword,sizeof(infoword),1,DF);
+
+   unsigned int Startgate;
+    struct InfoWord *infoword1;
+    struct InfoWord infoword2;
+
+    rc=fread(&Startgate,sizeof(Startgate),1,DF);
+    //  printf("unsign =%d struct =%d \n",sizeof(Startgate),sizeof(infoword2));
+    //printf("Infoword2 =%d \n",sizeof(struct InfoWord));
+    //  rc=fread(&infoword1,sizeof(infoword1),1,DF);
+   infoword1=(struct InfoWord *)&Startgate;
+
+//  rc=fread(&infoword,sizeof(infoword),1,DF);
   if(rc==0) {printf("EOF \n"); return 1;};
   if(iprint) { 
-    printf("InfoWord.framecnt=0x%x\n",infoword.framecnt);
-    printf("InfoWord.startgate=0x%x\n",infoword.startgate);
-    printf("InfoWord.succframes=0x%x\n",infoword.succframes);
-    printf("InfoWord.zerosupp=0x%x \n",infoword.zerosupp);
-    printf("InfoWord.startgate_ver=0x%x\n",infoword.startgate_ver);
-    printf("InfoWord.temperature=0x%x\n",infoword.temperature);
+    printf("InfoWord.framecnt=0x%x\n",infoword1->framecnt);
+    printf("InfoWord.startgate=0x%x\n",infoword1->startgate);
+    printf("InfoWord.succframes=0x%x\n",infoword1->succframes);
+    printf("InfoWord.zerosupp=0x%x \n",infoword1->zerosupp);
+    printf("InfoWord.startgate_ver=0x%x\n",infoword1->startgate_ver);
+    printf("InfoWord.temperature=0x%x\n",infoword1->temperature);
   };
 
   int eventsize =  header.EventSize-3 ;
-  Hit * depfet_hits = new Hit[eventsize];
+  if(iprint) printf("DEPFET_FR:: eventsize=%d \n",eventsize);
 
-  fread(&depfet_hits,sizeof(hit)*eventsize,1,DF);
+    Hit * depfet_hits = new Hit[eventsize];
+
+  rc=fread(depfet_hits,sizeof(Hit)*eventsize,1,DF);
+  // printf("DEPFET_FR:: rc=%d \n",rc);
   if(infoword.zerosupp) {
     for (icol=0;icol<NUM_COL;icol++) {
       for (irow=0;irow<NUM_ROW;irow++) {
@@ -140,16 +167,21 @@ int  DEPFET_FR::READ_DEPFET_EVENT(int iprint,int SkipStartGate,int DATA1[NUM_COL
     }
   }
 
-
+  if(iprint) {printf("DEPFET_FR:: Read depfet_hits done \n");} 
   for (i=0;i<eventsize;i++) {
-    if (i==0) startgate=depfet_hits[i].row;
-    int z= depfet_hits[i].data & 0x0000ffff;
-    // printf("z=%d\n",z);
+      Hit *tmp;
+      tmp=&depfet_hits[i];
+      //     memcpy(&tmp,&(depfet_hits[i]),sizeof(Hit));
+             if (i==0) startgate=depfet_hits[i].row;
+      //if (i==0) startgate=(*tmp).row;
+        int z= depfet_hits[i].data & 0x0000ffff;
+	//   printf("z=%d\n",z);
 
 //     DATA[depfet_hits[i].col][depfet_hits[i].row]=z;
     DATA1[depfet_hits[i].col][depfet_hits[i].row]=z;
 
   };
+  if(iprint) {printf("DEPFET_FR:: decoding done \n");} 
   if( SkipStartGate) {
     if (startgate/2<62) {
       for(icol=0;icol<NUM_COL;icol++) {
