520ad8d0c8adc11a2ec9f01dad9063ca8333de7b
Igor Rubinsky <rubinsky@mail.desy.de>
2010-06-16 08:13:04 +0000

    adding EUTelAPIXClusteringProcessor.h header file

---
+++ include/EUTelAPIXClusteringProcessor.h
@@ -0,0 +1,116 @@
+
+#ifndef EUTELAPIXCLUSTERINGPROCESSOR_H
+#define EUTELAPIXCLUSTERINGPROCESSOR_H 1
+
+// since v00-00-09 built only if GEAR is available
+#ifdef USE_GEAR
+
+// eutelescope includes ".h"
+#include "EUTelExceptions.h"
+#include "EUTELESCOPE.h"
+
+// marlin includes ".h"
+#include "marlin/EventModifier.h"
+#include "marlin/Processor.h"
+#include "marlin/Exceptions.h"
+
+// gear includes <.h>
+#include <gear/SiPlanesParameters.h>
+#include <gear/SiPlanesLayerLayout.h>
+
+// aida includes <.h>
+#if defined(USE_AIDA) || defined(MARLIN_USE_AIDA)
+#include <AIDA/IBaseHistogram.h>
+#endif
+
+// lcio includes <.h>
+#include <IMPL/TrackerRawDataImpl.h>
+#include <IMPL/LCCollectionVec.h>
+
+// system includes <>
+#include <string>
+#include <map>
+#include <cmath>
+#include <vector>
+#include <list>
+
+namespace eutelescope {
+
+
+  class EUTelAPIXClusteringProcessor : public marlin::Processor , public marlin::EventModifier {
+
+  public:
+    
+    virtual Processor * newProcessor() {
+      return new EUTelAPIXClusteringProcessor;
+    }
+    
+    EUTelAPIXClusteringProcessor();
+
+    virtual const std::string & name() const { return Processor::name() ; }
+    virtual void init ();
+    virtual void processRunHeader (LCRunHeader * run);
+    virtual void processEvent (LCEvent * evt);
+    virtual void modifyEvent( LCEvent * evt ) ;
+    virtual void check (LCEvent * evt);
+    virtual void end();
+    void resetStatus(IMPL::TrackerRawDataImpl * status);
+
+#if defined(USE_AIDA) || defined(MARLIN_USE_AIDA)
+	void bookHistos();
+	void fillHistos(LCEvent * evt);
+	static std::string _clusterSignalHistoName;
+	static std::string _seedSignalHistoName;
+	static std::string _hitMapHistoName;
+	static std::string _seedSNRHistoName;
+	static std::string _clusterNoiseHistoName;
+	static std::string _clusterSNRHistoName;
+	static std::string _eventMultiplicityHistoName;
+	static std::string _pixelSignalHistoName;
+	static std::string _clusterSizeName;
+	static std::string _clusterSizeXName;
+	static std::string _clusterSizeYName;
+	static std::string _lvl1TriggerName;
+	static std::string _lvl1TriggerDiffName;
+#endif
+    void initializeGeometry( LCEvent * event ) throw ( marlin::SkipEventException );
+    
+    protected:
+	void Clustering(LCEvent * evt, LCCollectionVec * pulse);
+	std::string _zsDataCollectionName;
+	std::string _clusterCollectionName;
+        int _iRun;
+	int _iEvt; 
+	bool _isFirstEvent;
+	unsigned int _initialClusterCollectionSize;
+	int _minNPixels;
+	int _minXDistance;
+	int _minYDistance;
+	int _minDiagDistance;
+	int _minCharge;
+	int _minLVL1;
+	bool _fillHistos;
+    
+	int _noOfDetector;
+	bool _isGeometryReady;
+	std::vector<int> _sensorIDVec;
+	gear::SiPlanesParameters* _siPlanesParameters;
+	gear::SiPlanesLayerLayout*_siPlanesLayerLayout;
+	std::map< int , int > _layerIndexMap;
+	std::vector< int > _orderedSensorIDVec;
+	   
+	
+	std::string _histoInfoFileName;
+	std::vector<int > _clusterSpectraNVector;
+	std::vector<int > _clusterSpectraNxNVector;
+	std::map<std::string , AIDA::IBaseHistogram * > _aidaHistoMap;
+
+
+  };
+
+  //! A global instance of the processor
+  EUTelAPIXClusteringProcessor gEUTelAPIXClusteringProcessor;
+
+}
+#endif // USE_GEAR
+#endif
