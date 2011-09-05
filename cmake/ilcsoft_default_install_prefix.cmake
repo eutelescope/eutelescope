2dc6f7db75ba52e32959eb62a63e4d4549e03eb0
Jan Engels <jan.engels@desy.de>
2011-09-05 15:50:15 +0000

    removed v01-10 backwards compatibility cmake scripts

--- cmake/ilcsoft_default_install_prefix.cmake
+++
@@ -1,9 +0,0 @@
-# set default install prefix to project root directory
-# instead of the cmake default /usr/local
-IF( CMAKE_INSTALL_PREFIX STREQUAL "/usr/local" )
-    SET( CMAKE_INSTALL_PREFIX "${PROJECT_SOURCE_DIR}" )
-ENDIF()
-
-# write this variable to cache
-SET( CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}" CACHE PATH "Where to install ${PROJECT_NAME}" FORCE )
-
