41bd6b46ea5a1c361ec4c0f49ca0d1ef48fe2322
Igor Rubinsky <rubinsky@mail.desy.de>
2011-11-14 00:51:10 +0000

    add ReferenceHit class in order to keep track of all geometry transofmrations per plane

---
+++ include/EUTelReferenceHit.h
@@ -0,0 +1,129 @@
+// -*- mode: c++; mode: auto-fill; mode: flyspell-prog; -*-
+/*
+ *   This source code is part of the Eutelescope package of Marlin.
+ *   You are free to use this source files for your own development as
+ *   long as it stays in a public research context. You are not
+ *   allowed to use it for commercial purpose. You must put this
+ *   header with author names in all development based on this file.
+ *
+ */
+
+#ifndef EUTELREFERENCEHIT_H
+#define EUTELREFERENCEHIT_H
+
+#define REFHIT_CONST_MAX_SIZE 20
+
+// lcio includes <.h>
+#include <lcio.h>
+#include <IMPL/LCGenericObjectImpl.h>
+
+// system includes <>
+#include <string>
+
+namespace eutelescope {
+
+  //! ReferenceHit class for the EUTelescope package
+  /*! The aim of this class is to store into a LCGenericObject the
+   *  hit nment contants obtained by the execution of EUTelMille +
+   *  pede. 
+   *
+   *  @author Igor Rubinskiy, DESY <mailto:rubinsky@mail.desy.de>
+   *  @version $Id: EUTelReferenceHit.h,v 1.3 2008-07-10 16:06:00 rubinsky Exp $
+   */ 
+  class EUTelReferenceHit : public IMPL::LCGenericObjectImpl {
+
+  public:
+    //! Default constructor
+    /*! This is the default constructor of the alignment constants
+     *  
+     */
+    EUTelReferenceHit();
+
+    //! Constructor with all the needed parameters
+    /*! This constructor is the recommended one because it set all the
+     *  needed parameters in one go. 
+     *
+     *  @param sensorID The sensor number as from the GEAR file
+     *  @param xOff Offset of the sensor along the x direction
+     *  @param yOff Offset of the sensor along the y direction
+     *  @param zOff Offset of the sensor along the z direction
+     *  @param alpha Angle of the sensor along x
+     *  @param beta  Angle of the sensor along y
+     *  @param gamma Angle of the sensor along z
+     *
+     */ 
+    EUTelReferenceHit( int sensorID, 
+       double xOff,   double yOff,   double zOff,
+       double alpha, double beta, double gamma );
+      
+    //! Default destructor
+    virtual ~EUTelReferenceHit() { /* NO-OP */ ; }
+
+    //! Set the sensor id
+    void setSensorID( int id ) ;
+
+    //! Set the x offset in mm 
+    void setXOffset( double off ) ;
+
+    //! Set the y offset in mm 
+    void setYOffset( double off ) ;
+
+    //! Set the z offset in mm 
+    void setZOffset( double off ) ;
+
+    //! Set the angle around x
+    void setAlpha( double theta );
+
+    //! Set the angle around y
+    void setBeta( double theta );
+
+    //! Set the angle around z
+    void setGamma( double theta );
+
+    //! Get the sensor ID
+    int getSensorID() const ;
+    
+    //! Get the offset along x
+    double getXOffset() const;
+
+    //! Get the offset along y
+    double getYOffset() const;
+
+    //! Get the offset along z
+    double getZOffset() const;
+
+    //! Get the theta along x
+    double getAlpha() const;
+
+    //! Get the theta along y
+    double getBeta() const;
+
+    //! Get the theta along z
+    double getGamma() const;
+
+    //! Print the output
+    /*! This method is used to print out the constant 
+     *
+     *  @param os The input output stream
+     */
+    virtual void print(std::ostream& os) const ;
+
+    //! Overload of operator<<
+    /*! This friend function is the overload of the operator << for
+     *  the EUTelReferenceHit
+     *
+     *  @param os The input output stream as modified by the print
+     *  method
+     *  @param c The alignment constant
+     *  @return The output stream
+     *
+     */ 
+    friend std::ostream& operator<< (std::ostream& os, const EUTelReferenceHit & c) { c.print(os); return os; }
+
+  };
+
+  
+
+}
+
+#endif
---
+++ src/EUTelReferenceHit.cc
@@ -0,0 +1,126 @@
+// -*- mode: c++; mode: auto-fill; mode: flyspell-prog; -*-
+// Author Igor Rubinskiy, INFN <mailto:rubinky@mail.desy.de>
+// Version $Id: EUTelReferenceHit.cc,v 1.3 2009-07-15 17:21:28 rubinsky Exp $
+/*
+ *   This source code is part of the Eutelescope package of Marlin.
+ *   You are free to use this source files for your own development as
+ *   long as it stays in a public research context. You are not
+ *   allowed to use it for commercial purpose. You must put this
+ *   header with author names in all development based on this file.
+ *
+ */
+
+// eutelescope includes ".h"
+#include "EUTelReferenceHit.h"
+
+// lcio includes <.h>
+#include <lcio.h>
+#include <IMPL/LCGenericObjectImpl.h>
+
+// system includes <>
+#include <iostream>
+#include <iomanip>
+
+using namespace lcio;
+using namespace eutelescope;
+using namespace std;
+
+EUTelReferenceHit::EUTelReferenceHit() : IMPL::LCGenericObjectImpl( 1, 0 , REFHIT_CONST_MAX_SIZE ) {
+  _typeName        = "Reference hit";
+  _dataDescription = "sensorID, xOff, yOff, zOff, alpha, beta, gamma ";
+  _isFixedSize     = true;
+
+
+  setIntVal   ( 0, 0   );
+  for ( size_t iPos = 0; iPos < REFHIT_CONST_MAX_SIZE; ++iPos ) {
+   setDoubleVal( iPos, 0.0 );
+  }
+
+
+}
+
+EUTelReferenceHit::EUTelReferenceHit( int sensorID,
+                                                double xOff,   double yOff,   double zOff,
+                                                double alpha, double beta, double gamma )  :
+
+  IMPL::LCGenericObjectImpl( 1, 0, REFHIT_CONST_MAX_SIZE) {
+
+  _typeName        = "Reference Hit";
+  _dataDescription = "sensorID,\n"
+    "xOff,    yOff,    zOff,    alpha,    beta,    gamma";
+  _isFixedSize     = true;
+
+  setIntVal   ( 0, 0   );
+  for ( size_t iPos = 0; iPos < REFHIT_CONST_MAX_SIZE; ++iPos ) {
+   setDoubleVal( iPos, 0.0 );
+  }
+
+  // set the sensor ID
+  setIntVal( 0 , sensorID );
+
+  // set the sensor shifts in mm
+  setDoubleVal( 0, xOff );
+  setDoubleVal( 1, yOff );
+  setDoubleVal( 2, zOff );
+
+  // set the sensor angles
+  setDoubleVal( 3, alpha );
+  setDoubleVal( 4, beta );
+  setDoubleVal( 5, gamma );
+}
+
+void EUTelReferenceHit::setSensorID( int id ) { setIntVal( 0 , id ) ; }
+
+void EUTelReferenceHit::setXOffset( double off ) { setDoubleVal( 0, off ); }
+void EUTelReferenceHit::setYOffset( double off ) { setDoubleVal( 1, off ); }
+void EUTelReferenceHit::setZOffset( double off ) { setDoubleVal( 2, off ); }
+
+void EUTelReferenceHit::setAlpha( double theta ) { setDoubleVal( 3, theta ); }
+void EUTelReferenceHit::setBeta( double theta ) { setDoubleVal( 4, theta ); }
+void EUTelReferenceHit::setGamma( double theta ) { setDoubleVal( 5, theta ); }
+
+
+int EUTelReferenceHit::getSensorID()   const { return getIntVal( 0 ) ; }
+
+double EUTelReferenceHit::getXOffset() const { return getDoubleVal( 0 ) ; }
+double EUTelReferenceHit::getYOffset() const { return getDoubleVal( 1 ) ; }
+double EUTelReferenceHit::getZOffset() const { return getDoubleVal( 2 ) ; }
+
+double EUTelReferenceHit::getAlpha() const { return getDoubleVal( 3 ) ; }
+double EUTelReferenceHit::getBeta()  const { return getDoubleVal( 4 ) ; }
+double EUTelReferenceHit::getGamma() const { return getDoubleVal( 5 ) ; }
+
+
+void EUTelReferenceHit::print(ostream & os ) const {
+
+  const int maxFieldNo = 7;
+  const int narrowWidth = 10;
+  const int largeWidth = 14;
+  const int lineWidth  = largeWidth * maxFieldNo;
+
+  string dashline( lineWidth, '-' );
+
+  os << dashline << endl
+     << setw( lineWidth - narrowWidth ) << setiosflags(ios::left) << "Sensor ID " << resetiosflags(ios::left)
+     << setw( narrowWidth ) << setiosflags(ios::right) << getSensorID() << resetiosflags(ios::right) << endl
+     << dashline << endl
+     << setw(largeWidth) << " X off [mm] "
+     << setw(largeWidth) << " Y off [mm] "
+     << setw(largeWidth) << " Z off [mm] "
+     << setw(largeWidth) << " X angle "
+     << setw(largeWidth) << " Y angle "
+     << setw(largeWidth) << " Z angle "
+     << endl
+     << setw(largeWidth) << getXOffset()
+     << setw(largeWidth) << getYOffset()
+     << setw(largeWidth) << getZOffset()
+     << setw(largeWidth) << getAlpha()
+     << setw(largeWidth) << getBeta()
+     << setw(largeWidth) << getGamma()
+     << endl
+     << dashline
+     << endl;
+
+
+  return;
+}
