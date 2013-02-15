920e405f150cc6802f65c17b05dc2c5d66b1bb2b
Denys Lontkovskyi <denys.lontkovskyi@desy.de>
2013-02-15 12:36:19 +0000

    Add track finder base class

---
+++ include/EUTelTrackFinder.h
@@ -0,0 +1,71 @@
+/* 
+ * File:   EUTelTrackFinder.h
+ * Contact: denys.lontkovskyi@desy.de
+ *
+ * Created on January 22, 2013, 2:13 PM
+ */
+
+#ifndef EUTELTRACKFINDER_H
+#define	EUTELTRACKFINDER_H
+
+// eutelescope includes ".h"
+//#include "EUTelUtility.h"
+
+// lcio includes <.h>
+#include <IMPL/TrackerHitImpl.h>
+
+// system includes <>
+#include <map>
+#include <string>
+#include <vector>
+
+
+namespace eutelescope {
+
+    
+    class EUTelTrackFinder {
+        
+    public:
+        EUTelTrackFinder();
+        EUTelTrackFinder( const EUTelTrackFinder& orig );
+        EUTelTrackFinder( std::string name );
+        
+        virtual ~EUTelTrackFinder();
+
+    public:
+        enum State {
+            kIsNotReady, kIsReady
+        };
+
+        enum SearchResult {
+            kFailed, kSuccess
+        };
+        
+    public:
+        virtual void Reset();
+        virtual EUTelTrackFinder::SearchResult SearchTracks();
+        virtual std::map<int, EVENT::TrackerHitVec> GetTrackCandidates() const;
+        
+        void SetAllHits( const std::vector< EVENT::TrackerHitVec >& _allHits);
+        std::vector< EVENT::TrackerHitVec > GetAllHits() const;
+
+        inline void SetName( std::string name) { this->_name = name; }
+        inline std::string GetName() const { return _name; }
+        
+    protected:
+        virtual EUTelTrackFinder::SearchResult DoTrackSearch();
+        
+    protected:        
+        State _isReady;
+        SearchResult _searchResult;
+                
+        std::string _name;
+        
+        std::vector< EVENT::TrackerHitVec > _allHits;
+        std::map< int, EVENT::TrackerHitVec > _trackCandidates;
+
+    };
+
+}
+#endif	/* EUTELTRACKFINDER_H */
+
---
+++ src/EUTelTrackFinder.cc
@@ -0,0 +1,116 @@
+/* 
+ * File:   EUTelTrackFinder.cc
+ * Contact: denys.lontkovskyi@desy.de
+ * 
+ * Created on January 22, 2013, 2:13 PM
+ */
+
+// eutelescope includes ".h"
+#include "EUTelTrackFinder.h"
+#include "EUTELESCOPE.h"
+//#include "EUTelUtility.h"
+
+// lcio includes <.h>
+#include <IMPL/TrackerHitImpl.h>
+
+namespace eutelescope {
+    
+
+    EUTelTrackFinder::EUTelTrackFinder() : _isReady(kIsNotReady),
+                                           _searchResult(kFailed),
+                                           _name("DefaultTrackFinder") {
+        _allHits.clear();
+        _trackCandidates.clear();
+
+        return;
+    }
+
+    EUTelTrackFinder::EUTelTrackFinder(
+            std::string name ) :
+                                            _isReady(kIsNotReady),
+                                            _searchResult(kFailed),
+                                            _name(name) {
+        _allHits.clear();
+        _trackCandidates.clear();
+
+        return;
+    }
+
+    EUTelTrackFinder::EUTelTrackFinder(const EUTelTrackFinder& orig) {
+    }
+
+    EUTelTrackFinder::~EUTelTrackFinder() {
+    }
+    
+    
+    std::map<int, EVENT::TrackerHitVec> EUTelTrackFinder::GetTrackCandidates() const {
+        
+        if( !_isReady ) {
+            streamlog_out( ERROR ) << "Track finder " << _name << std::endl;
+            streamlog_out( ERROR ) << "Tracks were not searched. " 
+                                   << "Initialise track finder by supplying hits first" << std::endl;
+            streamlog_out( ERROR ) << "Returning empty list of track candidates" << std::endl;
+                                   
+        }
+        
+        if( _isReady && _searchResult == kFailed ){
+            streamlog_out( MESSAGE ) << "Track finder " << _name << std::endl;
+            streamlog_out( MESSAGE ) << "Failed. Returning empty list of track candidates" << std::endl;
+        }
+        
+        return _trackCandidates;
+    }
+
+    void EUTelTrackFinder::SetAllHits( const std::vector< EVENT::TrackerHitVec >& allHits) {
+        
+        streamlog_out( DEBUG1 ) << "EUTelTrackFinder::SetAllHits()." << std::endl;
+        streamlog_out( DEBUG1 ) << " Hits for track search ready." << std::endl;
+        _isReady = kIsReady;
+	this->_allHits = allHits;
+//	streamlog_out( DEBUG ) << "Hits coorinates"<<std::endl;
+//	for( int layerIndex = 0; layerIndex < _allHits.size(); layerIndex++ ) {
+//		streamlog_out( DEBUG ) << "Plane #"<<layerIndex<<std::endl;
+//		for( int hitIndex = 0; hitIndex < _allHits[layerIndex].size(); hitIndex++ ) {
+//			Utility::Hit hit = _allHits[layerIndex][hitIndex];
+//			streamlog_out( DEBUG ) << hit.X() << std::endl;
+//		}
+//	}	
+	return;
+    }
+
+    std::vector< EVENT::TrackerHitVec > EUTelTrackFinder::GetAllHits() const {
+        return _allHits;
+    }
+
+    void EUTelTrackFinder::Reset() {
+        _trackCandidates.clear();
+	_searchResult = kFailed;
+    }
+    
+    EUTelTrackFinder::SearchResult EUTelTrackFinder::SearchTracks() {
+        
+        if( !_isReady ) {
+            streamlog_out( ERROR ) << "Track finder " << _name << std::endl;
+            streamlog_out( ERROR ) << "is not ready. ..." << std::endl;
+            _searchResult = kFailed;
+            return _searchResult;
+        }
+        
+        if( _allHits.empty() ) {
+            streamlog_out( ERROR ) << "Track finder " << _name << std::endl;
+            streamlog_out( MESSAGE4 ) << "No hits for tracks search" << std::endl;
+        }
+        
+        streamlog_out( DEBUG1 ) << "EUTelTrackFinder::SearchTracks()" << std::endl;
+        streamlog_out( DEBUG1 ) << "Looking for tracks ..." << std::endl;
+        
+        _searchResult = DoTrackSearch();
+        
+        return _searchResult;
+    }
+    
+    EUTelTrackFinder::SearchResult EUTelTrackFinder::DoTrackSearch() {
+        return kFailed;
+    }
+
+}
