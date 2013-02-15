59a66d1fa270f72c0a3472f1a584a64cfd4b2c78
Denys Lontkovskyi <denys.lontkovskyi@desy.de>
2013-02-15 12:36:54 +0000

    Add track fitter base class

---
+++ include/EUTelTrackFitter.h
@@ -0,0 +1,48 @@
+/* 
+ * File:   EUTelTrackFitter.h
+ * Contact: denys.lontkovskyi@desy.de
+ *
+ * Created on January 25, 2013, 6:55 PM
+ */
+
+#ifndef EUTELTRACKFITTER_H
+#define	EUTELTRACKFITTER_H
+
+// eutelescope includes ".h"
+#include <IMPL/TrackerHitImpl.h>
+#include <EVENT/LCCollection.h>
+#include <IMPL/LCCollectionVec.h>
+#include <IMPL/TrackerHitImpl.h>
+#include <IMPL/TrackImpl.h>
+#include <IMPL/LCFlagImpl.h>
+#include "lcio.h"
+
+// system includes <>
+#include <string>
+#include <map>
+
+namespace eutelescope {
+
+    class EUTelTrackFitter {
+    public:
+        EUTelTrackFitter();
+        EUTelTrackFitter(const EUTelTrackFitter& orig);
+
+        EUTelTrackFitter( std::string name );
+
+        virtual ~EUTelTrackFitter();
+        
+        inline void SetName( std::string name) { this->_name = name; }
+        inline std::string GetName() const { return _name; }
+
+        virtual void SetTrackCandidates( std::map< int, EVENT::TrackerHitVec >& );
+
+        virtual void FitTracks();
+    protected:
+        std::string _name;
+
+    };
+
+}
+#endif	/* EUTELTRACKFITTER_H */
+
---
+++ src/EUTelTrackFitter.cc
@@ -0,0 +1,35 @@
+/* 
+ * File:   EUTelTrackFitter.cpp
+ * Contact: diont
+ * 
+ * Created on January 25, 2013, 6:55 PM
+ */
+
+#include "EUTelTrackFitter.h"
+
+// system includes <>
+#include <string>
+
+namespace eutelescope {
+
+    EUTelTrackFitter::EUTelTrackFitter() : _name("DefaultTrackFitter") {
+    }
+
+    EUTelTrackFitter::EUTelTrackFitter(const EUTelTrackFitter& orig) {
+    }
+
+    EUTelTrackFitter::EUTelTrackFitter( std::string name ) : _name(name) {
+    }
+
+    EUTelTrackFitter::~EUTelTrackFitter() {
+    }
+
+    void EUTelTrackFitter::SetTrackCandidates( std::map< int, EVENT::TrackerHitVec>& ) {
+	return;
+    }
+
+    void EUTelTrackFitter::FitTracks() {
+	return;
+    }
+
+}
