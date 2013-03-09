245bd3ecdf4d50abc633241d84f47faf02ca756b
Igor Rubinsky <rubinsky@mail.desy.de>
2013-03-09 23:33:06 +0000

    removing comments too for allpix

--- cmake/FindALLPIX.cmake
+++ cmake/FindALLPIX.cmake
@@ -47,32 +47,16 @@ FIND_PATH( ALLPIX_INCLUDE_DIRS NAMES AllPixRun.hh *hh
 )
 
 
-#LIST( APPEND ALLPIX_INCLUDE_DIRS /home/ilcsoft/v01-15/Eutelescope/HEAD/external/allpix/include/ ${XERCESC_INCLUDE_DIRS} )
-#SET ( ALLPIX_INCLUDE_DIRS "/home/ilcsoft/v01-15/Eutelescope/HEAD/external/allpix/include/;/usr/include/xercesc/;/usr/include/xercesc/dom;/usr/include/xercesc/dom/impl;/usr/include/xercesc/parsers;/usr/include/xercesc/sax2;/usr/include/xercesc/validators;/usr/include/xercesc/validators/schema;/usr/include/xercesc/validators/schema/identity;/usr/include/xercesc/validators/DTD;/usr/include/xercesc/validators/datatype;/usr/include/xercesc/validators/common;/usr/include/xercesc/util;/usr/include/xercesc/util/MsgLoaders;/usr/include/xercesc/util/MsgLoaders/InMemory;/usr/include/xercesc/util/regx;/usr/include/xercesc/util/MutexManagers;/usr/include/xercesc/util/FileManagers;/usr/include/xercesc/util/Transcoders;/usr/include/xercesc/util/Transcoders/IconvGNU;/usr/include/xercesc/util/NetAccessors;/usr/include/xercesc/util/NetAccessors/Socket;/usr/include/xercesc/xinclude;/usr/include/xercesc/framework;/usr/include/xercesc/framework/psvi;/usr/include/xercesc/internal;/usr/include/xercesc/sax")
-
-
-
 # ---------- libraries --------------------------------------------------------
 INCLUDE( MacroCheckPackageLibs )
 
 LIST( APPEND ALLPIX_LIB_SEARCH_PATH $ENV{G4WORKDIR}/tmp/Linux-g++/allpix/  )
-#                                    /home/rubinsky/geant4_workdir/tmp/Linux-g++/allpix/liballpix.so
-#FIND_LIBRARY(ALLPIX_LIBRARIES
-#    NAMES allpix
-#    DOC "The allpix library"
-#    PATHS
-#	${ALLPIX_LIB_SEARCH_PATH}
-#    )
-
-
-
 
 # only standard libraries should be passed as arguments to CHECK_PACKAGE_LIBS
 # additional components are set by cmake in variable PKG_FIND_COMPONENTS
 # first argument should be the package name
 CHECK_PACKAGE_LIBS( ALLPIX allpix  )
 
-#SET ( ROOTLIBS "/home/ilcsoft/v01-15/root/5.32.00/lib/libXMLParser.so;/home/ilcsoft/v01-15/root/5.32.00/lib/libGeom.so;/usr/lib/libces-c-3.1.so;/home/ilcsoft/v01-15/root/5.32.00/lib/libGraf3d.so;/home/ilcsoft/v01-15/root/5.32.00/lib/libGraf.so")
 
 # ---------- final checking ---------------------------------------------------
 INCLUDE( FindPackageHandleStandardArgs )
