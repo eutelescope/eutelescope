df4659bf9e816428b056048ec859a2313dd11e2b
Igor Rubinsky <rubinsky@mail.desy.de>
2013-03-11 12:20:29 +0000

    fix search for GBL. the GBL has to be compiled beforehand with 'make install'

--- cmake/FindGBL.cmake
+++ cmake/FindGBL.cmake
@@ -25,6 +25,10 @@ IF( NOT GBL_DIR AND DEFINED ENV{GBL} )
     SET( GBL_DIR "$ENV{GBL}" )
 ENDIF()
 
+IF( NOT GBL_DIR )
+    SET( GBL_DIR "$ENV{EUTELESCOPE}/gbl/cpp/" )
+ENDIF()
+
 #MESSAGE (STATUS "gbl_home: ${GBL_HOME}")
 #MESSAGE (STATUS "env gbl_home: $ENV{GBL_HOME}")
 #MESSAGE (STATUS "env gbl: $ENV{GBL}")
@@ -32,25 +36,27 @@ ENDIF()
 
 
 
-
-
 # ---------- includes ---------------------------------------------------------
 SET( GBL_INCLUDE_DIRS GBL_INCLUDE_DIRS-NOTFOUND )
 MARK_AS_ADVANCED( GBL_INCLUDE_DIRS )
 
-FIND_PATH( GBL_INCLUDE_DIRS NAMES GblTrajectory.h include/GblTrajectory.h
-    PATHS ${GBL_DIR} ${GBL}/include
+FIND_PATH( GBL_INCLUDE_DIRS NAMES  include/GblTrajectory.h
+    PATHS ${GBL_DIR} 
     NO_DEFAULT_PATH
 )
 
+#INCLUDE_DIRECTORIES( ${GBL_INCLUDE_DIRS} )
 
+MESSAGE (STATUS "${GBL_INCLUDE_DIRS}") 
+
+EXECUTE_PROCESS( COMMAND  " cd ${GBL}  &&  make install  && cd ${EUTELESCOPE}/build" )
 
 
 # ---------- libraries --------------------------------------------------------
 INCLUDE( MacroCheckPackageLibs )
 
 # GBL library lives in bin directory rather than in lib
-LIST( APPEND GBL_LIB_SEARCH_PATH ${GBL_DIR}/lib )
+LIST( APPEND GBL_LIB_SEARCH_PATH ${GBL_DIR}/lib  ${GBL_DIR} )
 
 # only standard libraries should be passed as arguments to CHECK_PACKAGE_LIBS
 # additional components are set by cmake in variable PKG_FIND_COMPONENTS
