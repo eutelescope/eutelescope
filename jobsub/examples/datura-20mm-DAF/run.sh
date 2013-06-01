103a1e92c13c452acc8833753b7ab575a79c60e4
Igor Rubinsky <rubinsky@mail.desy.de>
2013-06-01 06:48:24 +0000

    commit example with DAF fitter for alignemnt and tracker

---
+++ jobsub/examples/datura-20mm-DAF/run.sh
@@ -0,0 +1,6 @@
+../../jobsub.py -c config.cfg -csv runlist.csv converter  33-68
+../../jobsub.py -c config.cfg -csv runlist.csv clustering 33-68
+../../jobsub.py -c config.cfg -csv runlist.csv hitmaker   33-68
+../../jobsub.py -c config.cfg -csv runlist.csv align      33-68
+../../jobsub.py -c config.cfg -csv runlist.csv fitter     33-68
+
