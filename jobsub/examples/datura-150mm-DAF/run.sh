6541503bfcbc26924c50062849f71648683ff6a3
Igor Rubinsky <rubinsky@mail.desy.de>
2013-06-11 10:32:44 +0000

    check in setup files for 150mm, differences to 20mm case are minor, keep in separate branch for now

---
+++ jobsub/examples/datura-150mm-DAF/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#export RUNRANGE="5003-5035"
+export RUNRANGE="5037-5080"
+#export RUNRANGE="5058-5080"
+
+
+#
+#../../jobsub.py -c config.cfg -csv runlist.csv converter  $RUNRANGE
+#../../jobsub.py -c config.cfg -csv runlist.csv clustering $RUNRANGE
+#../../jobsub.py -c config.cfg -csv runlist.csv hitmaker   $RUNRANGE
+../../jobsub.py -c config.cfg -csv runlist.csv align      $RUNRANGE
+../../jobsub.py -c config.cfg -csv runlist.csv fitter     $RUNRANGE
+#
