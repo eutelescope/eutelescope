e439fa1e4732930b1d9d2f8b7b93d71e365da76e
Denys Lontkovskyi <denys.lontkovskyi@desy.de>
2013-06-19 11:07:16 +0000

    Adding another version of apply alignment processor.

---
+++ include/EUTelProcessorApplyAlignment.h
@@ -0,0 +1,213 @@
+/*
+ *   This source code is part of the Eutelescope package of Marlin.
+ *   You are free to use this source files for your own development as
+ *   long as it stays in a public research context. You are not
+ *   allowed to use it for commercial purpose. You must put this
+ *   header with author names in all development based on this file.
+ *
+ */
+// built only if USE_GEAR
+#if defined(USE_GEAR)
+#ifndef EUTELPROCESSORAPPLYALIGNMENT_H
+#define EUTELPROCESSORAPPLYALIGNMENT_H
+
+// eutelescope includes ".h"
+
+// marlin includes ".h"
+#include "marlin/Processor.h"
+
+// lcio includes <.h>
+#include <IMPL/TrackerHitImpl.h>
+
+// gear includes <.h>
+#include <gear/SiPlanesParameters.h>
+#include <gear/SiPlanesLayerLayout.h>
+
+// AIDA includes <.h>
+#if defined(USE_AIDA) || defined(MARLIN_USE_AIDA)
+#include <AIDA/IBaseHistogram.h>
+#endif
+
+
+// system includes <>
+#include <iostream>
+#include <string>
+#include <map>
+
+namespace eutelescope {
+
+  //! Apply the alignment correction to the input hit collection
+  /*! The main goal of this processor is to apply the alignment
+   *  correction to the input hit collection.
+   *
+   *  The alignment constants are, as well, contained into collections
+   *  of EUTelAlignmentConstant objects and can be loaded from an
+   *  external LCIO file or from a DB query.
+   *
+   *  <h4>Input collections</h4>
+   *  <br><b>InputHit</b>.
+   *  This is a collection of TrackerHit containing all the hits that
+   *  need to be corrected for misalignment.
+   *
+   *  <br><b>AlignmentConstant</b>.
+   *  This is a collection of EUTelAlignmentConstant. This is a
+   *  LCGenericObject  containing all the needed alignment constants
+   *  calculated by previous processors.
+   *
+   *  <h4>Output</h4>
+   *
+   *  <br><b>OutputHit</b>.
+   *  This is a collection of TrackerHit with the correct hits.
+   *
+   *  @param CorrectionMethod There are actually several different
+   *  methods to apply the alignment constants. Here below a list of
+   *  available methods:
+   *     0. <b>Shifts only</b> No rotations angle will be taken into
+   *  account.
+   *     1. <b>Rotation first</b> The rotational matrix is applied
+   *  before the translation.
+   *     2. <b>Translation first</b> The hits are first shifted and
+   *  then rotated.
+   *
+   * This is a port of EUTelAPIXApplyAlignment processor
+   * 
+   *  @author Contact: antonio.bulgheroni@gmail.com
+   *  @version $Id: EUTelProcessorApplyAlignment.h 2305 2013-01-22 15:08:52Z hamnett $
+   *
+   *
+   */
+
+  class EUTelProcessorApplyAlign:public marlin::Processor {
+
+  public:
+
+
+    //! Returns a new instance of EUTelProcessorApplyAlign
+    /*! This method returns an new instance of the this processor.  It
+     *  is called by Marlin execution framework and it shouldn't be
+     *  called/used by the final user.
+     *
+     *  @return a new EUTelProcessorApplyAlign.
+     */
+    virtual Processor * newProcessor() {
+      return new EUTelProcessorApplyAlign;
+    }
+
+    //! Default constructor
+    EUTelProcessorApplyAlign ();
+
+    //! Called at the job beginning.
+    /*! This is executed only once in the whole execution. It prints
+     *  out the processor parameters and reset all needed data
+     *  members.
+     */
+    virtual void init ();
+
+    //! Called for every run.
+    /*! It is called for every run, and consequently the run counter
+     *  is incremented.
+     *
+     *  @param run the LCRunHeader of the this current run
+     */
+    virtual void processRunHeader (LCRunHeader * run);
+
+    //! Called every event
+    /*! This is called for each event in the file. A few consistency
+     *  checks are done on the first event to see if the alignment
+     *  constants and the input collection are compatible.
+     *
+     *  @param evt the current LCEvent event as passed by the
+     *  ProcessMgr
+     */
+    virtual void processEvent (LCEvent * evt);
+
+    //! Called after data processing.
+    /*! This method is called when the loop on events is finished. It
+     *  prints only a goodbye message
+     */
+    virtual void end();
+
+    int guessSensorID( lcio::TrackerHitImpl * hit ) ;
+
+  protected:
+
+    //! Input collection name.
+    /*! This is the name of the input hit collection.
+     */
+    std::string _inputHitCollectionName;
+
+    //! Alignment constant collection name
+    /*! This is the name of the collection containing the alignment
+     *  constants. This should be the results of the execution of a
+     *  EUTelMille and pede.
+     */
+    std::string _alignmentCollectionName;
+
+    //! Output collection name.
+    /*! This is the name of the output hit collection.
+     */
+    std::string _outputHitCollectionName;
+
+    //! Correction method
+    /*! There are actually several different
+     *  methods to apply the alignment constants. Here below a list of
+     *  available methods:
+     *     0. <b>Shifts only</b> No rotations angle will be taken into
+     *  account.
+     *     1. <b>Rotation first</b> The rotational matrix is applied
+     *  before the translation.
+     *     2. <b>Translation first</b> The hits are first shifted and
+     *  then rotated.
+     */
+    int _correctionMethod;
+
+    //! Current run number.
+    /*! This number is used to store the current run number
+     */
+    int _iRun;
+
+    //! Current event number.
+    /*! This number is used to store the current event number NOTE that
+     * events are counted from 0 and on a run base
+     */
+    int _iEvt;
+
+    //! Look Up Table for the sensor ID
+    std::map< int, int > _lookUpTable;
+
+    //! boolean to mark the first processed event
+    bool fevent;
+
+    //! Silicon planes parameters as described in GEAR
+    /*! This structure actually contains the following:
+     *  @li A reference to the telescope geoemtry and layout
+     *  @li An integer number saying if the telescope is w/ or w/o DUT
+     *  @li An integer number saying the number of planes in the
+     *  telescope.
+     *
+     *  This object is provided by GEAR during the init() phase and
+     *  stored here for local use.
+     */
+    gear::SiPlanesParameters * _siPlanesParameters;
+
+    //! Silicon plane layer layout
+    /*! This is the real geoemetry description. For each layer
+     *  composing the telescope the relevant information are
+     *  available.
+     *
+     *  This object is taken from the _siPlanesParameters during the
+     *  init() phase and stored for local use
+     */
+    gear::SiPlanesLayerLayout * _siPlanesLayerLayout;
+
+    //! An array with the Z position of planes
+    double * _siPlaneZPosition;
+
+  };
+
+  //! A global instance of the processor
+  EUTelProcessorApplyAlign gEUTelProcessorApplyAlign;
+
+}
+#endif
+#endif // GEAR
---
+++ src/EUTelProcessorApplyAlignment.cc
@@ -0,0 +1,246 @@
+// Author Antonio Bulgheroni, INFN <mailto:antonio.bulgheroni@gmail.com>
+// Version $Id: EUTelProcessorApplyAlignment.cc 2367 2013-02-12 15:41:08Z hperrey $
+/*
+ *   This source code is part of the Eutelescope package of Marlin.
+ *   You are free to use this source files for your own development as
+ *   long as it stays in a public research context. You are not
+ *   allowed to use it for commercial purpose. You must put this
+ *   header with author names in all development based on this file.
+ *
+ */
+ 
+#ifdef USE_GEAR
+// eutelescope includes ".h"
+#include "EUTelProcessorApplyAlignment.h"
+#include "EUTelRunHeaderImpl.h"
+#include "EUTelEventImpl.h"
+#include "EUTelAlignmentConstant.h"
+
+// marlin includes ".h"
+#include "marlin/Processor.h"
+#include "marlin/Exceptions.h"
+
+// lcio includes <.h>
+#include <IMPL/TrackerHitImpl.h>
+#include <IMPL/TrackerDataImpl.h>
+#include <IMPL/LCCollectionVec.h>
+#include <UTIL/CellIDDecoder.h>
+#include <UTIL/CellIDEncoder.h>
+
+#if defined(USE_AIDA) || defined(MARLIN_USE_AIDA)
+// aida includes
+#include <AIDA/IHistogram3D.h>
+#include <AIDA/IHistogram2D.h>
+#include <AIDA/ITree.h>
+#include <AIDA/IHistogramFactory.h>
+#include <marlin/AIDAProcessor.h>
+#endif
+
+// gear includes <.h>
+#include "marlin/Global.h"
+#include <gear/GearMgr.h>
+#include <gear/SiPlanesParameters.h>
+
+// system includes <>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+#include <algorithm>
+#include <memory>
+
+#include "TVector3.h"
+
+using namespace std;
+using namespace lcio;
+using namespace marlin;
+using namespace eutelescope;
+using namespace gear;
+
+EUTelProcessorApplyAlign::EUTelProcessorApplyAlign () :Processor("EUTelProcessorApplyAlignment") {
+  _description = "Apply alignment constants to hit collection";
+
+  registerInputCollection (LCIO::TRACKERHIT, "InputHitCollectionName",
+                           "The name of the input hit collection",
+                           _inputHitCollectionName, string ("hit"));
+  registerInputCollection (LCIO::LCGENERICOBJECT, "AlignmentConstantName",
+                           "Alignment constant from the condition file",
+                           _alignmentCollectionName, string ("alignment"));
+  registerOutputCollection (LCIO::TRACKERHIT, "OutputHitCollectionName",
+                            "The name of the output hit collection",
+                            _outputHitCollectionName, string("correctedHit"));
+}
+
+
+void EUTelProcessorApplyAlign::init () {
+  // this method is called only once even when the rewind is active
+  printParameters ();
+
+  // set to zero the run and event counters
+  _iRun = 0;
+  _iEvt = 0;
+
+  // check if the GEAR manager pointer is not null!
+  if ( Global::GEAR == NULL ) {
+    streamlog_out ( ERROR4 ) <<  "The GearMgr is not available, for an unknown reason." << endl;
+    exit(-1);
+  }
+
+  _siPlanesParameters  = const_cast<SiPlanesParameters* > (&(Global::GEAR->getSiPlanesParameters()));
+  _siPlanesLayerLayout = const_cast<SiPlanesLayerLayout*> ( &(_siPlanesParameters->getSiPlanesLayerLayout() ));
+
+  _siPlaneZPosition = new double[ _siPlanesLayerLayout->getNLayers() ];
+  for ( int iPlane = 0 ; iPlane < _siPlanesLayerLayout->getNLayers(); iPlane++ ) {
+    _siPlaneZPosition[ iPlane ] = _siPlanesLayerLayout->getLayerPositionZ(iPlane);
+  }
+
+  _isFirstEvent = true;
+  fevent = true;
+}
+
+void EUTelProcessorApplyAlign::processRunHeader (LCRunHeader * rdr) {
+  auto_ptr<EUTelRunHeaderImpl> runHeader ( new EUTelRunHeaderImpl( rdr ) ) ;
+  runHeader->addProcessor( type() );
+  ++_iRun;
+}
+
+void EUTelProcessorApplyAlign::processEvent (LCEvent * event) {
+  ++_iEvt;
+  if ( _iEvt % 10000 == 0 )
+    streamlog_out ( MESSAGE4 ) << "Processing event "
+                               << setw(6) << setiosflags(ios::right) << event->getEventNumber() << " in run "
+                               << setw(6) << setiosflags(ios::right) << setfill('0')  << event->getRunNumber()
+                               << setfill(' ')
+                               << " (Total = " << setw(10) << _iEvt << ")" << resetiosflags(ios::left) << endl;
+
+  EUTelEventImpl * evt = static_cast<EUTelEventImpl*> (event);
+
+  if ( evt->getEventType() == kEORE ) {
+    streamlog_out ( DEBUG4 ) << "EORE found: nothing else to do." << endl;
+    return;
+  } else if ( evt->getEventType() == kUNKNOWN ) {
+    streamlog_out ( WARNING2 ) << "Event number " << evt->getEventNumber() << " in run " << evt->getRunNumber()
+                               << " is of unknown type. Continue considering it as a normal Data Event." << endl;
+  }
+
+  try {
+    LCCollectionVec * inputCollectionVec         = dynamic_cast < LCCollectionVec * > (evt->getCollection(_inputHitCollectionName));
+    LCCollectionVec * alignmentCollectionVec     = dynamic_cast < LCCollectionVec * > (evt->getCollection(_alignmentCollectionName));
+    
+    if (fevent) {
+      streamlog_out ( MESSAGE5 ) << "The alignment collection contains: " <<  alignmentCollectionVec->size()
+                                << " planes " << endl;
+      
+      for ( size_t iPos = 0; iPos < alignmentCollectionVec->size(); ++iPos ) {
+        EUTelAlignmentConstant * alignment = static_cast< EUTelAlignmentConstant * > ( alignmentCollectionVec->getElementAt( iPos ) );
+        _lookUpTable[ alignment->getSensorID() ] = iPos;
+      }
+
+      map< int , int >::iterator mapIter = _lookUpTable.begin();
+      for(;mapIter != _lookUpTable.end(); mapIter++){
+        streamlog_out ( MESSAGE5 ) << "Sensor ID = " << mapIter->first
+                                << " is in position " << mapIter->second << endl;
+      }
+      _isFirstEvent = false;
+      fevent = false;
+    }
+
+    LCCollectionVec * outputCollectionVec = new LCCollectionVec(LCIO::TRACKERHIT);
+
+    for (size_t iHit = 0; iHit < inputCollectionVec->size(); iHit++) {
+      TrackerHitImpl   * inputHit   = dynamic_cast< TrackerHitImpl * >  ( inputCollectionVec->getElementAt( iHit ) ) ;
+      // now we have to understand which layer this hit belongs to.
+      int sensorID = guessSensorID( inputHit );
+
+      // copy the input to the output, at least for the common part
+      TrackerHitImpl   * outputHit  = new TrackerHitImpl;
+      outputHit->setType( inputHit->getType() );
+      outputHit->rawHits() = inputHit->getRawHits();
+      FloatVec cov;
+      cov = inputHit->getCovMatrix();
+      outputHit->setCovMatrix( cov );
+
+      // now that we know at which sensor the hit belongs to, we can
+      // get the corresponding alignment constants
+      map< int , int >::iterator  positionIter = _lookUpTable.find( sensorID );
+
+      double * inputPosition      = const_cast< double * > ( inputHit->getPosition() ) ;
+      double   outputPosition[3]  = { inputPosition[0],inputPosition[1], inputPosition[2] };
+
+      if ( positionIter != _lookUpTable.end() ) {
+        EUTelAlignmentConstant * alignment = static_cast< EUTelAlignmentConstant * >
+          ( alignmentCollectionVec->getElementAt( positionIter->second ) );
+	// Rotations
+        TVector3 inputVec( inputPosition[0], inputPosition[1], 0. );
+        inputVec.RotateZ( -alignment->getGamma() );
+        inputVec.RotateX( -alignment->getAlpha() );
+        inputVec.RotateY( -alignment->getBeta() );
+        outputPosition[0] = inputVec.X();
+        outputPosition[1] = inputVec.Y();
+	// Shifts
+	outputPosition[0] -= alignment->getXOffset();
+	outputPosition[1] -= alignment->getYOffset();
+	outputPosition[2] -= alignment->getZOffset();
+
+	streamlog_out( DEBUG5 ) << "position: " 
+			       << inputPosition[0] << "," 
+			       << inputPosition[1] << ","
+			       << inputPosition[2] << " -> "
+			       << outputPosition[0] << "," 
+			       << outputPosition[1] << ","
+			       << outputPosition[2] << endl;
+      } else {
+        // this hit belongs to a plane whose sensorID is not in the
+        // alignment constants. So the idea is to eventually advice
+        // the users if running in DEBUG and copy the not aligned hit
+        // in the new collection.
+        streamlog_out ( DEBUG5 ) << "Sensor ID " << sensorID << " not found. Skipping alignment for hit " << iHit << endl;
+      }
+      outputHit->setPosition( outputPosition );
+      outputCollectionVec->push_back( outputHit );
+    }
+    evt->addCollection( outputCollectionVec, _outputHitCollectionName );
+  } catch (DataNotAvailableException& e) {
+    streamlog_out  ( WARNING2 ) <<  "No input collection " << _inputHitCollectionName << " found on event " << event->getEventNumber()
+                                << " in run " << event->getRunNumber() << endl;
+  }
+
+}
+
+void EUTelProcessorApplyAlign::end() {
+  streamlog_out ( MESSAGE2 ) <<  "Successfully finished" << endl;
+  delete [] _siPlaneZPosition;
+}
+
+int EUTelProcessorApplyAlign::guessSensorID( TrackerHitImpl * hit ) {
+  int sensorID = -1;
+  double minDistance =  numeric_limits< double >::max() ;
+  double * hitPosition = const_cast<double * > (hit->getPosition());
+
+  for ( int iPlane = 0 ; iPlane < _siPlanesLayerLayout->getNLayers(); ++iPlane ) {
+    double distance = std::abs( hitPosition[2] - _siPlaneZPosition[ iPlane ] );
+    if ( distance < minDistance ) {
+      minDistance = distance;
+      sensorID = _siPlanesLayerLayout->getID( iPlane );
+    }
+  }
+  if  ( _siPlanesParameters->getSiPlanesType() == _siPlanesParameters->TelescopeWithDUT ) {
+    double distance = std::abs( hitPosition[2] - _siPlanesLayerLayout->getDUTPositionZ() );
+    if( distance < minDistance )
+      {
+        minDistance = distance;
+        sensorID = _siPlanesLayerLayout->getDUTID();
+      }
+  }
+  if ( minDistance > 5 /* mm */ ) {
+    // advice the user that the guessing wasn't successful 
+    streamlog_out( WARNING3 ) << "A hit was found " << minDistance << " mm far from the nearest plane\n"
+      "Please check the consistency of the data with the GEAR file " << endl;
+    throw SkipEventException(this);
+  }
+  return sensorID;
+}
+
+
+#endif
