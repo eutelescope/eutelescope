259cdf3743642caf1937e7fb0cd4903843f36c36
Thomas Eichhorn <thomas.eichhorn@desy.de>
2013-06-28 13:26:43 +0000

    Cleanup. The datura-noDUT example should now run for 20mm and 150mm geometries with all 3 current alignment methods "out of the box".

--- jobsub/examples/datura-noDUT/run.sh
+++ jobsub/examples/datura-noDUT/run.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 
-first="233"
-last="5080"
+first="97"
+last="97"
 
-#modus="straight"
+modus="straight"
 #modus="daf"
-modus="gbl"
+#modus="gbl"
+
 
 for i in `seq $first $last`; do
 
-../../jobsub.py -c config.cfg -csv runlist.csv converter  $i
-../../jobsub.py -c config.cfg -csv runlist.csv clustering $i
-../../jobsub.py -c config.cfg -csv runlist.csv hitmaker   $i
+jobsub.py -c config.cfg -csv runlist.csv converter  $i
+jobsub.py -c config.cfg -csv runlist.csv clustering $i
+jobsub.py -c config.cfg -csv runlist.csv hitmaker   $i
 
 if [[ $modus == "straight" ]]; then
-  ../../jobsub.py -c config.cfg -csv runlist.csv align      $i
-  ../../jobsub.py -c config.cfg -csv runlist.csv fitter     $i
+  jobsub.py -c config.cfg -csv runlist.csv align      $i
+  jobsub.py -c config.cfg -csv runlist.csv fitter     $i
 
 elif [[ $modus == "daf" ]]; then
-  ../../jobsub.py -c config.cfg -csv runlist.csv aligndaf   $i
-  ../../jobsub.py -c config.cfg -csv runlist.csv fitter     $i
+  jobsub.py -c config.cfg -csv runlist.csv aligndaf   $i
+  jobsub.py -c config.cfg -csv runlist.csv fitter     $i
 
 elif [[ $modus == "gbl" ]]; then
-  ../../jobsub.py -c config.cfg -csv runlist.csv tracksearch $i
-  ../../jobsub.py -c config.cfg -csv runlist.csv aligngbl $i
-  sh ../../../tools/parsepede/parsemilleout.sh output/database/run00$i-pede-steer.txt millepede.res output/database/run00$i-align.slcio
-  ../../jobsub.py -c config.cfg -csv runlist.csv trackfit $i
+  jobsub.py -c config.cfg -csv runlist.csv tracksearch $i
+  jobsub.py -c config.cfg -csv runlist.csv aligngbl $i
+  jobsub.py -c config.cfg -csv runlist.csv trackfit $i
+  jobsub.py -c config.cfg -csv runlist.csv fitter $i
 
 fi
 done
