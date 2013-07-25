e6e4f0a6177edf676db7b4d869c5c38cb70baefc
Denys Lontkovskyi <denys.lontkovskyi@desy.de>
2013-07-25 22:24:30 +0000

    Added string tokenizer function

--- include/EUTelUtility.h
+++ include/EUTelUtility.h
@@ -205,6 +205,34 @@ namespace eutelescope {
         /** Solve quadratic equation a*x^2 + b*x + c = 0 */
         std::vector< double > solveQuadratic( double, double, double );
         
+        /** Tokenize string */
+                /**
+         * String tokenizer
+         * @param source
+         * @param delimiter
+         * @param keepEmpty
+         * @return vector of tokens
+         */
+        std::vector<std::string> inline stringSplit(const std::string& source, const char* delimiter = " ", bool keepEmpty = false ) {
+            std::vector<std::string> results;
+
+            size_t prev = 0;
+            size_t next = 0;
+
+            while ( ( next = source.find_first_of( delimiter, prev ) ) != std::string::npos ) {
+                if ( keepEmpty || ( next - prev != 0 ) ) {
+                    results.push_back( source.substr( prev, next - prev ) );
+                }
+                prev = next + 1;
+            }
+
+            if ( prev < source.size( ) ) {
+                results.push_back( source.substr( prev ) );
+            }
+
+            return results;
+        }
+        
         /** Possible choices of alignment degrees of freedom */
         enum AlignmentMode {
             noAlignment, XYShift, XYShiftXYRot, XYZShiftXYRot, XYShiftYZRotXYRot, XYShiftXZRotXYRot, XYShiftXZRotYZRotXYRot, XYZShiftXZRotYZRotXYRot
