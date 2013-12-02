cfd1958c7b426b8347c92ce8ced4f4279c2c1192
Igor Rubinsky <rubinsky@mail.desy.de>
2013-12-02 16:18:35 +0000

    update example run.sh script to manage two different tracksearch processors / hitmaker replaced by hitlocal for GBL

--- jobsub/examples/datura-noDUT/run.sh
+++ jobsub/examples/datura-noDUT/run.sh
@@ -1,33 +1,41 @@
 #!/bin/sh
 
-first="97"
-last="97"
+first="36"
+last="36"
 
-modus="straight"
+#modus="straight"
 #modus="daf"
-#modus="gbl"
+modus="gbl"
 
+DRY=--dry-run
+RUNLIST="runlist-20.csv"
 
 for i in `seq $first $last`; do
 
-jobsub.py -c config.cfg -csv runlist.csv converter  $i
-jobsub.py -c config.cfg -csv runlist.csv clustering $i
-jobsub.py -c config.cfg -csv runlist.csv filter $i
-jobsub.py -c config.cfg -csv runlist.csv hitmaker   $i
+#jobsub.py $DRY -c config.cfg -csv $RUNLIST converter  $i
+#jobsub.py  $DRY -c config.cfg -csv $RUNLIST clustering $i
+#jobsub.py  $DRY -c config.cfg -csv $RUNLIST filter $i
 
 if [[ $modus == "straight" ]]; then
-  jobsub.py -c config.cfg -csv runlist.csv align      $i
-  jobsub.py -c config.cfg -csv runlist.csv fitter     $i
+ jobsub.py  $DRY -c config.cfg -csv $RUNLIST hitmaker   $i
+# alignment using straight line assumption
+ jobsub.py -c config.cfg -csv $RUNLIST align      $i
+# fitter using broken line implementation by F.Zarnezki
+ jobsub.py -c config.cfg -csv $RUNLIST fitter     $i
 
 elif [[ $modus == "daf" ]]; then
-  jobsub.py -c config.cfg -csv runlist.csv aligndaf   $i
-  jobsub.py -c config.cfg -csv runlist.csv fitter     $i
+ jobsub.py  $DRY -c config.cfg -csv $RUNLIST hitmaker   $i
+ jobsub.py -c config.cfg -csv $RUNLIST aligndaf   $i
+ jobsub.py -c config.cfg -csv $RUNLIST fitterdaf  $i
 
 elif [[ $modus == "gbl" ]]; then
-  jobsub.py -c config.cfg -csv runlist.csv tracksearch $i
-  jobsub.py -c config.cfg -csv runlist.csv aligngbl $i
-  jobsub.py -c config.cfg -csv runlist.csv trackfit $i
-  jobsub.py -c config.cfg -csv runlist.csv fitter $i
+# FOR NEW gbl ONE NEEDS TO GET HITS IN LOCAL COORDINATE SYSTEM:
+ jobsub.py  $DRY -c config.cfg -csv $RUNLIST hitlocal $i
+# Exhautsive and Helix track search results are not identical - to be investigated (perhaps trivially explained)
+# jobsub.py  $DRY -c config.cfg -csv $RUNLIST tracksearchExh $i
+ jobsub.py  $DRY -c config.cfg -csv $RUNLIST tracksearchHelix $i
+ jobsub.py  $DRY -c config.cfg -csv $RUNLIST aligngbl $i
+# jobsub.py  $DRY -c config.cfg -csv $RUNLIST trackgbl $i
 
 fi
 done
