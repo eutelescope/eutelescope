5082edd2fca7b1410ec172195692d0b234420826
Igor Rubinsky <rubinsky@mail.desy.de>
2013-12-03 18:28:47 +0000

    add DRY run steering file dump for intermediate reco steps; rename fitterdaf to trackdaf;

--- jobsub/examples/datura-noDUT/run.sh
+++ jobsub/examples/datura-noDUT/run.sh
@@ -19,23 +19,23 @@ jobsub.py  $DRY -c config.cfg -csv $RUNLIST filter $i
 if [[ $modus == "straight" ]]; then
  jobsub.py  $DRY -c config.cfg -csv $RUNLIST hitmaker   $i
 # alignment using straight line assumption
- jobsub.py -c config.cfg -csv $RUNLIST align      $i
+ jobsub.py $DRY -c config.cfg -csv $RUNLIST align      $i
 # fitter using broken line implementation by F.Zarnezki
- jobsub.py -c config.cfg -csv $RUNLIST fitter     $i
+ jobsub.py $DRY -c config.cfg -csv $RUNLIST fitter     $i
 
 elif [[ $modus == "daf" ]]; then
- jobsub.py  $DRY -c config.cfg -csv $RUNLIST hitmaker   $i
- jobsub.py -c config.cfg -csv $RUNLIST aligndaf   $i
- jobsub.py -c config.cfg -csv $RUNLIST fitterdaf  $i
+ jobsub.py $DRY -c config.cfg -csv $RUNLIST hitmaker   $i
+ jobsub.py $DRY -c config.cfg -csv $RUNLIST aligndaf   $i
+ jobsub.py $DRY -c config.cfg -csv $RUNLIST trackdaf  $i
 
 elif [[ $modus == "gbl" ]]; then
 # FOR NEW gbl ONE NEEDS TO GET HITS IN LOCAL COORDINATE SYSTEM:
  jobsub.py  $DRY -c config.cfg -csv $RUNLIST hitlocal $i
 # Exhautsive and Helix track search results are not identical - to be investigated (perhaps trivially explained)
-# jobsub.py  $DRY -c config.cfg -csv $RUNLIST tracksearchExh $i
+ jobsub.py  $DRY -c config.cfg -csv $RUNLIST tracksearchExh $i
  jobsub.py  $DRY -c config.cfg -csv $RUNLIST tracksearchHelix $i
  jobsub.py  $DRY -c config.cfg -csv $RUNLIST aligngbl $i
-# jobsub.py  $DRY -c config.cfg -csv $RUNLIST trackgbl $i
+ jobsub.py  $DRY -c config.cfg -csv $RUNLIST trackgbl $i
 
 fi
 done
