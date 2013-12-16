172166a13da9e9b7d4d55a126f90ed88dbdf05e5
Igor Rubinsky <rubinsky@mail.desy.de>
2013-12-16 14:54:10 +0000

    resume merging with Denys's branch: alter TrackCandidates to const EVENT::TrackVec&

--- include/EUTelTrackFitter.h
+++ include/EUTelTrackFitter.h
@@ -41,8 +41,7 @@ namespace eutelescope {
         inline void SetName( std::string name) { this->_name = name; }
         inline std::string GetName() const { return _name; }
 
-//        virtual void SetTrackCandidates( const EVENT::TrackVec& );
-        virtual void SetTrackCandidates( std::vector< EVENT::TrackerHitVec>& );
+        virtual void SetTrackCandidates( const EVENT::TrackVec& );
 
 
         virtual void FitTracks();
