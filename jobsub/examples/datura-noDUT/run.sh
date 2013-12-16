983e379d5e51957f0599b55b935d20016ced6632
Igor Rubinsky <rubinsky@mail.desy.de>
2013-12-16 14:51:50 +0000

    add few comments to the run.sh script; do not use Exhaustive TrackSearch processor in GBL section - use only the Helix type one

--- jobsub/examples/datura-noDUT/run.sh
+++ jobsub/examples/datura-noDUT/run.sh
@@ -9,12 +9,13 @@ modus="gbl"
 
 #DRY=--dry-run
 RUNLIST="runlist-150.csv"
+#RUNLIST="runlist-20.csv"
 
 for i in `seq $first $last`; do
 
-jobsub.py $DRY -c config.cfg -csv $RUNLIST converter  $i
-jobsub.py  $DRY -c config.cfg -csv $RUNLIST clustering $i
-jobsub.py  $DRY -c config.cfg -csv $RUNLIST filter $i
+ jobsub.py $DRY -c config.cfg -csv $RUNLIST converter  $i
+ jobsub.py  $DRY -c config.cfg -csv $RUNLIST clustering $i
+ jobsub.py  $DRY -c config.cfg -csv $RUNLIST filter $i
 
 if [[ $modus == "straight" ]]; then
  jobsub.py $DRY -c config.cfg -csv $RUNLIST hitmaker   $i
@@ -32,8 +33,9 @@ elif [[ $modus == "gbl" ]]; then
 # FOR NEW gbl ONE NEEDS TO GET HITS IN LOCAL COORDINATE SYSTEM:
  jobsub.py  $DRY -c config.cfg -csv $RUNLIST hitlocal $i
 # Exhautsive and Helix track search results are not identical - to be investigated (perhaps trivially explained)
- jobsub.py  $DRY -c config.cfg -csv $RUNLIST tracksearchExh $i
+# jobsub.py  $DRY -c config.cfg -csv $RUNLIST tracksearchExh $i
  jobsub.py  $DRY -c config.cfg -csv $RUNLIST tracksearchHelix $i
+# echo jobsub.py  $DRY -c config.cfg -csv $RUNLIST aligngbl $i
  jobsub.py  $DRY -c config.cfg -csv $RUNLIST aligngbl $i
  jobsub.py  $DRY -c config.cfg -csv $RUNLIST trackgbl $i
 
