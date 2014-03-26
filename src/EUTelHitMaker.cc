46b048bb0e0ceeae2f9c05ed6c6f552b6a29946c
Hanno Perrey <hanno.perrey@desy.de>
2014-03-26 20:25:05 +0100

    Revert "hitmaker now adds sensorID and properties fields"
    This reverts commit 6e9e440015fa4f50743f9149626c389e5759f26d.
    This was a premature accidental commit. Sorry.

--- src/EUTelHitMaker.cc
+++ src/EUTelHitMaker.cc
@@ -384,6 +384,7 @@ void EUTelHitMaker::processEvent (LCEvent * event) {
 
    ++_iEvt;
 
+
   EUTelEventImpl * evt = static_cast<EUTelEventImpl*> (event) ;
 
   if ( evt->getEventType() == kEORE ) {
@@ -481,9 +482,6 @@ void EUTelHitMaker::processEvent (LCEvent * event) {
        hitCollection = new LCCollectionVec(LCIO::TRACKERHIT);
     }
   
-    // prepare an encoder for the hit collection
-    CellIDEncoder<TrackerHitImpl> idHitEncoder(EUTELESCOPE::HITENCODING, hitCollection);
-
     CellIDDecoder<TrackerPulseImpl>  pulseCellDecoder(pulseCollection);
 
     int detectorID    = -99; // it's a non sense
@@ -1004,15 +1002,9 @@ void EUTelHitMaker::processEvent (LCEvent * event) {
       // add the clusterVec to the hit
       hit->rawHits() = clusterVec;
       
-      // Determine sensorID from the cluster data.
-      idHitEncoder["sensorID"] = pulseCellDecoder(pulse)["sensorID"];
-
-      // set the local/global bit flag property for the hit
-      idHitEncoder["properties"] = 0; // init
-      if (!_wantLocalCoordinates) idHitEncoder["properties"] = kHitInGlobalCoord;
-
-      // store values
-      idHitEncoder->setCellID( hit );
+      // Determine sensorID from raw data.
+      int sensorID = Utility::GuessSensorID( hit );
+      hit->setCellID0( sensorID );
 
       // add the new hit to the hit collection
       hitCollection->push_back( hit );
