2881b33979acae5900df96c72c0b7d6f6d9c572f
Igor Rubinskiy <igor.rubinskiy@desy.de>
2014-05-27 11:38:59 +0200

    remove commented kEUTelAPIXSparsePixel

--- include/EUTELESCOPE.h
+++ include/EUTELESCOPE.h
@@ -600,7 +600,6 @@ namespace eutelescope
     kEUTelSimpleSparsePixel = 1,
     kEUTelGenericSparsePixel = 2,
     kEUTelGeometricPixel = 3,
-//    kEUTelAPIXSparsePixel = 4,
     // add here your implementation
     kUnknownPixelType       = 31
   };
