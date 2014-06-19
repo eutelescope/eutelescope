3fec7b247fb33c160e2a03f2c5d15c1bf0939fdc
Igor Rubinskiy <igor.rubinskiy@desy.de>
2014-06-19 18:45:41 +0200

    only one line changed! every thing code alignment. now, every event have to reset hit and track vectors of the base class.
    +        _trackFitter->Clear();

--- src/EUTelProcessorGBLFitCandidates.cpp
+++ src/EUTelProcessorGBLFitCandidates.cpp
@@ -39,24 +39,22 @@ void EUTelProcessorGBLFitCandidates::init() {
 	_nProcessedRuns = 0;
 	_nProcessedEvents = 0;
 
-
 	// Getting access to geometry description
 	std::string name("test.root");
 	geo::gGeometry().initializeTGeoDescription(name,false);
 
 	// Initialize GBL fitter
 	EUTelGBLFitter* Fitter = new EUTelGBLFitter("GBLFitter");
-  Fitter->SetBeamCharge(_beamQ);
-  Fitter->SetBeamEnergy(_eBeam);
-  _trackFitter = Fitter;
-
+	Fitter->SetBeamCharge(_beamQ);
+	Fitter->SetBeamEnergy(_eBeam);
+	_trackFitter = Fitter;
 
 	if (!_trackFitter) {
 		streamlog_out(ERROR) << "Can't allocate an instance of EUTelGBLFitter. Stopping ..." << std::endl;
 		throw UnknownDataTypeException("Track finder was not created");
 	}
 
-    streamlog_out(DEBUG2) << "EUTelProcessorGBLFitCandidates::init( )---------------------------------------------END" << std::endl;
+	streamlog_out(DEBUG2) << "EUTelProcessorGBLFitCandidates::init( )---------------------------------------------END" << std::endl;
 
 
 }
@@ -64,23 +62,23 @@ void EUTelProcessorGBLFitCandidates::init() {
 void EUTelProcessorGBLFitCandidates::processRunHeader(LCRunHeader * run) {
 
 	auto_ptr<EUTelRunHeaderImpl> header(new EUTelRunHeaderImpl(run));
-  header->addProcessor(type());
+	header->addProcessor(type());
 
 
 	// this is the right place also to check the geometry ID. This is a
-  // unique number identifying each different geometry used at the
-  // beam test. The same number should be saved in the run header and
+  	// unique number identifying each different geometry used at the
+  	// beam test. The same number should be saved in the run header and
  	// in the xml file. If the numbers are different, warn the user.
 
 	if (header->getGeoID() == 0)
  		streamlog_out(WARNING0) << "The geometry ID in the run header is set to zero." << endl << "This may mean that the GeoID parameter was not set" << endl;
 
 
-  if (header->getGeoID() != geo::gGeometry().getSiPlanesLayoutID()) {  
+  	if (header->getGeoID() != geo::gGeometry().getSiPlanesLayoutID()) {  
 		streamlog_out(WARNING5) << "Error during the geometry consistency check: " << endl << "The run header says the GeoID is " << header->getGeoID() << endl << "The GEAR description says is     " << geo::gGeometry().getSiPlanesLayoutID() << endl;
-  }
+  	}
     
-    _nProcessedRuns++;
+    	_nProcessedRuns++;
 
 }
 
@@ -92,43 +90,46 @@ void EUTelProcessorGBLFitCandidates::processEvent(LCEvent * evt){
 
 	
 	//////////////////////////////////////////////////////////////////////// Do not process last events
-  if (event->getEventType() == kEORE) {
-  	streamlog_out(DEBUG4) << "EORE found: nothing else to do." << endl;
-  	return;
-  }else if (event->getEventType() == kUNKNOWN) {
-  	streamlog_out(WARNING2) << "Event number " << event->getEventNumber() << " in run " << event->getRunNumber() << " is of unknown type. Continue considering it as a normal Data Event." << endl;
-  }
+  	if (event->getEventType() == kEORE) {
+  		streamlog_out(DEBUG4) << "EORE found: nothing else to do." << endl;
+  		return;
+  	}else if (event->getEventType() == kUNKNOWN) {
+  		streamlog_out(WARNING2) << "Event number " << event->getEventNumber() << " in run " << event->getRunNumber() << " is of unknown type. Continue considering it as a normal Data Event." << endl;
+  	}
 	////////////////////////////////////////////////////////////////////////
 	
 	//////////////////////////////////////////////////////////////////////////Try to access collection	
-  LCCollection* col = NULL;
+  	LCCollection* col = NULL;
 	try {
-  	col = evt->getCollection(_trackCandidatesInputCollectionName);
-    streamlog_out(DEBUG1) << "collection : " << _trackCandidatesInputCollectionName << " retrieved" << std::endl;
-  } catch (DataNotAvailableException e) {
-  	streamlog_out(MESSAGE0) << _trackCandidatesInputCollectionName << " collection not available" << std::endl;
-    throw marlin::SkipEventException(this);
-  }
+  		col = evt->getCollection(_trackCandidatesInputCollectionName);
+    		streamlog_out(DEBUG1) << "collection : " << _trackCandidatesInputCollectionName << " retrieved" << std::endl;
+  	} catch (DataNotAvailableException e) {
+  		streamlog_out(MESSAGE0) << _trackCandidatesInputCollectionName << " collection not available" << std::endl;
+    		throw marlin::SkipEventException(this);
+  	}
 	///////////////////////////////////////////////////////////////////////////////
 
+	//This is a good point to clear all things that need to be reset for each event. Why should gop here?
+        _trackFitter->Clear(); 
+
+
 	// this will only be entered if the collection is available
-  if (col != NULL) {
-  	streamlog_out(DEBUG2) << "Collection contains data! Continue!" << endl;
+  	if (col != NULL) {
+  		streamlog_out(DEBUG2) << "Collection contains data! Continue!" << endl;
 
 		////////////////////////////////////////////////////////////////////////////For the each event we get loop over all track candidates and fit them	
-    for (int iCol = 0; iCol < col->getNumberOfElements(); iCol++) {
+		for (int iCol = 0; iCol < col->getNumberOfElements(); iCol++) {
 			
-    	if (!col) {
-      	streamlog_out(WARNING2) << "Track collection not found found for event " << _nProcessedEvents << " in run " << _nProcessedRuns << endl;
-        throw SkipEventException(this);
-      }
-    	IMPL::TrackImpl* trackimpl = static_cast<IMPL::TrackImpl*> (col->getElementAt(iCol));
+			if (!col) {
+		      		streamlog_out(WARNING2) << "Track collection not found found for event " << _nProcessedEvents << " in run " << _nProcessedRuns << endl;
+		        	throw SkipEventException(this);
+     			}
+	   		IMPL::TrackImpl* trackimpl = static_cast<IMPL::TrackImpl*> (col->getElementAt(iCol));
 			EUTelTrackImpl* EUtrack;
 			CreateEUTrackandStates(trackimpl,EUtrack);
-      streamlog_out(DEBUG1) << "Track " << iCol << " nhits " << trackimpl->getTrackerHits().size() << endl;
+      			streamlog_out(DEBUG1) << "Track " << iCol << " nhits " << trackimpl->getTrackerHits().size() << endl;
 
-			//_trackFitter->Clear(); //This is a good point to clear all things that need to be reset for each event. Why should gop here?
-      _trackFitter->FillInformationToGBLPointObject(EUtrack);      
+	      		_trackFitter->FillInformationToGBLPointObject(EUtrack);      
 
 		}//END OF LOOP FOR ALL TRACKS IN AN EVENT
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -144,10 +145,10 @@ void EUTelProcessorGBLFitCandidates::end() {}
 /////////////////////////////////////////////////////Functions
 void EUTelProcessorGBLFitCandidates::CreateEUTrackandStates(TrackImpl* trackimpl, EUTelTrackImpl* EUtrack){
 	
-  for(int i=0;i < trackimpl->getTrackStates().size(); i++){
+  	for(int i=0;i < trackimpl->getTrackStates().size(); i++){
 		EUTelTrackStateImpl *EUstate = new EUTelTrackStateImpl;
 
-  	IMPL::TrackStateImpl* state = static_cast < IMPL::TrackStateImpl*> ( trackimpl->getTrackStates().at(i) ) ;
+  		IMPL::TrackStateImpl* state = static_cast < IMPL::TrackStateImpl*> ( trackimpl->getTrackStates().at(i) ) ;
 		EUstate->setX(state->getD0()); //x position global
 		EUstate->setY(state->getPhi()); //y position global
 		EUstate->setTx(state->getOmega()); //tx position global
@@ -163,13 +164,12 @@ void EUTelProcessorGBLFitCandidates::CreateEUTrackandStates(TrackImpl* trackimpl
 	}
 
    	// Assign hits to LCIO TRACK
-    const EVENT::TrackerHitVec& trkcandhits = trackimpl->getTrackerHits();
-    EVENT::TrackerHitVec::const_iterator itrHit;
-    for ( itrHit = trkcandhits.begin(); itrHit != trkcandhits.end(); ++itrHit ){
-    	EUtrack->addHit( *itrHit );
-    }
+    	const EVENT::TrackerHitVec& trkcandhits = trackimpl->getTrackerHits();
+    	EVENT::TrackerHitVec::const_iterator itrHit;
+    	for ( itrHit = trkcandhits.begin(); itrHit != trkcandhits.end(); ++itrHit ){
+    		EUtrack->addHit( *itrHit );
+    	}
 
-	
 }
 
 
