d26f10a8046a6d8ba570bd783cb68300563d6500
Alexander Morton <alexander.morton@desy.de>
2014-07-03 13:32:23 +0200

    Add simple class template for users to begin from

---
+++ include/EUTelClassTemplate.h
@@ -0,0 +1,15 @@
+#include "EUTelUtility.h"
+
+namespace eutelescope {
+
+	class EUTelClassTemplate{
+        
+		private:
+  	DISALLOW_COPY_AND_ASSIGN(EUTelClassTemplate)        // prevent users from making (default) copies of processors
+      
+		public:
+		EUTelClassTemplate();
+
+	};
+
+}
