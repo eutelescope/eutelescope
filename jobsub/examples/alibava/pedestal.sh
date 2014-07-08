c9d2d7f88fc65b0554e890eca7825f415b4ee094
Eda Yildirim <eda.yildirim@desy.de>
2014-07-08 13:22:22 +0200

    renaming templates for alibava data

--- jobsub/examples/alibava/pedestal.sh
+++ jobsub/examples/alibava/pedestal.sh
@@ -9,8 +9,8 @@
 # usage: sh pedestal.sh runnumber
 
 To calculate Pedestals
-../../jobsub.py -c config.cfg -csv runlist.csv convert-ped $1
-../../jobsub.py -c config.cfg pedestal $1
-../../jobsub.py -c config.cfg -csv runlist.csv commonmode $1
-../../jobsub.py -c config.cfg pedestal2 $1
+../../jobsub.py -c config.cfg -csv runlist.csv alibava-convert-ped $1
+../../jobsub.py -c config.cfg alibava-pedestal $1
+../../jobsub.py -c config.cfg -csv runlist.csv alibava-commonmode $1
+../../jobsub.py -c config.cfg alibava-pedestal2 $1
 
