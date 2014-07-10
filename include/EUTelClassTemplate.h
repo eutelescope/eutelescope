2b54067cad5cacbf488cb9f70c8979f8ae4b835b
Alexander Morton <alexander.morton@desy.de>
2014-07-10 14:07:51 +0200

    remove the exclude of the class template. Since this will cause errors when you want to copy the object

--- include/EUTelClassTemplate.h
+++ include/EUTelClassTemplate.h
@@ -5,7 +5,6 @@ namespace eutelescope {
 	class EUTelClassTemplate{
         
 		private:
-  	DISALLOW_COPY_AND_ASSIGN(EUTelClassTemplate)        // prevent users from making (default) copies of processors
       
 		public:
 		EUTelClassTemplate();
