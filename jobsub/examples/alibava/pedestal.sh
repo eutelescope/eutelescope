10aef68e844d70dd437b1729b91d43399602871c
Eda Yildirim <eda.yildirim@desy.de>
2014-09-23 11:51:40 +0200

    commenting out the comment line

--- jobsub/examples/alibava/pedestal.sh
+++ jobsub/examples/alibava/pedestal.sh
@@ -8,7 +8,7 @@
 
 # usage: sh pedestal.sh runnumber
 
-To calculate Pedestals
+#To calculate Pedestals
 jobsub -c config.cfg -csv runlist.csv alibava-convert-ped $1
 jobsub -c config.cfg alibava-pedestal $1
 jobsub -c config.cfg -csv runlist.csv alibava-commonmode $1
