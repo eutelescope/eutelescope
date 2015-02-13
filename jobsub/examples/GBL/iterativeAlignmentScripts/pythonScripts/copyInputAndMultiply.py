4da2acb97cc9ae163500164dda2c388962ce4a4f
Alexander Morton <alexander.morton975@gmail.com>
2015-02-13 10:55:32 +0000

    Working python scripts

---
+++ jobsub/examples/GBL/iterativeAlignmentScripts/pythonScripts/copyInputAndMultiply.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+import sys
+resolution=str(sys.argv[1])
+numberOfDUTs=str(sys.argv[2])
+print numberOfDUTs 
---
+++ jobsub/examples/GBL/iterativeAlignmentScripts/pythonScripts/countElements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+import sys
+numberOfDUTsPlus1 = len(sys.argv)
+print (numberOfDUTsPlus1 - 1)
