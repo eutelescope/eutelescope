90f88284a2bf4bb6e2aa49865cf8d43164aa4b2e
Alexander Morton <alexander.morton975@gmail.com>
2015-02-13 19:02:02 +0000

    Final change to python script to change resolution depending on fixed planes or any planes you specify

--- jobsub/examples/GBL/iterativeAlignmentScripts/pythonScripts/copyInputAndMultiply.py
+++ jobsub/examples/GBL/iterativeAlignmentScripts/pythonScripts/copyInputAndMultiply.py
@@ -3,4 +3,8 @@
 import sys
 resolution=str(sys.argv[1])
 numberOfDUTs=str(sys.argv[2])
-print numberOfDUTs 
+output=""
+for i in range(0,int(numberOfDUTs)):
+	output+=str(resolution)
+	output+=" "
+print output
--- jobsub/examples/GBL/noDUTExample/noDUTIterativeAlignment.sh
+++ jobsub/examples/GBL/noDUTExample/noDUTIterativeAlignment.sh
@@ -20,8 +20,8 @@ export scriptsLocation="/afs/phas.gla.ac.uk/user/a/amorton/ilcsoft/v01-17-05/Eut
 
 #VARIABLES WHICH VARY THROUGH ALIGNMENT ARE SET HERE. IMPORTANT NOT ALL VARIABLES ARE HERE LOOK IN CONFIG-ALIGNMENT.
 export r="1"; #Make resolution large so we begin with small chi2 and factor improvement to get to chi2/ndf=1 on next iteration. 
-export dutXs="" #This is the resolution of the DUT in the x LOCAL direction taking into account the misalignment
-export dutYs="" #This is the resolution of the DUT in the y LOCAL direction taking into account the misalignment
+export xres="$r $r $r $dutXs $r $r $r";
+export yres="$r $r $r $dutYs $r $r $r";
 export minTracksPerEventAcceptance=0.001 #This is the number of tracks that is needed per event before we stop pattern recognition. Note value should depend on other cuts. 
 export ResidualsRMax="1" #This is the window size on the next plane that we will accept hits from. This will increase if less than 1 track per event is found.
 export inputGearInitial="gear-1T.xml" #Note the gear file changes through the process so must be placed here.
