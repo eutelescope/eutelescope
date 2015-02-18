77a731a7fad03f765954636bc947b08833196ac8
Tobias Bisanz <tobias.bisanz@phys.uni-goettingen.de>
2015-02-18 17:12:13 +0100

    Cleanign up src/EUTelPatternRecognition.cc

--- src/EUTelPatternRecognition.cc
+++ src/EUTelPatternRecognition.cc
@@ -45,10 +45,12 @@ void EUTelPatternRecognition::testTrackCandidates(){
 		}
 	}
 }
+
 //This is the work horse of the class. Using seeds it propagates the track forward using equations of motion. This can be with or without magnetic field.
-void EUTelPatternRecognition::propagateForwardFromSeedState( EUTelState& stateInput, EUTelTrack & track    ){
-	EUTelState *state = &stateInput;//Make it a pointer so we can change this to newState after.
-	streamlog_out ( DEBUG1 ) << "EUTelPatternRecognition::propagateForwardFromSeedState-----BEGIN "<< std::endl;
+void EUTelPatternRecognition::propagateForwardFromSeedState(EUTelState& stateInput, EUTelTrack& track)
+{
+	EUTelState* state = &stateInput;//Make it a pointer so we can change this to newState after.
+	
 	//TO DO: To delete this in smart way. 
 	EUTelState *firstState = new EUTelState(stateInput);//Need to create an initial state that will not be deleted outside this scope  
 	streamlog_out(DEBUG2) << "This is the memory location of the state: "<< firstState << std::endl;
@@ -97,7 +99,7 @@ void EUTelPatternRecognition::propagateForwardFromSeedState( EUTelState& stateIn
 		}
 		
 		//So we have intersection lets create a new state
-		EUTelState *newState = new EUTelState();//Need to create this since we save the pointer and we would be out of scope when we leave this function. Destroying this object. 
+		EUTelState* newState = new EUTelState();//Need to create this since we save the pointer and we would be out of scope when we leave this function. Destroying this object. 
 		newState->setDimensionSize(_planeDimensions[newSensorID]);//We set this since we need this information for later processors
 		newState->setBeamCharge(_beamQ);
 		newState->setLocation(newSensorID);
@@ -112,16 +114,27 @@ void EUTelPatternRecognition::propagateForwardFromSeedState( EUTelState& stateIn
 		}
 		EVENT::TrackerHit* closestHit = const_cast< EVENT::TrackerHit* > ( findClosestHit( *newState )); //This will look for the closest hit but not if it is within the excepted range		
 		double distance;
-		if(newState->getDimensionSize() == 2){
-			distance = sqrt(computeResidual( *newState, closestHit ).Norm2Sqr());//This distance could be 2D or 1D depending on if you have a strip or pixel sensor. Norm2Sqr does not square toot for some reason.
-		}else if(newState->getDimensionSize() == 1){
-			distance = computeResidual( *newState, closestHit )[0];//If strip sensor then use only displacement along strips. Which should be x axis.
-		}else{
+		if(newState->getDimensionSize() == 2)
+		{
+			//This distance could be 2D or 1D depending on if you have a strip or pixel sensor. 
+			//Norm2Sqr does not square root for some reason.
+			distance = sqrt(computeResidual( *newState, closestHit ).Norm2Sqr());
+		}
+		else if(newState->getDimensionSize() == 1)
+		{
+			//If strip sensor then use only displacement along strips, which should be x axis.
+			distance = computeResidual( *newState, closestHit )[0];
+		}
+		else
+		{
 			throw(lcio::Exception( "The closest hit is not on a pixel or strip sensor. Since the dimensionality if less than 1 or greater than 2."));
 		}
-		const double DCA = getXYPredictionPrecision( *newState ); //This does nothing but return a number specified by user. In the future this should use convariance matrix information TO DO: FIX
+
+		const double DCA = getXYPredictionPrecision(*newState);
+
 		streamlog_out ( DEBUG1 ) <<"At plane: "<<newState->getLocation() << ". Distance between state and hit: "<< distance <<" Must be less than: "<<DCA<< std::endl;
 		streamlog_out(DEBUG0) <<"Closest hit position: " << closestHit->getPosition()[0]<<" "<< closestHit->getPosition()[1]<<"  "<< closestHit->getPosition()[2]<<std::endl;
+
 		if ( distance > DCA ) {
 			streamlog_out ( DEBUG1 ) << "Closest hit is outside of search window." << std::endl;
 			track.addTrack(static_cast<EVENT::Track*>(newState));//Need to return this to LCIO object. Loss functionality but retain information 
@@ -131,6 +144,7 @@ void EUTelPatternRecognition::propagateForwardFromSeedState( EUTelState& stateIn
 		if(closestHit == NULL){
 			throw(lcio::Exception( "The closest hit you are trying to add is NULL. This can not be correct"));
 		}
+
 		streamlog_out ( DEBUG1 ) << "Found a hit with memory address: " << closestHit<<" and ID of " <<closestHit->id() <<" At a Distance: "<< distance<<" from state." << std::endl;
 		newState->addHit(closestHit);
 		_totalNumberOfHits++;//This is used for test of the processor later.   
@@ -139,16 +153,11 @@ void EUTelPatternRecognition::propagateForwardFromSeedState( EUTelState& stateIn
 		track.addTrack(static_cast<EVENT::Track*>(newState));//Need to return this to LCIO object. Loss functionality but retain information 
 		track.print();
 		streamlog_out ( DEBUG1 ) << "The number of hits on the track now is "<< track.getNumberOfHitsOnTrack()<< std::endl;
-		//cout<<"HERE3: "<<newState->getPosition()[0]<<","<<newState->getPosition()[1]<<","<<newState->getPosition()[2]<<","<<newState->getLocation()<<std::endl;
-		//cout<<"HERE5: "<<state->getPosition()[0]<<","<<state->getPosition()[1]<<","<<state->getPosition()[2]<<","<<state->getLocation()<<std::endl;
 		state = newState;
-		//cout<<"HERE4: "<<state->getPosition()[0]<<","<<state->getPosition()[1]<<","<<state->getPosition()[2]<<","<<state->getLocation()<<std::endl;
 		streamlog_out ( DEBUG1 ) << "End of loop "<< std::endl;
-
 	}
-	streamlog_out ( DEBUG1 ) << "EUTelPatternRecognition::propagateForwardFromSeedState-----END "<< std::endl;
+}
 
-}	
 void EUTelPatternRecognition::printTrackCandidates(){
 	streamlog_out ( DEBUG1 ) << "EUTelKalmanFilter::printTrackCandidates----BEGIN "<< std::endl;
 	for(size_t i = 0; i < _tracks.size();++i){
@@ -450,7 +459,6 @@ void EUTelPatternRecognition::setHitsVecPerPlane(){
 //Note loop through all planes. Even the excluded. This is easier since you don't have to change this input each time then.
 //TO DO: sensors z position as used here only works if there is sufficient difference between planes. In the order of 1mm in gear file. This is too large.
 void EUTelPatternRecognition::setPlaneDimensionsVec(EVENT::IntVec planeDimensions){
-	streamlog_out(DEBUG0) <<"EUTelPatternRecognition::setPlaneDimensionsVec()----------------------------BEGIN" <<std::endl;
 	if(planeDimensions.size() != geo::gGeometry().sensorZOrdertoIDs().size()){
 		streamlog_out(ERROR) << "The size of planesDimensions input is: "<< planeDimensions.size()<<" The size of sensorZOrdertoIDs is: " << geo::gGeometry().sensorZOrdertoIDs().size()<< std::endl;
 		throw(lcio::Exception( "The input dimension vector not the same as the number of planes!"));
@@ -465,12 +473,10 @@ void EUTelPatternRecognition::setPlaneDimensionsVec(EVENT::IntVec planeDimension
 			throw(lcio::Exception( "You are trying to map the same sensor ID to two different plane dimensions. There is something wrong with you gear file input. Make sure there is some distance between your planes in the gear file!"));
 		}
 	}//END of loop over planes
-	streamlog_out(DEBUG0) <<"EUTelPatternRecognition::setPlaneDimensionsVec()----------------------------END" <<std::endl;
 }	    
 
 
 void EUTelPatternRecognition::testHitsVecPerPlane(){
-	streamlog_out(DEBUG4) <<"EUTelPatternRecognition::testHitsVecPerPlane----------------------------BEGIN" <<std::endl;
 	if(_mapHitsVecPerPlane.size() !=  geo::gGeometry().sensorZOrderToIDWithoutExcludedPlanes().size()){
 		streamlog_out(ERROR0) <<"The size of the planes with hits " << _mapHitsVecPerPlane.size() <<"  Sensors from Geometry with no excluded planes: "<<  geo::gGeometry().sensorZOrderToIDWithoutExcludedPlanes().size()<<std::endl;
 		throw(lcio::Exception("The number of planes that could contain hits and the number of planes is different. Problem could be the gear file has to planes at the same z position.")); 	
@@ -482,13 +488,11 @@ void EUTelPatternRecognition::testHitsVecPerPlane(){
 			streamlog_out(DEBUG0) << "One plane has no hits at all. Is this correct?" << std::endl;
 		}
 	}
-	streamlog_out(DEBUG4) <<"EUTelPatternRecognition::testHitsVecPerPlane----------------------------END" <<std::endl;
-
 }
+
 //This function makes sure that the input for the dimension size is correct.
 //NOTE:This input is alway for ALL planes and not just no excluded ones. This means we don't have to change this if we want to exclude planes. 
 void EUTelPatternRecognition::testPlaneDimensions(){
-	streamlog_out(DEBUG4) <<"EUTelPatternRecognition::testPlaneDimensions----------------------------BEGIN" <<std::endl;
 	if(_planeDimensions.size() != geo::gGeometry().sensorZOrdertoIDs().size()){
 		streamlog_out(ERROR5) << "The size of _planesDimensions is: "<< _planeDimensions.size()<<" The size of sensorZOrdertoIDs is: " << geo::gGeometry().sensorZOrdertoIDs().size()<< std::endl;
 		throw(lcio::Exception( "The output dimension vector is not the same size as the number of planes. Could be something to do with the gear file. Make sure you have some distances between you planes!"));
@@ -498,7 +502,6 @@ void EUTelPatternRecognition::testPlaneDimensions(){
 			throw(lcio::Exception( "The number of dimension for one of your planes is greater than 2 or less than 0. If this is not a mistake collect you nobel prize now!"));
 		}
 	}
-	streamlog_out(DEBUG4) <<"EUTelPatternRecognition::testPlaneDimensions----------------------------END" <<std::endl;
 }
 
     /** Find the hit closest to the intersection of a track with given sensor
@@ -507,13 +510,15 @@ void EUTelPatternRecognition::testPlaneDimensions(){
      * @return hit closest to the intersection of the track with the sensor plane
      * 
 	 */
-const EVENT::TrackerHit* EUTelPatternRecognition::findClosestHit(EUTelState & state ) {
-	streamlog_out(DEBUG2) << "EUTelPatternRecognition::findClosestHit()" << std::endl;
+const EVENT::TrackerHit* EUTelPatternRecognition::findClosestHit(EUTelState& state)
+{
 	EVENT::TrackerHitVec& hitInPlane = _mapHitsVecPerPlane[state.getLocation()];
 	double maxDistance = std::numeric_limits<double>::max();
 	EVENT::TrackerHitVec::const_iterator itClosestHit;
 	EVENT::TrackerHitVec::const_iterator itHit;
+
 	streamlog_out(DEBUG0) << "N hits in plane " << state.getLocation() << ": " << hitInPlane.size() << std::endl;
+
 	for ( itHit = hitInPlane.begin(); itHit != hitInPlane.end(); ++itHit ) {
 		double distance;
 		if(state.getDimensionSize() == 2){
@@ -530,18 +535,16 @@ const EVENT::TrackerHit* EUTelPatternRecognition::findClosestHit(EUTelState & st
 		}
 	}
 	streamlog_out(DEBUG0) << "Minimal distance^2 between hit and track intersection: " << maxDistance << std::endl;
-	streamlog_out(DEBUG2) << "----------------------EUTelPatternRecognition::findClosestHit()------------------------" << std::endl;
 
 	return *itClosestHit;
 }
+
+//TODO: Need proper error analysis to calculate this rather than providing an answer.  
 //This is not very useful at the moment since the covariant matrix for the hit is guess work at the moment.   
-double EUTelPatternRecognition::getXYPredictionPrecision(EUTelState& ts ) const {
-	streamlog_out(DEBUG2) << "EUTelPatternRecognition::getXYPredictionPrecision()---BEGIN" << std::endl;
-	// TO DO: Need proper error analysis to calculate this rather than providing an answer.  
-	double xyPrec = getWindowSize();   //sqrt( Ckkm1[0][0]*Ckkm1[0][0] + Ckkm1[1][1]*Ckkm1[1][1] );
-	streamlog_out(DEBUG0) << "Minimal combined UV resolution : " << xyPrec << std::endl;
-	streamlog_out(DEBUG2) << "----------------------EUTelPatternRecognition::getXYPredictionPrecision()------------------------END" << std::endl;
-	return xyPrec;
+double EUTelPatternRecognition::getXYPredictionPrecision(EUTelState& /*ts*/) const 
+{
+	//sqrt( Ckkm1[0][0]*Ckkm1[0][0] + Ckkm1[1][1]*Ckkm1[1][1] );
+	return getWindowSize();   
 }
 
         
@@ -551,31 +554,37 @@ double EUTelPatternRecognition::getXYPredictionPrecision(EUTelState& ts ) const 
 * @param hit hit
 * @return 
 */
-TVectorD EUTelPatternRecognition::computeResidual(EUTelState& state , const EVENT::TrackerHit* hit ) const {
-	streamlog_out( DEBUG2 ) << "EUTelPatternRecognition::computeResidual()---BEGIN" << std::endl;
+TVectorD EUTelPatternRecognition::computeResidual(EUTelState& state, const EVENT::TrackerHit* hit) const
+{
 	const double* hitPosition = hit->getPosition();//In local coordinates
 
-	streamlog_out( DEBUG3 ) << "Hit (id=" << hit->id() << ") local(u,v) coordinates of hit: (" << hitPosition[0] << "," << hitPosition[1] <<","<<hitPosition[2] << ")" << std::endl;
-
 	double localPosition [3];
-	localPosition[0]=state.getPosition()[0];	localPosition[1]=state.getPosition()[1];	localPosition[2]=state.getPosition()[2];
-	streamlog_out( DEBUG3 ) << "	Prediction for hit (id=" << hit->id() << ") local(u,v) coordinates of state: ("  << localPosition[0] << "," << localPosition[1] <<","<<localPosition[2] << ")" << std::endl;
+	localPosition[0] = state.getPosition()[0];
+	localPosition[1] = state.getPosition()[1];
+	localPosition[2] = state.getPosition()[2];
+
+	streamlog_out(DEBUG3)	<< "Hit (id=" << hit->id() << ") local(u,v) coordinates of hit: (" 
+				<< hitPosition[0] << ", " << hitPosition[1] << ", " << hitPosition[2] << ")" << std::endl
+				<< "Prediction for hit (id=" << hit->id() << ") local(u,v) coordinates of state: ("  
+				<< localPosition[0] << ", " << localPosition[1] << ", " <<localPosition[2] << ")" << std::endl;
 
 	TVectorD residual(2);
-	residual[0] = 0 ; residual[1] = 0;
-	streamlog_out( DEBUG2 ) << "The size of this state dimension: "<< _planeDimensions.at(state.getLocation())<< std::endl;
+	residual[0] = 0; 
+	residual[1] = 0;
+	
+	streamlog_out(DEBUG2) << "The size of this state dimension: "<< _planeDimensions.at(state.getLocation())<< std::endl;
+	
 	//This loop is used in case we have a strip sensor. So we should use only 1 dimension of information
-	for(int i=0; i<_planeDimensions.at(state.getLocation());++i){
+	for(int i=0; i<_planeDimensions.at(state.getLocation()); ++i)
+	{
 		residual[i] = hitPosition[i] - localPosition[i];
 	}	
 	
-	if ( streamlog_level(DEBUG2) ){
-			streamlog_out( DEBUG2 ) << "	Residual vector residual: (" <<residual[0] <<","<<residual[1]<<")"<< std::endl;
+	if (streamlog_level(DEBUG2))
+	{
+			streamlog_out(DEBUG2) << "Residual vector residual: (" << residual[0] << ", " << residual[1] << ")" << std::endl;
 			residual.Print();
 	}
-	
-	streamlog_out( DEBUG2 ) << "----------------------------------EUTelPatternRecognition::computeResidual()------------------------------------END" << std::endl;
-	
 	return residual;
 }
 
@@ -591,15 +600,20 @@ void EUTelPatternRecognition::findHitsOrderVec(LCCollection* lcCollection,EVENT:
 	} // end loop over all hits in lcCollection
 
 }
-void EUTelPatternRecognition::printHits(){
+
+void EUTelPatternRecognition::printHits()
+{
 	streamlog_out(MESSAGE0) << "EUTelPatternRecognition::prinitHit: BEGIN ==============" << std::endl;
 	EVENT::TrackerHitVec::const_iterator itHit;
-	for ( itHit = _allHitsVec.begin() ; itHit != _allHitsVec.end(); ++itHit ) {
+	for(itHit = _allHitsVec.begin(); itHit != _allHitsVec.end(); ++itHit )
+	{
 		const double* uvpos = (*itHit)->getPosition();
 		const int sensorID = Utility::getSensorIDfromHit( static_cast<IMPL::TrackerHitImpl*> (*itHit) );
-		streamlog_out(MESSAGE0) << "Hit (id=" << std::setw(3) << sensorID << ") local(u,v) coordinates: ("<< std::setw(7) << std::setprecision(4) << uvpos[0] << "," << std::setw(7) << std::setprecision(4) << uvpos[1] << ")" << std::endl;
+		streamlog_out(MESSAGE0)	<< "Hit (id=" << std::setw(3) << sensorID << ") local(u,v) coordinates: (" 
+					<< std::setw(7) << std::setprecision(4) << uvpos[0] << ", " << std::setw(7) 
+					<< std::setprecision(4) << uvpos[1] << ")" << std::endl;
 	}
-streamlog_out(MESSAGE0) << "EUTelPatternRecognition::printHits: END ==============" << std::endl;
+	streamlog_out(MESSAGE0) << "EUTelPatternRecognition::printHits: END ==============" << std::endl;
 }
-    
+
 } // namespace eutelescope
