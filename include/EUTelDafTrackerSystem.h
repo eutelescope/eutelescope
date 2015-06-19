22422278ad28f0f5a8f43d97ae72c7caa4b6282d
haavagj <hgjersdal@gmail.com>
2015-06-19 14:19:39 +0200

    Adding the new eigen-fitter template classes from hgjersdal repo

--- include/EUTelDafTrackerSystem.h
+++ include/EUTelDafTrackerSystem.h
@@ -1,72 +1,89 @@
-// Version: $Id$
 #ifndef TRACKERSYSTEM_H
 #define TRACKERSYSTEM_H
 
-// Uncomment as soon as we have a recent version of Eigen from system libraries or elsewhere
-//#define EIGEN2_SUPPORT
-//#include <Eigen/Eigen2Support>
 #include <Eigen/Core>
-
-#include <marlin/AIDAProcessor.h>
-#include "marlin/Processor.h"
 #include <list>
 #include <vector>
 #include <cmath>
 #include <iostream>
 
-namespace daffitter{
+using namespace Eigen;
 
+namespace daffitter{
+  template <typename T, size_t N>
   class TrackEstimate{
   public:
-    TrackEstimate():params(),cov(){;}
-    Eigen::Vector4f params;
-    Eigen::Matrix4f cov;
-    void copy(TrackEstimate* e){
+    Matrix<T, N, 1> params;
+    Matrix<T, N, N> cov;
+    void copy(TrackEstimate<T, N>* e){
       this->cov = e->cov;
       this->params = e->params;
     }
-    float getX() const { return( params(0) ); }
-    float getY() const { return( params(1) ); }
-    float getXdz() const { return( params(2) ); }
-    float getYdz() const { return( params(3) ); }
-  
-    float getSigmaX() const {return(  std::sqrt(cov(0,0))); }
-    float getSigmaY() const {return( std::sqrt(cov(1,1))); }
-    float getSigmaXdz() const {return( std::sqrt(cov(2,2))); }
-    float getSigmaYdz() const {return( std::sqrt(cov(3,3))); }
+    void print(){
+      if( cov.fullPivLu().isInvertible() ){
+	std::cout << "Cov: " << "Invertible!" << std::endl;
+      } else {
+	std::cout << "Cov: " << "non-inverible! :(" << std::endl;
+      }
+      std::cout << "Params: " << std::endl << cov.inverse() * params << std::endl;
+      std::cout << "Cov: " << std::endl << cov.inverse() << std::endl;
+      std::cout << std::endl << params << std::endl << std::endl << cov << std::endl;
+    }
+    void makeSeed(bool keepState, bool freeP = false);
+    void makeSeedInfo(bool freeP = false);
+    bool isSeed();
+
+    T getX() const { return( params(0) ); }
+    T getY() const { return( params(1) ); }
+    T getXdz() const { return( params(2) ); }
+    T getYdz() const { return( params(3) ); }
+    T getMomentum();
+
+    T getSigmaX() const {return(  std::sqrt(cov(0,0))); }
+    T getSigmaY() const {return( std::sqrt(cov(1,1))); }
+    T getSigmaXdz() const {return( std::sqrt(cov(2,2))); }
+    T getSigmaYdz() const {return( std::sqrt(cov(3,3))); }
   };
 
+  template <typename T>
   class Measurement{
-    Eigen::Vector2f m;
+    Matrix<T, 2, 1> m;
     bool m_goodRegion;
-    float zPos;
+    T zPos;
     size_t m_iden;
   public:
-    Eigen::Vector2f getM() const { return(m); }
-    float getX() const { return(m(0)); }
-    float getY() const { return(m(1)); }
-    float getZ() const {return(zPos);}
+    Matrix<T, 2, 1> getM() const { return(m); }
+    T getX() const { return(m(0)); }
+    T getY() const { return(m(1)); }
+    T getZ() const {return(zPos);}
     bool goodRegion() const { return(m_goodRegion); }
     size_t getIden() const { return(m_iden); }
     
-    Measurement(float x, float y, float z, bool goodRegion, size_t iden) : m(), m_goodRegion(goodRegion), zPos(z), m_iden(iden){ 
+    Measurement(T x, T y, T z, bool goodRegion, size_t iden){ 
       m(0) = x; m(1) = y;
       zPos = z;
       m_goodRegion = goodRegion;
       m_iden = iden;
     }
+    Measurement(const Measurement<T>& m ){
+      this->m = m.m;
+      this->m_goodRegion = m.m_goodRegion;
+      this->zPos = m.zPos;
+      this->m_iden = m.m_iden;
+    }
   };
 
+  template <typename T, size_t N>
   class TrackCandidate{
     public:
     //Information about track candidate
     //Measurement indexes for KF
     std::vector<int> indexes;
     //Weights for DAF
-    std::vector< Eigen::VectorXf > weights;
+    std::vector< Matrix<T, Eigen::Dynamic, 1> > weights;
     //Results from fit
-    float chi2, ndof;
-    std::vector<TrackEstimate*> estimates;
+    T chi2, ndof;
+    std::vector<TrackEstimate<T,N>*> estimates;
     void print(){
       std::cout << "Track candidate with " << indexes.size() << " planes:" << std::endl;
       for(size_t ii = 0; ii < indexes.size(); ii++){
@@ -82,80 +99,96 @@ namespace daffitter{
       weights.resize(nPlanes);
       estimates.resize(nPlanes);
       for(int ii = 0; ii < nPlanes; ii++){
-	estimates.at(ii) = new TrackEstimate();
+	estimates.at(ii) = new TrackEstimate<T,N>();
       }
     }
   };
 
+
+  template<typename T>
   class FitPlane{
     // Info about a detector plane in the system
     //Unique identifier
     int sensorID;
     //Increase to angular uncertainties from scattering
-    float scatterThetaSqr;
+    T scatterThetaSqr;
     //If excluded, plane should not be used in any fit
     bool excluded;
-    float zPosition;
-    float measZ;
+    T zPosition;
+    T measZ;
     // Uncertainties of measurements in this plane
-    Eigen::Vector2f sigmas;
-    Eigen::Vector2f variances;
+    Matrix<T, 2, 1> sigmas;
+    Matrix<T, 2, 1> variances;
     //Sum of DAF weights for all measurements
-    float sumWeights;
+    T sumWeights;
     //Ref point
-    Eigen::Vector3f ref0, ref1, ref2;
+    Matrix<T, 3, 1> ref0, ref1, ref2;
     //Norm vector
-    Eigen::Vector3f norm;
+    Matrix<T, 3, 1> norm;
 
   public:
     //Measurements in plane
-    std::vector<Measurement> meas;
+    std::vector< Measurement<T> > meas;
     //Daf weights of measurements
-    Eigen::VectorXf weights;
-    Eigen::Vector2f invMeasVar;
-
-    FitPlane(int sensorID, float zPos, float sigmaX, float sigmaY, float scatterVariance, bool excluded);
+    //Matrix<T, Eigen::Dynamic, 1> weights;
+    Matrix<T, 2, 1> invMeasVar;
+    FitPlane(int sensorID, T zPos, T sigmaX, T sigmaY, T scatterVariance, bool excluded);
     int getSensorID()  const {return(this->sensorID); }
     bool isExcluded() const { return(this->excluded); }
     void exclude() { this->excluded = true; }
     void include() { this->excluded = false; }
-    float getZpos()    const { return( this->zPosition); }
-    float getSigmaX()  const { return(sigmas(0));}
-    float getSigmaY()  const { return(sigmas(1));}
-    Eigen::Vector2f getSigmas() const { return(sigmas);}
-    Eigen::Vector2f getVars() const { return(variances);}
+    T getZpos()    const { return( this->zPosition); }
+    void setZpos(T zPos)    { this->zPosition = zPos; }
+    T getSigmaX()  const { return(sigmas(0));}
+    T getSigmaY()  const { return(sigmas(1));}
+    Matrix<T, 2, 1> getSigmas() const { return(sigmas);}
+    Matrix<T, 2, 1> getVars() const { return(variances);}
     void print();
-    float getScatterThetaSqr() const {return(scatterThetaSqr);}
-    void addMeasurement(float x, float y, float z, bool goodRegion, size_t measIden){ Measurement a(x,y, z, goodRegion, measIden); meas.push_back(a); }
-    void setTotWeight(float weight){ sumWeights = weight;}
-    float getTotWeight() const { return(sumWeights); };
+    T getScatterThetaSqr() const {return(scatterThetaSqr);}
+    void setScatterThetaSqr(T variance) { scatterThetaSqr = variance;}
+    void addMeasurement(T x, T y, T z, bool goodRegion, size_t measIden){ Measurement<T> a(x,y, z, goodRegion, measIden); meas.push_back(a); }
+    void addMeasurement(Measurement<T> m) { meas.push_back(m);}
+    void setTotWeight(T weight){ sumWeights = weight;}
+    T getTotWeight() const { return(sumWeights); };
     void clear(){ meas.clear(); measZ = zPosition;}
-    float getMeasZ() const { return(measZ); }
-    void setMeasZ(float z)  { measZ = z; }
+    T getMeasZ() const { return(measZ); }
+    void setMeasZ(T z)  { measZ = z; }
     //ref points
-    Eigen::Vector3f& getRef0()  { return(ref0); }
-    Eigen::Vector3f& getRef1() { return(ref1); }
-    Eigen::Vector3f& getRef2() { return(ref2); }
-    void setRef0(Eigen::Vector3f point) { ref0 = point; }
-    void setRef1(Eigen::Vector3f point) { ref1 = point; }
-    void setRef2(Eigen::Vector3f point) { ref2 = point; }
+    Matrix<T, 3, 1>& getRef0()  { return(ref0); }
+    Matrix<T, 3, 1>& getRef1() { return(ref1); }
+    Matrix<T, 3, 1>& getRef2() { return(ref2); }
+    void setRef0(Matrix<T, 3, 1> point) { ref0 = point; }
+    void setRef1(Matrix<T, 3, 1> point) { ref1 = point; }
+    void setRef2(Matrix<T, 3, 1> point) { ref2 = point; }
     //Norm vector
-    Eigen::Vector3f& getPlaneNorm() { return(norm); }
-    void setPlaneNorm(Eigen::Vector3f n) { norm = n.normalized(); }
-    void scaleErrors(float scaleX, float scaleY){
+    Matrix<T, 3, 1>& getPlaneNorm() { return(norm); }
+    void setPlaneNorm(Matrix<T, 3, 1> n) { norm = n.normalized(); }
+    void scaleErrors(T scaleX, T scaleY){
       sigmas(0) *= scaleX; sigmas(1) *= scaleY;
       invMeasVar(0) = 1.0f / ( sigmas(0) * sigmas(0));
       invMeasVar(1) = 1.0f / ( sigmas(1) * sigmas(1));
     }
+    void setSigmas(T sigmaX, T sigmaY){
+      sigmas(0) = sigmaX; sigmas(1) = sigmaY;
+      invMeasVar(0) = 1.0f / ( sigmas(0) * sigmas(0));
+      invMeasVar(1) = 1.0f / ( sigmas(1) * sigmas(1));
+    }
+    void setSigmaX(T sigmaX){
+      sigmas(0) = sigmaX; invMeasVar(0) = 1.0/(sigmaX * sigmaX);
+    }
+    void setSigmaY(T sigmaY){
+      sigmas(1) = sigmaY; invMeasVar(1) = 1.0/(sigmaY * sigmaY);
+    }
   };
+  template <typename T>
   class PlaneHit {
   private:
-    Eigen::Vector2f xy;
+    Matrix<T, 2, 1> xy;
     int plane, index;
   public:
-  PlaneHit(float x, float y, int plane, int index): plane(plane), index(index){ xy(0) = x; xy(1) = y; }
-  PlaneHit(Eigen::Vector2f xy, int plane, int index) : xy(xy), plane(plane), index(index) {}
-    const Eigen::Vector2f& getM() { return(xy); }
+  PlaneHit(T x, T y, int plane, int index): plane(plane), index(index){ xy(0) = x; xy(1) = y; }
+  PlaneHit(Matrix<T, 2, 1> xy, int plane, int index) : xy(xy), plane(plane), index(index) {}
+    const Matrix<T, 2, 1>& getM() { return(xy); }
     int getPlane() const {return(plane); }
     int getIndex() const{return(index); };
     void print() {
@@ -163,94 +196,162 @@ namespace daffitter{
     }
   };
 
+  template <typename T, size_t N>
   class EigenFitter{
     //Eigen recommends fixed size matrixes up to 4x4
-    Eigen::Matrix4f transM, transMtranspose, tmp4x4, tmp4x4_2, tmp4x4_3;
-    Eigen::Matrix2f tmp2x2, tmp2x2_2;
-    Eigen::Matrix<float, 4, 2> tmp4x2, kalmanGain;
-    Eigen::Matrix<float, 2, 4> tmp2x4, H;
-    Eigen::Vector4f tmpState1, tmpState2;
-    Eigen::Vector2f resids, chi2s, residsum;
-    Eigen::Vector2f invScatterCov;
+    Matrix<T, N, N> transM, transMtranspose, tmpNxN, tmpNxN_2, tmpNxN_3;
+    Matrix<T, 2, 2> tmp2x2, tmp2x2_2;
+    Matrix<T, N, 2> tmpNx2, kalmanGain;
+    Matrix<T, 2, N> tmp2xN, H;
+    Matrix<T, N, 1> tmpState1, tmpState2;
+    Matrix<T, 2, 1> resids, chi2s, residsum;
+    Matrix<T, 2, 1> invScatterCov;
     //DAF temperature
-    float tval;
-  
+    T tval;
   public:
-    std::vector<TrackEstimate*> forward;
-    std::vector<TrackEstimate*> backward;
-    std::vector<TrackEstimate*> smoothed;
+    std::vector<TrackEstimate<T,N>*> forward;
+    std::vector<TrackEstimate<T,N>*> backward;
+    std::vector<TrackEstimate<T,N>*> smoothed;
   
     EigenFitter(int nPlanes);
 
     //daf weights
-    void setT(float tval) {this->tval = tval;};
-    float getT() { return(this->tval); };
-    void calculateWeights(std::vector<FitPlane> &pl, float chi2cut);
-    void calculatePlaneWeight(FitPlane &pl, TrackEstimate *e, float chi2cutoff);
+    void setT(T tval) {this->tval = tval;};
+    T getT() { return(this->tval); };
+    void calculateWeights(std::vector<FitPlane<T> > &pl, T chi2cut, std::vector< Matrix<T, Eigen::Dynamic, 1> > &weights);
+    void calculatePlaneWeight(FitPlane<T>  &pl, TrackEstimate<T,N> *e, T chi2cutoff, Matrix<T, Eigen::Dynamic, 1> &weights);
 
     //Information filter
-    void predictInfo(const FitPlane &prev, const FitPlane &cur, TrackEstimate* e);
-    void updateInfo(const FitPlane &pl, const int index, TrackEstimate* e);
-    void updateInfoDaf(const FitPlane &pl, TrackEstimate* e);
-    void getAvgInfo(TrackEstimate* e1, TrackEstimate* e2, TrackEstimate* result);
+    void predictInfo(const FitPlane<T>  &prev, const FitPlane<T>  &cur, TrackEstimate<T,N>* e);
+    void addScatteringInfo(const FitPlane<T> & pl, TrackEstimate<T,N>* e);
+    void updateInfo(const FitPlane<T>  &pl, const int index, TrackEstimate<T,N>* e);
+    void updateInfoDaf(const FitPlane<T>  &pl, TrackEstimate<T,N>* e, Matrix<T, Eigen::Dynamic, 1> &weights);
+    void getAvgInfo(TrackEstimate<T,N>* e1, TrackEstimate<T,N>* e2, TrackEstimate<T,N>* result);
     void smoothInfo();
+    //Standard formulation
+    void predict(const FitPlane<T>  &prev, const FitPlane<T>  &cur, TrackEstimate<T,N> *e);
+    //smoother
+    void smooth();
+    void getAvg(TrackEstimate<T,N> *f, TrackEstimate<T,N> *b, TrackEstimate<T,N> *result);
+    //kf
+    void kfUpdate(const FitPlane<T>  &cur, int index, TrackEstimate<T,N> *e);
+    void getKFGain(const FitPlane<T>  &p1, TrackEstimate<T,N> *e);
+    void predictB(const FitPlane<T>  &prev, const FitPlane<T>  &cur, TrackEstimate<T,N> *e);
   };
 
+  template <typename T, size_t N>
   class TrackerSystem{
-    EigenFitter* m_fitter;
+    //EigenFitter* m_fitter;
     bool m_inited;
     size_t m_nTracks, m_maxCandidates, m_minClusterSize;
- 
-    float m_dafChi2, m_chi2OverNdof, m_sqrClusterRadius;
 
-    float m_nXdz, m_nYdz;
+    T m_nXdz, m_nYdz, m_nXdzdeviance, m_nYdzdeviance;
+    T m_dafChi2, m_ckfChi2, m_chi2OverNdof, m_sqrClusterRadius;
+    size_t m_skipMax;
     
-    void getChi2Daf(daffitter::TrackCandidate *candidate);
-    void getChi2Kf(daffitter::TrackCandidate *candidate);
-
-    int addNeighbors(std::vector<PlaneHit> &candidate, std::list<PlaneHit> &hits);
-
-    float runTweight(float t);
-    float fitPlanesInfoDafInner();
-    float fitPlanesInfoDafBiased();
-    float getNominalXdz() const { return(m_nXdz); }
-    float getNominalYdz() const { return(m_nYdz); }
+    int addNeighbors(std::vector<PlaneHit<T> > &candidate, std::list<PlaneHit<T> > &hits);
+    T runTweight(T t, daffitter::TrackCandidate<T,N>* candidate);
+    T fitPlanesInfoDafInner(daffitter::TrackCandidate<T,N>* candidate);
+    T fitPlanesInfoDafBiased(daffitter::TrackCandidate<T,N>* candidate);
     size_t getMinClusterSize() const { return(m_minClusterSize); }
-    void checkNan(TrackEstimate* e);
-  public:
-    std::vector<daffitter::FitPlane> planes;
-    std::vector<daffitter::TrackEstimate*> mcTruth;
-    std::vector<daffitter::TrackCandidate*> tracks;
+    void checkNan(TrackEstimate<T,N>* e);
+    //CKF
+    void finalizeCKFTrack(TrackEstimate<T,N> *est, std::vector<int>& indexes, int nMeas, T chi2);
+    void fitPermutation(int plane, TrackEstimate<T,N> *est, int nSkipped, std::vector<int> &indexes, int nMeas, T chi2);
+    
+  public: 
+    //std::vector<int> m_chi2vals;
+
+    EigenFitter<T,N>* m_fitter;
+    std::vector<daffitter::FitPlane<T> > planes;
+    std::vector<daffitter::TrackCandidate<T,N>*> tracks;
 
     TrackerSystem();
-    TrackerSystem(const TrackerSystem &z);
-    void addPlane(int sensorID, float zPos, float sigmaX, float sigmaY, float scatterVariance, bool excluded);
-    void addMeasurement(size_t planeIndex, float x, float y, float z, bool goodRegion, size_t iden);
-    void init();
+    TrackerSystem(const TrackerSystem<T,N>& sys);
+    void addPlane(int sensorID, T zPos, T sigmaX, T sigmaY, T scatterVariance, bool excluded);
+    void addMeasurement(size_t planeIndex, T x, T y, T z, bool goodRegion, size_t iden);
+    void addMeasurement(Measurement<T>& meas);
+    void init(bool quiet = false);
     void clear();
-    void setTruth(int plane, float x, float y, float xdz, float ydz);
     void setMaxCandidates(int nCandidates);
     size_t getNtracks() const { return(m_nTracks); };
-    void weightToIndex(daffitter::TrackCandidate* cnd);
-
+    void weightToIndex(daffitter::TrackCandidate<T,N>* cnd);
+    void indexToWeight(daffitter::TrackCandidate<T,N>* cnd);
+    Matrix<T, 2, 1> getBiasedResidualErrors(FitPlane<T> & pl, TrackEstimate<T,N>* estim);
+    Matrix<T, 2, 1> getUnBiasedResidualErrors(FitPlane<T> & pl, TrackEstimate<T,N>* estim);
+    Matrix<T, 2, 1> getResiduals(Measurement<T>& meas, TrackEstimate<T,N>* estim);
+      
     //Set cut values for track finder
-    void setDAFChi2Cut(float chival) { m_dafChi2 = chival;}
-    float getDAFChi2Cut() const { return(m_dafChi2);};
-    void setChi2OverNdofCut(float chival) { m_chi2OverNdof = chival;}
-    float getChi2OverNdofCut() const { return(m_chi2OverNdof);};
-    void setClusterRadius(float rad) { m_sqrClusterRadius = rad * rad;}
-    void setNominalXdz(float xdz) { m_nXdz = xdz; }
-    void setNominalYdz(float ydz) { m_nYdz = ydz; }
+    void setDAFChi2Cut(T chival) { m_dafChi2 = chival;}
+    void setCKFChi2Cut(T chival) { m_ckfChi2 = chival;}
+    T getDAFChi2Cut() const { return(m_dafChi2);};
+    T getCKFChi2Cut() const { return(m_ckfChi2);};
+    void setChi2OverNdofCut(T chival) { m_chi2OverNdof = chival;}
+    T getChi2OverNdofCut() const { return(m_chi2OverNdof);};
+    void setClusterRadius(T rad) { m_sqrClusterRadius = rad * rad;}
+    void setNominalXdz(T xdz) { m_nXdz = xdz; }
+    void setNominalYdz(T ydz) { m_nYdz = ydz; }
+    void setXdzMaxDeviance(T xdz) { m_nXdzdeviance = xdz; }
+    void setYdzMaxDeviance(T ydz) { m_nYdzdeviance = ydz; }
     void setMinClusterSize( size_t n) { m_minClusterSize = n; }
     void intersect();
+    void setMaxSkippedHits(size_t n) { m_skipMax; }
+
+    T getNominalXdz() const { return(m_nXdz); }
+    T getNominalYdz() const { return(m_nYdz); }
+    T getXdzMaxDeviance() const { return(m_nXdzdeviance); }
+    T getYdzMaxDeviance() const { return(m_nYdzdeviance); }
 
     //Track finders
     void clusterTracker();
     void truthTracker();
+    void combinatorialKF();
+    void index0tracker();
 
     //Fitters
-    void fitPlanesInfo(daffitter::TrackCandidate *candidate);
-    void fitPlanesInfoDaf(daffitter::TrackCandidate*);
+    void fitPlanesInfoBiased(daffitter::TrackCandidate<T,N> *candidate);
+    void fitPlanesInfoUnBiased(daffitter::TrackCandidate<T,N> *candidate);
+    void fitPlanesInfoDaf(daffitter::TrackCandidate<T,N>*);
+    void fitPlanesKF(daffitter::TrackCandidate<T,N>* candidate);
+    //partial fitters
+    void fitInfoFWBiased(TrackCandidate<T,N> *candidate);
+    void fitInfoFWUnBiased(TrackCandidate<T,N> *candidate);
+    void fitInfoBWUnBiased(TrackCandidate<T,N> *candidate);
+    void fitInfoBWBiased(TrackCandidate<T,N> *candidate);
+    //Getting chi2
+    void getChi2Kf(daffitter::TrackCandidate<T,N> *candidate);
+    void getChi2BiasedInfo(TrackCandidate<T,N> *candidate);
+    void getChi2UnBiasedInfo(TrackCandidate<T,N> *candidate);
+    void getChi2UnBiasedInfoDaf(TrackCandidate<T,N> *candidate);
+
+    void smoothInfo(TrackCandidate<T,N> *candidate);
   };
+
+  // Invert sparce matrix
+  template <typename T>
+  inline void partialFastInvert(Matrix<T, 4, 4> &cov, size_t p1, size_t p2){
+    // Matrix is sparce, if state vector was [x,dx/dz, y,dy/dz] it would be block diagonal
+    //Invert as if it was
+
+    //Invert block
+    T a(cov(p1,p1)), d(cov(p2,p2)), b(cov(p1,p2));
+    T det = 1.0f / (a * d - b * b);
+    cov(p1,p1) = det * d;
+    cov(p2,p2) = det * a;
+    cov(p2,p1) = cov(p1,p2) = det * -b;
+  }
+  
+  template <typename T>
+  inline void fastInvert(Matrix<T, 4, 4> &cov){
+    // Matrix is sparce, if state vector was [x,dx/dz, y,dy/dz] it would be block diagonal
+    //Invert as if it was
+    // "Block" [x, dx]
+    partialFastInvert(cov, 0, 2);
+    // "Block" [y, dy]
+    partialFastInvert(cov, 1, 3);
+  }
 }
+#include <EUTelDafTrackerSystem.tcc>
+#include <EUTelDafEigenFitter.tcc>
+
 #endif
