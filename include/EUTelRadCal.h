7bc3f97e2db2edb526580bf53e22c4ea47888062
Alexander Morton <alexander.morton975@gmail.com>
2015-07-16 12:07:24 +0100

    Begin to add simple radiation length calculations. Conversion of some values not done correctly yet

--- include/EUTelRadCal.h
+++ include/EUTelRadCal.h
@@ -14,10 +14,11 @@
 namespace eutelescope {
   //! This class performs all radiation length calculations. 
   /*! This class will use EUTelTrack objects directly. 
-   *  However the analysis code simply uses points in global space to propgate through.
    *  Radiation length is stored in terms of blocks. All sensors included in fit are assumed a thin scatterer.
    *  The radiation length in front of each sensor is assumed thick. Integration over the propagated trajectory for thick scatterers is needed.
    *  After this integration the weighted position and variance of the arclength is determined. This is used to model the thick scatterer by two scattering points.
+   *  The "integration" is done by getting the needed radiation length and distance for each homogeneous volume.  
+   *  These volumes are combined if dead material to describe the full scattering by only two scattering points.
    *
    */ 
 
@@ -26,92 +27,38 @@ namespace eutelescope {
         public:
             //! Struct object
             /*! This stores the basic information for each block of radiation length.
-             *  Each block is either a sensor or the material in front of it.
+             *  Each block contains the information to model the scattering for the sensor and the material infront of it.  
              *
              */
 
             struct Block{
                 bool isSen;
-                double totalRad;
+                double senRadPer;
+                double medRadPer;
                 double weigMean;
                 double weigVar;
-                std::vector<double> startPos; 
-                std::vector<double> endPos;
+                std::vector<std::pair<double,double> > thicknessAndRad; 
+
             };
-            //! Set block homogeneous information.
-            /*! This function will take the start and end points just outside the detector system.
-             *  It will then propagate through the full detector system and get the total radiation length for each block. 
-             *  Will only calculate the radiation length if in the system. If you propagate from outside then this is ignored.
-             *  @param[in] start start position of propagation 
-             *  @param[in] end end position of propagation
-             *  @param[out] blocks This is a vector of blocks.  
+            void setMeanWeight(Block & block);
+            void setVarWeight(Block & block);
+
+            //! The function will take a track and return the radiation length blocks 
+            /*! The tracks are needed to determine which planes should be excluded. 
+             *  @param [in] track
              */
 
-            float setHomoBlocks(TVector3& start , TVector3& end ,std::vector<int>& sen,std::map<int ,Block>& blocks);
-            //! Set radiation information to blocks 
-            /*! 
-             *  This will use the first and last hit on a track to parameterise a straight line. 
-             *  This straight line is then used to propagate through each detector volume and determine the radiation length
-             *  @param[in] track EUTelTrack object 
-             */
-            /// \todo Can make this propagation through the volumes curved by updating the propagtion in TGeo. Simple but not needed now.
-
-            //! Get weighted moments for thick scatterers.
-            /*! This function will take each block which is medium. These all will have negative ID.
-             *  The start and end positions of each block are used to propagate in steps allowing the radiation length at each position
-             *  to weight the position and variance of the arclength
-             *  With these the inhomogenous medium can be modelled with two scattering points. 
-             *  
-             *  @param[in] blocks Filled vector of blocks 
-             *  @param[in] beamEnergy This is needed for the radiation length to be converted to a variance.
-             */
+            void getRad(EUTelTrack& track);
+            void setIncSenBlocks(EUTelTrack const & track, std::map<int, Block>& blocks); 
+            void getThicknessAndRad(EUTelTrack const & track, std::map<int, Block> & blocks);
+            void getScatParam(std::map<int, Block> & blocks);
 
 
-            float setInHomoBlocks(std::map<int ,Block>& blocks, double beamEnergy);
 
-            void setRad(EUTelTrack& track);
+            void setBlockWithoutTrackInfo(EUTelTrack const  & track, std::map<int, Block>& blocks);
 
-            //! Moves node and calculates the radiation length from that motion. 
-            /*! This will take a node and try to find the next node which it can propagate to. 
-             *  If it does this over a sensible range then take the node radiation length and distance travelled and move the node. 
-             *  If the distance to the next node was too small then propagate manually and update usign this.
-             *  @param [in out ] node TGeoNode
-             *  @param [out] rad radiation length from node moving to new location
-             *  @param [out] distance travelled 
-             */
-            void move(TGeoNode *node, double& rad, double& dist);
-
-        private:
-            //! Fill the blocks in the correct order with homogeneous information 
-            /*! This function will update the map from sensor ID to block. Material infront of each sensor is mapped to -ID to block.   
-             *  @param [in] geo EUTelGeometryTelescopeGeoDescription passed to get the node location and global position
-             *  @param [in] rad pass radition length to update block with 
-             *  @param [in] dist Distance travelled by block
-             *  @param [out] blocks updated blocks.
-             */
-
-            void fillBlocks(geo::EUTelGeometryTelescopeGeoDescription& geo,double & rad, double& dist,std::map<int ,Block>& blocks);
-            //! Calculate the meanWeight for the scatter 
-            /*! Function will update block using the start and end positions of the Block object.
-             *  The arclength weighted with the variance at each point is added to the point
-             *  
-             *  @param[in] beamEnergy This is needed for the radiation length to be converted to a variance.
-             *  @param[in out] itBl This is an iterator of a map from ID to block
-             */
-            void getDistWeig(double& beamEnergy, std::map<int, Block>::iterator itBl);
-            //! Calculate the varWeight for the scatter 
-            /*! The function will not get the variance of the arclength weighted with the variance due to scattering. 
-             *  
-             *  @param[in] beamEnergy This is needed for the radiation length to be converted to a variance.
-             *  @param[in out] itBl This is an iterator of a map from ID to block
-             */
-
-            void getVarWeig(double& beamEnergy, std::map<int, Block>::iterator itBl);
 
         protected:
-            ///Planes included in fit.
-            std::vector<int> _planes;
-            std::map<int,Block> _blocks;
 
 
 	};
--- src/EUTelRadCal.cpp
+++ src/EUTelRadCal.cpp
@@ -2,187 +2,83 @@
 using namespace eutelescope;
 
 
-float EUTelRadCal::setHomoBlocks(TVector3& start , TVector3& end ,std::vector<int>& sen,std::map<int ,Block>& blocks){
-    streamlog_out(DEBUG5) << "/////////////////////////////////////////////////////////////////////////////////////////////////// " << std::endl;
-    streamlog_out(DEBUG5) << "/////////////////////////////////////////////////////////////////////////////////////////////////// " << std::endl;
-    streamlog_out(DEBUG5) << "              CALCULATING THE TOTAL RADIATION LENGTH BETWEEN TWO POINTS.                            " << std::endl;
-    streamlog_out(DEBUG5) << "                                 POINTS GO FROM:                            " << std::endl;
-    streamlog_out(DEBUG5) << "              ("<< start[0] << ","  << start[1]<<","<<start[2]<<")"<< "-------------------------------------> ("<< end[0] << ","  << end[1]<<","<<end[2]<<")" << std::endl;
-    TVector3 diff = end - start;
-    diff.Unit();
-    static geo::EUTelGeometryTelescopeGeoDescription& geo = geo::gGeometry();
-	TGeoManager* gGeoManager = geo._geoManager;
-    gGeoManager->InitTrack( start[0]/*mm*/, start[1]/*mm*/, start[2]/*mm*/, diff[0], diff[1], diff[2] ); //Start point and direction
-    TGeoNode *node = gGeoManager->GetCurrentNode( );
-    while ( node ) {
-        ///This will call the current node and get the ID. Silly to access this way but leave for now.
-        double rad,dist;
-        move(node,rad,dist);
-        fillBlocks(geo,rad,dist,blocks);
-    }
-}
-float EUTelRadCal::setInHomoBlocks(std::map<int ,Block>& blocks, double beamEnergy){
-    for(std::map<int, Block>::iterator itBl = blocks.begin(); itBl != blocks.end(); ++itBl){
-        if(itBl->first < 0){///If block is medium then integrate.
-            getDistWeig(beamEnergy,itBl);
-            getVarWeig(beamEnergy,itBl);
-        }
-    }
-}
-
-void EUTelRadCal::move(TGeoNode *node, double & rad, double& dist){
-    const double mm2cm = 0.1;
-    TGeoMedium *med = NULL;
-    if ( node ) med = node->GetVolume()->GetMedium();
-    else throw(std::string("Passed non node in move EUTelRadCal! "));
-    double radlen = med->GetMaterial()->GetRadLen() /*cm*/;
-    double lastrad = 1. / radlen * mm2cm; //calculate 1/radiationlength per cm. This will transform radlen to mm
-    node = gGeoManager->FindNextBoundaryAndStep( 1000 /*mm*/ ); //Some large number to check for new boundary. 
-    dist  = gGeoManager->GetStep() /*mm*/; //This will output the distance traveled by FindNextBoundaryAndStep
-    rad = 0; //This is the calculated (rad per distance x distance)
-    double delta = 0.01;//This is the minimum block size 
-    streamlog_out(DEBUG5)<<std::endl <<std::endl  << "DECISION: Step size over min?  "  <<" Block width: " << dist << " delta: " << delta  << std::endl;
-    if(dist < delta){
-       streamlog_out(DEBUG5) << "INCREASE TO MINIMUM DISTANCE!" << std::endl;
-        dist = delta;
-        double pt[3];
-        memcpy( pt, gGeoManager->GetCurrentPoint(), 3 * sizeof (double) ); //Get global position
-        const double *dir = gGeoManager->GetCurrentDirection();//Direction vector
-        for ( Int_t i = 0; i < 3; i++ ) pt[i] += delta * dir[i]; //Move the current point slightly in the direction of motion. 
-        node = gGeoManager->FindNode( pt[0], pt[1], pt[2] );//Move to new node where we will begin to look for more radiation length   
-        rad=lastrad*dist; //Calculate radiation length for the increased block.
-    }else{
-        streamlog_out(DEBUG5) << "OVERMAX!" << std::endl;
-        rad = lastrad*dist; //This is the calculated (rad per distance x distance)
-    }
-    streamlog_out(DEBUG5) << "NEW BLOCK:SensorID: " <<" Block width: " << dist << " Radiation total/per unit length: " << rad<<"/"<< std::endl;
-}
-/// Will take the EUTelescope object and get the sensor ID from the node.  
-/// It will then update the radiation length and add the start and end points to the trajectory.
-/**
- */
-
-void EUTelRadCal::fillBlocks(geo::EUTelGeometryTelescopeGeoDescription& geo,double & rad, double& dist, std::map<int ,Block>& blocks){
-    double pt[3];
-    memcpy( pt, gGeoManager->GetCurrentPoint(), 3 * sizeof (double) ); //Get global position
-    int senID = geo.getSensorIDFromManager();
-    if(senID !=  *(_planes.rbegin()) and blocks.size() == 0   ){ ///Must not have reached first sensor.
-        if(std::find(_planes.begin(),  _planes.end(), senID) !=  _planes.end()){//Should include this to sensor.
-            try{///Is it a new block? Yes if exception. Else update sensor radiation length
-                blocks.at(senID).totalRad = blocks.at(senID).totalRad + rad;
-            }catch(std::out_of_range&  e){///Reached new sensor boundary. Add position and initial rad.
-                blocks[senID].startPos.push_back(pt[0]);blocks[senID].startPos.push_back(pt[1]);blocks[senID].startPos.push_back(pt[2]);
-                blocks[senID].totalRad = rad;
-                if(blocks.size() != 0 ){//If this is not the first block it must be the end for thick scatterer block.
-                    int id = -1.0*senID +1;///Add end point to the last medium.  
-                    blocks[id].endPos.push_back(pt[0]);blocks[id].endPos.push_back(pt[1]);blocks[id].endPos.push_back(pt[2]);
-                }
-            }
-        }else{///Must be medium inbetween sensors.
-            int lastSenID =  blocks.rbegin()->first;//Last sensor we intersected.
-            int idMat = -1.0*lastSenID;//Material infront of senID is mapped to -senID
-            try{///Update If this has been created before.
-                blocks.at(idMat).totalRad = blocks.at(idMat).totalRad + rad;
-            }catch(std::out_of_range& e){///New medium block boundary. Add this to the end of the sensor block
-                blocks[lastSenID].endPos.push_back(pt[0]);blocks[lastSenID].endPos.push_back(pt[1]);blocks[lastSenID].endPos.push_back(pt[2]);
-                if(lastSenID != *(_planes.rbegin()) ){///If we have not got the last sensor then this must be saved. If so then do nothing.
-                    blocks[idMat].totalRad = rad;
-                    blocks[idMat].startPos.push_back(pt[0]);blocks[idMat].startPos.push_back(pt[1]);blocks[idMat].startPos.push_back(pt[2]);
-                }else{
-                    ///We have the last plane and now in next part of sensor
-                    ///Do nothing
-                 }
-            }
-        }
-    }
-}
-void EUTelRadCal::setRad(EUTelTrack& track){
-    _planes = track.getPlaIDs();
-    streamlog_out(DEBUG3)<<"compute radiation..."<<std::endl;
-    TVector3 gPosS =  track.getStates().at(0).getPositionGlobal();
-    TVector3 gPosE =  track.getStates().back().getPositionGlobal();
-    /// Take the start position just outside the first volume. So we include this in the calculation of radiaiton length.
-    TVector3 start(gPosS[0],gPosS[1],gPosS[2]-0.05);
-    TVector3 end(gPosE[0],gPosE[1],gPosE[2]+0.05);
-    ///NOW WE CALCULATE THE RADIATION LENGTH FOR THE FULL FLIGHT AND THEN SPLIT THESE INTO LINEAR COMPONENTS FOR SCATTERING ESTIMATION. 
-    streamlog_out(DEBUG3)<<"Use TGeo now!"<<std::endl;
+void EUTelRadCal::getRad(EUTelTrack& track){
     std::map<int ,Block> blocks;
-    std::vector<int> pla = track.getPlaIDs();
-    setHomoBlocks(start ,end ,pla,blocks );
-    setInHomoBlocks(blocks, track.getBeamEnergy());
-    _blocks =blocks;
+    setIncSenBlocks(track,blocks);
+    getThicknessAndRad(track,blocks); 
+    getScatParam(blocks); 
 
 }
-void EUTelRadCal::getDistWeig(double& beamEnergy, std::map<int, Block>::iterator itBl){
-    TVector3 end(itBl->second.endPos.at(0), itBl->second.endPos.at(1), itBl->second.endPos.at(2)); 
-    TVector3 start(itBl->second.startPos.at(0), itBl->second.startPos.at(1), itBl->second.startPos.at(2)); 
-    TVector3 diff = end  - start;
-    diff.Unit();
-    static geo::EUTelGeometryTelescopeGeoDescription& geo = geo::gGeometry();
-    TGeoManager* gGeoManager = geo._geoManager;
-    gGeoManager->InitTrack( start[0]/*mm*/, start[1]/*mm*/, start[2]/*mm*/, diff[0], diff[1], diff[2] ); //Start point and direction
-    TGeoNode *node = gGeoManager->GetCurrentNode( );
-    double radTotal =0;
-    double distTotal=0;
-    double weigMean=0;
-    while ( node ) {
-        double rad,dist;
-        move(node,rad,dist);
-        radTotal = radTotal +rad; //Used as check at the end.
-        distTotal = distTotal + dist;//Keep total distance travelled since start
-        double var =  pow(Utility::getThetaRMSHighland( beamEnergy, rad ),2);
-        double weigMean = weigMean +  distTotal*var;//Update where mean should be placed.
-        double pt[3];
-        memcpy( pt, gGeoManager->GetCurrentPoint(), 3 * sizeof (double) ); //Get global position
-        TVector3 newPos(pt[0], pt[1],pt[2] ); 
-        if((end - newPos).Mag() ==  0){
-            break;
+void EUTelRadCal::setIncSenBlocks(EUTelTrack const & track, std::map<int, Block>& blocks){ 
+    for(std::vector<EUTelState>::iterator  itSt = track.getStatesCopy().begin(); itSt != track.getStatesCopy().end(); ++itSt){
+            double senRad =  geo::gGeometry().siPlaneRadLength(itSt->getLocation());
+            double senSize =  geo::gGeometry().siPlaneZSize(itSt->getLocation());
+            ///Must check values for this method of access
+            blocks[itSt->getLocation()].senRadPer = senSize/senRad;
+    }
+}
+
+void EUTelRadCal::getThicknessAndRad(EUTelTrack const & track, std::map<int, Block> & blocks){ 
+    std::map<int,std::vector<std::pair<double,double> > > IDToThickAndRad;
+    /// to air = 37.15 g cm/2   
+    /// Density:0.001225 g/cm3
+    /// 30326 radiaiton length cm-1
+    ///Should get this from TGeo. How??
+    double radAir = 30326;
+    for(std::map<int,Block>::iterator itBl = blocks.begin(); itBl != blocks.end(); ++itBl){//Loop over included sensors.
+        int zOrdStartInc =  geo::gGeometry().sensorIDtoZOrder( itBl->first );
+        int zOrdEndInc =  geo::gGeometry().sensorIDtoZOrder( (itBl++)->first );
+        itBl--; //This is silly but map is not random access iterator. How should I do this?
+        int diff= zOrdEndInc - zOrdStartInc - 1; 
+        std::vector<std::pair<double,double> > thicknessAndRad; 
+        int ordIncrease=0;
+        for(int i =0 ; i < diff; ++i){ ///For excluded sensors: collect air then sensor, repeat ......
+            int startAirID  = geo::gGeometry().sensorIDsVec().at(zOrdStartInc + i);
+            int endAirID = geo::gGeometry().sensorIDsVec().at(zOrdStartInc +i +1);
+            double startZPosAir =  geo::gGeometry().siPlaneZPosition(startAirID);
+            double endZPosAir =  geo::gGeometry().siPlaneZPosition(endAirID);
+            double sizeAir = endZPosAir - startZPosAir;
+            thicknessAndRad.push_back(std::make_pair(sizeAir,radAir));
+            double excSize =  geo::gGeometry().siPlaneZSize(endAirID);
+            double excRad =  geo::gGeometry().siPlaneRadLength(endAirID);
+            thicknessAndRad.push_back(std::make_pair(excSize,excRad));
+            ordIncrease++;
         }
+        ///Add the last block of air before the included sensor.
+        double sizeAir =  geo::gGeometry().siPlaneZPosition((itBl++)->first) - geo::gGeometry().siPlaneZPosition( geo::gGeometry().sensorIDsVec().at(zOrdStartInc + ordIncrease));
+        itBl--;
+        thicknessAndRad.push_back(std::make_pair(sizeAir,radAir));
+        itBl->second.thicknessAndRad = thicknessAndRad;
     }
-    if(itBl->second.totalRad == radTotal){
-        double varTot =  pow(Utility::getThetaRMSHighland( beamEnergy, radTotal ),2);
-        itBl->second.weigMean = weigMean/varTot;
-    }else{
-        //Something must be wrong since the total radiation length should be the same as calculated in Homogenous block
-        throw(std::string("Radition lengths from iteration do not match"));
-    }
-
 }
 
-void EUTelRadCal::getVarWeig(double& beamEnergy, std::map<int, Block>::iterator itBl){
-    TVector3 end(itBl->second.endPos.at(0), itBl->second.endPos.at(1), itBl->second.endPos.at(2)); 
-    TVector3 start(itBl->second.startPos.at(0), itBl->second.startPos.at(1), itBl->second.startPos.at(2)); 
-    TVector3 diff = end  - start;
-    diff.Unit();
-    static geo::EUTelGeometryTelescopeGeoDescription& geo = geo::gGeometry();
-    TGeoManager* gGeoManager = geo._geoManager;
-    gGeoManager->InitTrack( start[0]/*mm*/, start[1]/*mm*/, start[2]/*mm*/, diff[0], diff[1], diff[2] ); //Start point and direction
-    TGeoNode *node = gGeoManager->GetCurrentNode( );
-    double radTotal =0;
-    double distTotal=0;
-    double weigVar=0;
-    while ( node ) {
-        double rad,dist;
-        move(node,rad,dist);
-        radTotal = radTotal +rad; //Used as check at the end.
-        distTotal = distTotal + dist;//Keep total distance travelled since start
-        double var =  pow(Utility::getThetaRMSHighland( beamEnergy, rad ),2);
-        double weigVar = weigVar +  pow(distTotal-itBl->second.weigMean ,2)*var;//Update where mean should be placed.
-        double pt[3];
-        memcpy( pt, gGeoManager->GetCurrentPoint(), 3 * sizeof (double) ); //Get global position
-        TVector3 newPos(pt[0], pt[1],pt[2] ); 
-        if((end - newPos).Mag() ==  0){
-            break;
-        }
+void EUTelRadCal::getScatParam(std::map<int, Block> & blocks){ 
+    for(std::map<int,Block>::iterator itBl = blocks.begin(); itBl != blocks.end(); ++itBl){
+        setMeanWeight(itBl->second);
+        setVarWeight(itBl->second);
     }
-    if(itBl->second.totalRad == radTotal){
-        double varTot =  pow(Utility::getThetaRMSHighland( beamEnergy, radTotal ),2);
-
-        itBl->second.weigVar = weigVar/varTot;
-    }else{
-        //Something must be wrong since the total radiation length should be the same as calculated in Homogenous block
-        throw(std::string("Radition lengths from iteration do not match"));
-    }
-
 }
+void EUTelRadCal::setMeanWeight(Block & block){ 
+    double mean = 0;
+    double norm = 0;
+    ///Add each homogeneous scatter as a new term in the for loop.
+    for(std::vector<std::pair<double, double > >::iterator itThiRad = block.thicknessAndRad.begin (); itThiRad != block.thicknessAndRad.end() ; ++itThiRad){
+        mean = mean + 0.5*pow(itThiRad->first,2)/itThiRad->second;        
+        norm = norm + itThiRad->first/itThiRad->second; 
+    }
+    block.weigMean = mean/norm;
+}
+void EUTelRadCal::setVarWeight(Block & block){ 
+    double var = 0;
+    double norm = 0;
+    for(std::vector<std::pair<double, double > >::iterator itThiRad = block.thicknessAndRad.begin (); itThiRad != block.thicknessAndRad.end() ; ++itThiRad){
+        var = var + ((1.0/3.0)*pow(itThiRad->first,3) - pow(itThiRad->first,2)*block.weigMean + pow(block.weigMean,2)*itThiRad->first)/itThiRad->second;
+        norm = norm + itThiRad->first/itThiRad->second; 
+    }
+    block.weigMean = var/norm;
+}
+
+
+
+
 
