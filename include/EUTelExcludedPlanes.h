026f3931203399071e09c2ce031b6c9bdae37ee3
Alexander Morton <alexander.morton975@gmail.com>
2015-07-18 21:10:05 +0100

    Exclude plane not working. Now test and works well

--- include/EUTelExcludedPlanes.h
+++ include/EUTelExcludedPlanes.h
@@ -11,11 +11,15 @@ class EUTelExcludedPlanes
 	public: 
 		static std::vector<int> getExcludedPlaneIDVec(std::vector<int> const& planesToExclude);
 		static std::vector<int> getExcludedPlaneIDVecZSorted(std::vector<int> const& planesToExclude);
-  	
+        static void setRelativeComplementSet(std::vector<int> const& planesToExclude);
+
+        static  std::vector<int>  _senInc;
+ 	
 	private:
 		static std::vector<int> getRelativeComplementSet(std::vector<int> const& planesToExclude);
 		static bool sortSensorIDByZPos(int i, int j);
 		EUTelExcludedPlanes();
+
 };
 
 }
