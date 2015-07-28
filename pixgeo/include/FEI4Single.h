dc03c39feff30ff18fb8e1eae7303988ec3dc2fb
Ryan Nelson <rnelson@ppepc17.physics.gla.ac.uk>
2015-07-28 17:22:51 +0100

    Suspicious change to header file

--- pixgeo/include/FEI4Single.h
+++ pixgeo/include/FEI4Single.h
@@ -17,46 +17,147 @@
 #include "TGeoVolume.h"
 
 namespace eutelescope {
-namespace geo {
-
-class FEI4Single : public EUTelGenericPixGeoDescr {
-	
-	public:
-		FEI4Single();
-		~FEI4Single();
-
-		void createRootDescr(char const *);
-		std::string getPixName(int, int);
-		std::pair<int, int> getPixIndex(char const *);
-
-	protected:
-		TGeoMaterial* matSi;
-		TGeoMedium* Si;
-		TGeoVolume* plane;
-
-};
-
-class FEI4Single250x50 : public EUTelGenericPixGeoDescr {
-	
-	public:
-		FEI4Single250x50();
-		~FEI4Single250x50();
-
-		void createRootDescr(char const *);
-		std::string getPixName(int, int);
-		std::pair<int, int> getPixIndex(char const *);
-
-	protected:
-		TGeoMaterial* matSi;
-		TGeoMedium* Si;
-		TGeoVolume* plane;
-
-};
-
+    namespace geo {
+        
+        class FEI4Single : public EUTelGenericPixGeoDescr {
+            
+        public:
+            FEI4Single();
+            ~FEI4Single();
+            
+            void createRootDescr(char const *);
+            std::string getPixName(int, int);
+            std::pair<int, int> getPixIndex(char const *);
+            
+        protected:
+            TGeoMaterial* matSi;
+            TGeoMedium* Si;
+            TGeoVolume* plane;
+            
+        };
+        
+        class FEI4Single250x50 : public EUTelGenericPixGeoDescr {
+            
+        public:
+            FEI4Single250x50();
+            ~FEI4Single250x50();
+            
+            void createRootDescr(char const *);
+            std::string getPixName(int, int);
+            std::pair<int, int> getPixIndex(char const *);
+            
+        protected:
+            TGeoMaterial* matSi;
+            TGeoMedium* Si;
+            TGeoVolume* plane;
+            
+        };
+        
+        class FEI4Single125x100 : public EUTelGenericPixGeoDescr {
+            
+        public:
+            FEI4Single125x100();
+            ~FEI4Single125x100();
+            
+            void createRootDescr(char const *);
+            std::string getPixName(int, int);
+            std::pair<int, int> getPixIndex(char const *);
+            
+        protected:
+            TGeoMaterial* matSi;
+            TGeoMedium* Si;
+            TGeoVolume* plane;
+            
+        };
+        
+        class FEI4Single167x125 : public EUTelGenericPixGeoDescr {
+            
+        public:
+            FEI4Single167x125();
+            ~FEI4Single167x125();
+            
+            void createRootDescr(char const *);
+            std::string getPixName(int, int);
+            std::pair<int, int> getPixIndex(char const *);
+            
+        protected:
+            TGeoMaterial* matSi;
+            TGeoMedium* Si;
+            TGeoVolume* plane;
+            
+        };
+        
+        class FEI4Single167x125_super : public EUTelGenericPixGeoDescr {
+            
+        public:
+            FEI4Single167x125_super();
+            ~FEI4Single167x125_super();
+            
+            void createRootDescr(char const *);
+            std::string getPixName(int, int);
+            std::pair<int, int> getPixIndex(char const *);
+            
+        protected:
+            TGeoMaterial* matSi;
+            TGeoMedium* Si;
+            TGeoVolume* plane;
+            
+        };
+        
+        class FEI4Single2000x25 : public EUTelGenericPixGeoDescr {
+            
+        public:
+            FEI4Single2000x25();
+            ~FEI4Single2000x25();
+            
+            void createRootDescr(char const *);
+            std::string getPixName(int, int);
+            std::pair<int, int> getPixIndex(char const *);
+            
+        protected:
+            TGeoMaterial* matSi;
+            TGeoMedium* Si;
+            TGeoVolume* plane;
+            
+        };
+        
+        class FEI4Single500x25 : public EUTelGenericPixGeoDescr {
+            
+        public:
+            FEI4Single500x25();
+            ~FEI4Single500x25();
+            
+            void createRootDescr(char const *);
+            std::string getPixName(int, int);
+            std::pair<int, int> getPixIndex(char const *);
+            
+        protected:
+            TGeoMaterial* matSi;
+            TGeoMedium* Si;
+            TGeoVolume* plane;
+            
+        };
+        
+        class FEI4Single500x25_alt : public EUTelGenericPixGeoDescr {
+            
+        public:
+            FEI4Single500x25_alt();
+            ~FEI4Single500x25_alt();
+            
+            void createRootDescr(char const *);
+            std::string getPixName(int, int);
+            std::pair<int, int> getPixIndex(char const *);
+            
+        protected:
+            TGeoMaterial* matSi;
+            TGeoMedium* Si;
+            TGeoVolume* plane;
+            
+        };
 
 extern "C"
 {
-	EUTelGenericPixGeoDescr* maker();
+EUTelGenericPixGeoDescr* maker();
 }
 
 } //namespace geo
