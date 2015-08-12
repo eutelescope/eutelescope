e1c33ef0236bdc51491fcd804b3ab84063794d47
Eda Yildirim <eda.yildirim@desy.de>
2015-08-12 18:09:41 +0200

    making the shell scripts nicer

--- jobsub/examples/alibava/pedestal_nafsubmission.sh
+++ jobsub/examples/alibava/pedestal_nafsubmission.sh
@@ -12,6 +12,19 @@
 # you will need to run these templates
 TEMPLATELIST=('alibava-convert-ped' 'alibava-pedestal' 'alibava-commonmode' 'alibava-pedestal2')
 
+
 for TEMPLATE in $TEMPLATELIST; do
-jobsub -c config.cfg -csv runlist.csv $TEMPLATE $1
+jobsub -c config.cfg -csv runlist.csv --subdir --naf qsubparameters.cfg $TEMPLATE $1
+
+#check if there is any run submitted to the naf
+#if there is wait for them to be finished 
+while [[ -n `qstat` ]]; do
+        echo Waiting for jobs to be finished
+        sleep 30
 done
+if [[ -z `qstat` ]];then
+        echo ready to start new job
+fi
+
+done
+
--- jobsub/examples/alibava/reco_nafsubmission.sh
+++ jobsub/examples/alibava/reco_nafsubmission.sh
@@ -13,7 +13,17 @@
 TEMPLATELIST=('alibava-converter' 'alibava-reco' 'alibava-datahisto' 'alibava-commonmodecut' 'alibava-seedclustering' 'alibava-crosstalk-it1' 'alibava-crosstalk-it2' 'alibava-crosstalk-it3')
 
 for TEMPLATE in $TEMPLATELIST; do
-jobsub -c config.cfg -csv runlist.csv $TEMPLATE $1
-done
+jobsub -c config.cfg -csv runlist.csv --subdir --naf qsubparameters.cfg $TEMPLATE $1
 
+#check if there is any run submitted to the naf
+#if there is wait for them to be finished 
+while [[ -n `qstat` ]]; do
+        echo Waiting for jobs to be finished
+        sleep 30
+done
+if [[ -z `qstat` ]];then
+        echo ready to start new job
+fi
+
+done
 
