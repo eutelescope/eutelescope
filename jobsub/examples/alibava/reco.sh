dbbec0a4dc735c490e755ed9554966874a0d1859
Eda Yildirim <eda.yildirim@desy.de>
2015-08-20 19:15:52 +0200

    rearranging analysis chain removin the templates that are not used

--- jobsub/examples/alibava/reco.sh
+++ jobsub/examples/alibava/reco.sh
@@ -10,7 +10,7 @@
 
 # To get pedestal subtracted, common mode and cross talk corrected signal values
 # you will need to run these templates
-TEMPLATELIST=('alibava-converter' 'alibava-reco' 'alibava-datahisto' 'alibava-commonmodecut' 'alibava-seedclustering' 'alibava-crosstalk-it1' 'alibava-crosstalk-it2' 'alibava-crosstalk-it3')
+TEMPLATELIST=('alibava-converter' 'alibava-reco' 'alibava-seedclustering' 'alibava-crosstalk-it1' 'alibava-crosstalk-it2' 'alibava-crosstalk-it3')
 
 for TEMPLATE in $TEMPLATELIST; do
 jobsub -c config.cfg -csv runlist.csv $TEMPLATE $@
