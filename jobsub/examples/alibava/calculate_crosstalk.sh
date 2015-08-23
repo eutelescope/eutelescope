5797fb60e3e603140b2785d1b6a66ef1c74947a4
Eda Yildirim <eda.yildirim@desy.de>
2015-08-23 18:18:24 +0200

    adding scrtipts to calculate crosstalk

---
+++ jobsub/examples/alibava/calculate_crosstalk.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+#########################################################################################
+#                                                                                       #
+# ALiBaVa Analysis - Calculates CrossTalk and does Signal Reconstruction and clustering #
+#                                                                                       #
+#########################################################################################
+
+# usage: source reco.sh runnumber(s)
+
+# To get pedestal subtracted, common mode and cross talk corrected signal values
+# you will need to run these templates
+TEMPLATELIST=('alibava-converter' 'alibava-reco' 'alibava-crosstalk-it1' 'alibava-crosstalk-it2' 'alibava-crosstalk-it3')
+
+for TEMPLATE in $TEMPLATELIST; do
+	jobsub -c config/config.cfg -csv runlistfiles/runlist.csv $TEMPLATE $@
+done
+
+
