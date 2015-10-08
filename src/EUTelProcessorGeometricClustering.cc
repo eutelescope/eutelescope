8d658146b1da511ef1e4385e5775907143468f35
Dorothea vom Bruch <vombruch@physi.uni-heidelberg.de>
2015-10-08 15:56:15 +0200

    Merge with cherry-pick for Mupixel implementation

--- src/EUTelProcessorGeometricClustering.cc
+++ src/EUTelProcessorGeometricClustering.cc
@@ -331,190 +331,191 @@ void EUTelProcessorGeometricClustering::geometricClustering(LCEvent * evt, LCCol
 		minX = minY = maxX = maxY = 0;
 		geoDescr->getPixelIndexRange( minX, maxX, minY, maxY );
 
-    		if ( type == kEUTelGenericSparsePixel ) 
-		{
-
-			// now prepare the EUTelescope interface to sparsified data.
-			std::auto_ptr<EUTelTrackerDataInterfacerImpl<EUTelGenericSparsePixel > > sparseData( new EUTelTrackerDataInterfacerImpl<EUTelGenericSparsePixel> ( zsData ) );
-
-			streamlog_out ( DEBUG2 ) << "Processing sparse data on detector " << sensorID << " with " << sparseData->size() << " pixels " << std::endl;
-			
-			// check for number of hits per sensor
-			if( sparseData->size() > _cutNPixels ) {
-			  streamlog_out ( MESSAGE3 ) << "Not processing event " << evt->getEventNumber() << ", more than " << _cutNPixels << " pixels" << std::endl;
-			  continue;
-			}
-
-			int hitPixelsInEvent = sparseData->size();
-			std::vector<EUTelGeometricPixel> hitPixelVec;
-			EUTelGenericSparsePixel* genericPixel = new EUTelGenericSparsePixel;
-
-			//This for-loop loads all the hits of the given event and detector plane and stores them as GeometricPixels
-			for(int i = 0; i < hitPixelsInEvent; ++i )
-			{
-				//Load the information of the hit pixel into genericPixel
-				sparseData->getSparsePixelAt( i, genericPixel );
-				EUTelGeometricPixel hitPixel( *genericPixel );
-
-				//And get the path to the given pixel
-				std::string pixelPath = geoDescr->getPixName(hitPixel.getXCoord(), hitPixel.getYCoord());
-
-				//Then navigate to this pixel with the TGeo manager
-				geo::gGeometry()._geoManager->cd( (planePath+pixelPath).c_str() );
-
-				//get the imbedding box
-				TGeoShape* currentShape =  geo::gGeometry()._geoManager->GetCurrentVolume()->GetShape();
-				TGeoBBox* bbox = dynamic_cast<TGeoBBox*>( currentShape );
-				//store the dimensions of this box in the GeometricPixel
-				hitPixel.setBoundaryX( bbox->GetDX() );
-				hitPixel.setBoundaryY( bbox->GetDY() );
-
-				//Get how deep the node description goes (this is how often we have to transform to get coordinates in the local plane coordinate system)
-				std::vector<std::string> split = Utility::stringSplit( planePath+pixelPath , "/", false);
-
-				//Three recursions for the telescope/plane
-				int recursionDepth = split.size() - 3;
-
-				//The do the transformation
-				Double_t origin_pt[3] = {0,0,0};
-				Double_t transformed1_pt[3];
-				Double_t transformed2_pt[3];
-				gGeoManager->GetCurrentNode()->LocalToMaster(origin_pt, transformed1_pt);
-
-				transformed2_pt[0] = transformed1_pt[0];
-				transformed2_pt[1] = transformed1_pt[1];
-				transformed2_pt[2] = transformed1_pt[2];
-
-				//transform into local plane coordinate system
-				for(int i = 1 ; i < recursionDepth; ++i)
-				{
-					gGeoManager->GetMother(i)->LocalToMaster(transformed1_pt, transformed2_pt);
-					transformed1_pt[0] = transformed2_pt[0];
-					transformed1_pt[1] = transformed2_pt[1];
-					transformed1_pt[2] = transformed2_pt[2];
-				}
-
-				//store all the position information in the GeometricPixel
-				hitPixel.setPosX( transformed2_pt[0] );
-				hitPixel.setPosY( transformed2_pt[1] );
-				//and push this pixel back
-				hitPixelVec.push_back( hitPixel );
-			}		
-
-			std::vector<EUTelGeometricPixel> newlyAdded;
-			//We now cluster those hits together
-			while( !hitPixelVec.empty() )
-			{
-				// prepare a TrackerData to store the cluster candidate
-				std::auto_ptr< TrackerDataImpl > zsCluster ( new TrackerDataImpl );
-				// prepare a reimplementation of sparsified cluster
-				std::auto_ptr<EUTelGenericSparseClusterImpl<EUTelGeometricPixel > > sparseCluster ( new EUTelGenericSparseClusterImpl<EUTelGeometricPixel >( zsCluster.get() ) );
-
-				//First we need to take any pixel, so let's take the first one
-				//Add it to the cluster as well as the newly added pixels
-				newlyAdded.push_back( hitPixelVec.front() );
-				sparseCluster->addSparsePixel( &hitPixelVec.front() );
-				//And remove it from the original collection
-				hitPixelVec.erase( hitPixelVec.begin() );
+		// now prepare the EUTelescope interface to sparsified data.  
+		std::auto_ptr<EUTelTrackerDataInterfacer> sparseData = std::auto_ptr<EUTelTrackerDataInterfacer>();
 		
-				//Now process all newly added pixels, initially this is the just previously added one
-				//but in the process of neighbour finding we continue to add new pixels
-				while( !newlyAdded.empty() )
-				{
-					bool newlyDone = true;
-					float x1, x2, y1, y2, dX, dY, cx1, cy1, cx2, cy2, cutX, cutY, t1 , t2, dT;
-
-					//check against all pixels in the hitPixelVec
-					for( std::vector<EUTelGeometricPixel>::iterator hitVec = hitPixelVec.begin(); hitVec != hitPixelVec.end(); ++hitVec )
-					{
-						//get the relevant infos from the newly added pixel
-						x1 = newlyAdded.front().getPosX();
-						y1 = newlyAdded.front().getPosY();
-						t1 =  newlyAdded.front().getTime();
-						cx1 = newlyAdded.front().getBoundaryX();
-						cy1 = newlyAdded.front().getBoundaryY();
-
-						//and the pixel we test against
-						x2 = hitVec->getPosX();
-						y2 = hitVec->getPosY();
-						t2 = hitVec->getTime();
-						cx2 = hitVec->getBoundaryX();
-						cy2 = hitVec->getBoundaryY();
-
-						dX = x1 - x2;
-						dY = y1 - y2;
-						dT = t1 - t2;
-						cutX = (cx1+cx2)*1.01; //this additional 1% is accounting for precision
-						cutY = (cy1+cy2)*1.01; //uncertainty with the geo framework
-
-						//if they pass the spatial and temporal cuts, we add them	
-						if(	(dX*dX <= cutX*cutX) && (dY*dY <= cutY*cutY) && (dT*dT <= _cutT*_cutT) )
-						{
-							//add them to the cluster as well as to the newly added ones
-							newlyAdded.push_back( *hitVec );
-							sparseCluster->addSparsePixel( &(*hitVec) );
-							//and remove it from the original collection
-							hitPixelVec.erase( hitVec );
-							//for the pixel we test there might be other neighbours, we still have to check
-							newlyDone = false;
-							break;
-						}
-					}
-				
-					//if no neighbours are found, we can delete the pixel from the newly added
-					//we tested against _ALL_ non cluster pixels, there are no other pixels
-					//which could be neighbours
-					if(newlyDone) newlyAdded.erase( newlyAdded.begin() );
-				}	
-
-				//Now we need to process the found cluster
-				if ( sparseCluster->size() > 0 ) 
-				{
-					// set the ID for this zsCluster
-					idZSClusterEncoder["sensorID"]  = sensorID;
-					idZSClusterEncoder["sparsePixelType"] = static_cast<int>( kEUTelGeometricPixel );
-					idZSClusterEncoder["quality"] = 0;
-					idZSClusterEncoder.setCellID( zsCluster.get() );
-
-					// add it to the cluster collection
-					sparseClusterCollectionVec->push_back( zsCluster.get() );
-
-					//int xSeed, ySeed, xSize, ySize;
-					//sparseCluster->getClusterInfo(xSeed, ySeed, xSize, ySize);
-
-					// prepare a pulse for this cluster
-					std::auto_ptr<TrackerPulseImpl> zsPulse ( new TrackerPulseImpl );
-					idZSPulseEncoder["sensorID"]  = sensorID;
-					//idZSPulseEncoder["xSeed"]     = xSeed;
-					//idZSPulseEncoder["ySeed"]     = ySeed;
-					idZSPulseEncoder["type"]      = static_cast<int>(kEUTelGenericSparseClusterImpl);
-					idZSPulseEncoder.setCellID( zsPulse.get() );
-
-					zsPulse->setCharge( sparseCluster->getTotalCharge() );
-					//zsPulse->setQuality( static_cast<int > (sparseCluster->getClusterQuality()) );
-					zsPulse->setTrackerData( zsCluster.release() );
-					pulseCollection->push_back( zsPulse.release() );
-
-					// last but not least increment the totClusterMap
-					_totClusterMap[ sensorID ] += 1;
-
-				} //cluster processing if
-
-				else 
-				{
-					//in the case the cluster candidate is not passing the threshold ...
-					//forget about them, the memory should be automatically cleaned by std::auto_ptr's
-				}
-			} //loop over all found clusters
+    		if ( type == kEUTelGenericSparsePixel ) 
+		  {
+		    sparseData =  std::auto_ptr<EUTelTrackerDataInterfacer>( new EUTelTrackerDataInterfacerImpl<EUTelGenericSparsePixel>(zsData) );
+		  }
+		else if( type == kEUTelBinaryPixel )
+		  {
+		    
+		    sparseData =  std::auto_ptr<EUTelTrackerDataInterfacer>( new EUTelTrackerDataInterfacerImpl<EUTelBinaryPixel>(zsData) );
+		  }
+		else
+		  {
+		    throw UnknownDataTypeException("Unknown sparsified pixel");
+		  }
+		
+		streamlog_out ( DEBUG2 ) << "Processing sparse data on detector " << sensorID << " with " << sparseData->size() << " pixels " << std::endl;
+		
+		int hitPixelsInEvent = sparseData->size();
+		std::vector<EUTelGeometricPixel> hitPixelVec;
+		EUTelGenericSparsePixel* pixel = NULL;
 			
-			delete genericPixel;
-    		}	 
-		else 
-		{
-    			throw UnknownDataTypeException("Unknown sparsified pixel");
-    		}
-	} // this is the end of the loop over all ZS detectors
+		//This for-loop loads all the hits of the given event and detector plane and stores them as GeometricPixels
+		for(int i = 0; i < hitPixelsInEvent; ++i )
+		  {
+		   
+		    pixel = dynamic_cast<EUTelGenericSparsePixel *>( sparseData->getSparsePixelAt( i, pixel ) );
+		    EUTelGeometricPixel hitPixel( *pixel );
+		    
+		    //And get the path to the given pixel
+		    std::string pixelPath = geoDescr->getPixName(hitPixel.getXCoord(), hitPixel.getYCoord());
+		    
+		    //Then navigate to this pixel with the TGeo manager
+		    geo::gGeometry()._geoManager->cd( (planePath+pixelPath).c_str() );
+		    
+		    //get the imbedding box
+		    TGeoShape* currentShape =  geo::gGeometry()._geoManager->GetCurrentVolume()->GetShape();
+		    TGeoBBox* bbox = dynamic_cast<TGeoBBox*>( currentShape );
+		    //store the dimensions of this box in the GeometricPixel
+		    hitPixel.setBoundaryX( bbox->GetDX() );
+		    hitPixel.setBoundaryY( bbox->GetDY() );
+		    
+		    //Get how deep the node description goes (this is how often we have to transform to get coordinates in the local plane coordinate system)
+		    std::vector<std::string> split = Utility::stringSplit( planePath+pixelPath , "/", false);
+		    
+		    //Three recursions for the telescope/plane
+		    int recursionDepth = split.size() - 3;
+		    
+		    //The do the transformation
+		    Double_t origin_pt[3] = {0,0,0};
+		    Double_t transformed1_pt[3];
+		    Double_t transformed2_pt[3];
+		    gGeoManager->GetCurrentNode()->LocalToMaster(origin_pt, transformed1_pt);
+		    
+		    transformed2_pt[0] = transformed1_pt[0];
+		    transformed2_pt[1] = transformed1_pt[1];
+		    transformed2_pt[2] = transformed1_pt[2];
 
+		    //transform into local plane coordinate system
+		    for(int i = 1 ; i < recursionDepth; ++i)
+		      {
+			gGeoManager->GetMother(i)->LocalToMaster(transformed1_pt, transformed2_pt);
+			transformed1_pt[0] = transformed2_pt[0];
+			transformed1_pt[1] = transformed2_pt[1];
+			transformed1_pt[2] = transformed2_pt[2];
+		      }
+		    
+		    //store all the position information in the GeometricPixel
+		    hitPixel.setPosX( transformed2_pt[0] );
+		    hitPixel.setPosY( transformed2_pt[1] );
+		    //and push this pixel back
+		    hitPixelVec.push_back( hitPixel );
+		  }		
+		
+		std::vector<EUTelGeometricPixel> newlyAdded;
+		//We now cluster those hits together
+		while( !hitPixelVec.empty() )
+		  {
+		    // prepare a TrackerData to store the cluster candidate
+		    std::auto_ptr< TrackerDataImpl > zsCluster ( new TrackerDataImpl );
+		    // prepare a reimplementation of sparsified cluster
+		    std::auto_ptr<EUTelGenericSparseClusterImpl<EUTelGeometricPixel > > sparseCluster ( new EUTelGenericSparseClusterImpl<EUTelGeometricPixel >( zsCluster.get() ) );
+		    
+		    //First we need to take any pixel, so let's take the first one
+		    //Add it to the cluster as well as the newly added pixels
+		    newlyAdded.push_back( hitPixelVec.front() );
+		    sparseCluster->addSparsePixel( &hitPixelVec.front() );
+		    //And remove it from the original collection
+		    hitPixelVec.erase( hitPixelVec.begin() );
+		    
+		    //Now process all newly added pixels, initially this is the just previously added one
+		    //but in the process of neighbour finding we continue to add new pixels
+		    while( !newlyAdded.empty() )
+		      {
+			bool newlyDone = true;
+			float x1, x2, y1, y2, dX, dY, cx1, cy1, cx2, cy2, cutX, cutY, t1 , t2, dT;
+			
+			//check against all pixels in the hitPixelVec
+			for( std::vector<EUTelGeometricPixel>::iterator hitVec = hitPixelVec.begin(); hitVec != hitPixelVec.end(); ++hitVec )
+			  {
+			    //get the relevant infos from the newly added pixel
+			    x1 = newlyAdded.front().getPosX();
+			    y1 = newlyAdded.front().getPosY();
+			    t1 =  newlyAdded.front().getTime();
+			    cx1 = newlyAdded.front().getBoundaryX();
+			    cy1 = newlyAdded.front().getBoundaryY();
+			    
+			    //and the pixel we test against
+			    x2 = hitVec->getPosX();
+			    y2 = hitVec->getPosY();
+			    t2 = hitVec->getTime();
+			    cx2 = hitVec->getBoundaryX();
+			    cy2 = hitVec->getBoundaryY();
+			    
+			    dX = x1 - x2;
+			    dY = y1 - y2;
+			    dT = t1 - t2;
+			    cutX = (cx1+cx2)*1.01; //this additional 1% is accounting for precision
+			    cutY = (cy1+cy2)*1.01; //uncertainty with the geo framework
+			    
+			    //if they pass the spatial and temporal cuts, we add them	
+			    if(	(dX*dX <= cutX*cutX) && (dY*dY <= cutY*cutY) && (dT*dT <= _cutT*_cutT) )
+			      {
+				//add them to the cluster as well as to the newly added ones
+				newlyAdded.push_back( *hitVec );
+				sparseCluster->addSparsePixel( &(*hitVec) );
+				//and remove it from the original collection
+				hitPixelVec.erase( hitVec );
+				//for the pixel we test there might be other neighbours, we still have to check
+				newlyDone = false;
+				break;
+			      }
+			  }
+			
+			//if no neighbours are found, we can delete the pixel from the newly added
+			//we tested against _ALL_ non cluster pixels, there are no other pixels
+			//which could be neighbours
+			if(newlyDone) newlyAdded.erase( newlyAdded.begin() );
+		      }	
+		    
+		    //Now we need to process the found cluster
+		    if ( sparseCluster->size() > 0 ) 
+		      {
+			// set the ID for this zsCluster
+			idZSClusterEncoder["sensorID"]  = sensorID;
+			idZSClusterEncoder["sparsePixelType"] = static_cast<int>( kEUTelGeometricPixel );
+			idZSClusterEncoder["quality"] = 0;
+			idZSClusterEncoder.setCellID( zsCluster.get() );
+			
+			// add it to the cluster collection
+			sparseClusterCollectionVec->push_back( zsCluster.get() );
+			
+			//int xSeed, ySeed, xSize, ySize;
+			//sparseCluster->getClusterInfo(xSeed, ySeed, xSize, ySize);
+			
+			// prepare a pulse for this cluster
+			std::auto_ptr<TrackerPulseImpl> zsPulse ( new TrackerPulseImpl );
+			idZSPulseEncoder["sensorID"]  = sensorID;
+			//idZSPulseEncoder["xSeed"]     = xSeed;
+			//idZSPulseEncoder["ySeed"]     = ySeed;
+			idZSPulseEncoder["type"]      = static_cast<int>(kEUTelGenericSparseClusterImpl);
+			idZSPulseEncoder.setCellID( zsPulse.get() );
+			
+			zsPulse->setCharge( sparseCluster->getTotalCharge() );
+			//zsPulse->setQuality( static_cast<int > (sparseCluster->getClusterQuality()) );
+			zsPulse->setTrackerData( zsCluster.release() );
+			pulseCollection->push_back( zsPulse.release() );
+			
+			// last but not least increment the totClusterMap
+			_totClusterMap[ sensorID ] += 1;
+			
+		      } //cluster processing if
+		    
+		    else 
+		      {
+			//in the case the cluster candidate is not passing the threshold ...
+			//forget about them, the memory should be automatically cleaned by std::auto_ptr's
+		      }
+		  } //loop over all found clusters
+		
+		delete pixel;
+		
+	} // this is the end of the loop over all ZS detectors
+	
 	// if the sparseClusterCollectionVec isn't empty add it to the
 	// current event. The pulse collection will be added afterwards
 	if ( ! isDummyAlreadyExisting ) 
