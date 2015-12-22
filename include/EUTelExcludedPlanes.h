c7738bbe6a1e0236aba392f581d39f8d3a7fe222
Alexander Morton <alexander.morton975@gmail.com>
2015-12-22 19:04:43 +0000

    Update the exclude planes code to work with dead material

--- include/EUTelExcludedPlanes.h
+++ include/EUTelExcludedPlanes.h
@@ -11,8 +11,12 @@ class EUTelExcludedPlanes
 	public: 
 		static std::vector<int> getExcludedPlaneIDVec(std::vector<int> const& planesToExclude);
 		static std::vector<int> getExcludedPlaneIDVecZSorted(std::vector<int> const& planesToExclude);
-  	
+        static std::vector<int> getSensorsNotDeadMaterial();
+        static void setPlaneInc(std::vector<int> const& plaInc);	
+        static void setRelativeComplementSet(std::vector<int> const& planesToExclude);
 	private:
+        static  std::vector<int>  _senInc;
+        static  std::vector<int>  _senNoDeadMaterial;
 		static std::vector<int> getRelativeComplementSet(std::vector<int> const& planesToExclude);
 		static bool sortSensorIDByZPos(int i, int j);
 		EUTelExcludedPlanes();
