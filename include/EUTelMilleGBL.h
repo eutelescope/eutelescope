35a4c48679761ae403eb989bd572ee06c4a1842a
Thomas <thomas.eichhorn@desy.de>
2017-06-16 09:29:22 +0200

    Header files...

--- include/EUTelMilleGBL.h
+++ include/EUTelMilleGBL.h
@@ -41,11 +41,10 @@
 #include <map>
 
 #if defined(USE_ROOT) || defined(MARLIN_USE_ROOT)
-#include "TMinuit.h"
-#include <TSystem.h>
-#include <TMath.h>
+//#include "TMinuit.h"
+//#include <TSystem.h>
+//#include <TMath.h>
 #include "TVector3.h"
-#include <TProfile.h>
 class TMinuit;
 #endif
 
@@ -53,297 +52,183 @@ class TMinuit;
 namespace eutelescope
 {
 
-	class EUTelMilleGBL : public marlin::Processor
-	{
+    class EUTelMilleGBL : public marlin::Processor
+    {
+
+	public:
+
+	    #if defined ( USE_ROOT ) || defined ( MARLIN_USE_ROOT )
+
+	    std::map < std::string , TObject * > _rootObjectMap;
+
+	    #endif
+
+	    class HitsInPlane
+	    {
 
 		public:
 
-		#if defined(USE_ROOT) || defined(MARLIN_USE_ROOT)
-
-		std::map<std::string , TObject * > _rootObjectMap;
-
-		class hit
-		{
-
-			public:
-
-			hit() {}
-			hit(double tx, double ty, double tz, double rx, double ry, double rz, int i)
-			{
-				x = tx;
-				y = ty;
-				z = tz;
-				resolution_x = rx;
-				resolution_y = ry;
-				resolution_z = rz;
-				planenumber = i;
-			}
-
-			double x;
-			double y;
-			double z;
-			double resolution_x;
-			double resolution_y;
-			double resolution_z;
-			int planenumber;
-		};
-
-		#endif
-
-		class HitsInPlane
-		{
-
-			public:
-
-			HitsInPlane()
-			{
-				measuredX = 0.0;
-				measuredY = 0.0;
-				measuredZ = 0.0;
-			}
-			HitsInPlane(double x, double y, double z)
-			{
-				measuredX = x;
-				measuredY = y;
-				measuredZ = z;
-			}
-			bool operator<(const HitsInPlane& b) const
-			{
-				return (measuredZ < b.measuredZ);
-			}
-			double measuredX;
-			double measuredY;
-			double measuredZ;
-		};
-
-		virtual Processor * newProcessor()
-		{
-			return new EUTelMilleGBL;
-		}
-
-		EUTelMilleGBL ();
-
-		virtual void init ();
-
-		virtual void processRunHeader (LCRunHeader * run);
-
-		virtual void processEvent (LCEvent * evt);
-
-		virtual void end();
-
-		virtual bool isPointInPlane(TVector3 point);
-
-		void bookHistos();
-
-		bool _BField;
-
-		protected:
-
-		//! Ordered sensor ID
-		/*! Within the processor all the loops are done up to _nPlanes and
-		 *  according to their position along the Z axis (beam axis).
-		 *
-		 *  This vector is containing the sensorID sorted according to the
-		 *  same rule.
-		*/
-
-		std::vector< int > _orderedSensorID;
-		std::vector< int > _orderedSensorID_wo_excluded;
-
-		std::vector<std::string > _hitCollectionName;
-
-		//only for internal usage
-		std::vector<int > _excludePlanes;
-
-		//this is going to be set by the user.
-		std::vector<int > _excludePlanes_sensorIDs;
-
-		//only for internal usage
-		std::vector<int > _FixedPlanes;
-		std::vector<int > _FixedPlanes_sensorIDs;
-
-		//this is going to be
-		//set by the user.
-		double _eBeam;
-		double _triCut;
-		double _triCutDUTx;
-		double _triCutDUTy;
-		double _driCut;
-		double _driCutREFx;
-		double _driCutREFy;
-		double _sixCut;
-		double _resx;
-		double _resy;
-		double _resdutx;
-		double _resduty;
-		double _resrefx;
-		double _resrefy;
-
-		//! Id of telescope layer which should be used as a DUT
-		int _manualDUTid;
-
-		int _dutFitMethod;
-
-		int _doPreAlignment;
-
-		int _doDUTAlignment;
-
-		IntVec _FixParameter;
-
-		double _probCut;
-		double _chi2ndfCut;
-		double _slopecutDUTx;
-		double _slopecutDUTy;
-		int _probCutCount;
-		int _chi2ndfCutCount;
-		int _cutfailsx;
-		int _cutfailsy;
-
-		int _maxTrackCandidatesTotal;
-
-		std::string _binaryFilename;
-
-		int _alignMode;
-
-		int _generatePedeSteerfile;
-		std::string _pedeSteerfileName;
-		int _runPede;
-
-		int _inputMode;
-
-		int _requireDUTHit;
-
-		bool _useREF;
-
-		bool _dthistos;
-
-		float _testModeSensorResolution;
-		float _testModeXTrackSlope;
-		float _testModeYTrackSlope;
-		std::vector<float > _testModeSensorZPositions;
-		std::vector<float > _testModeSensorXShifts;
-		std::vector<float > _testModeSensorYShifts;
-		std::vector<float > _testModeSensorGamma;
-		std::vector<float > _testModeSensorAlpha;
-		std::vector<float > _testModeSensorBeta;
-
-		std::string _alignmentConstantLCIOFile;
-		std::string _alignmentConstantCollectionName;
-		std::string _coordinatorPreAlignmentCollectionName;
-
-		double * _dut_align_x;
-		double * _dut_align_y;
-		double * _dut_align_z;
-		double * _dut_align_a;
-		double * _dut_align_b;
-		double * _dut_align_c;
-		double * _dut_align_x_error;
-		double * _dut_align_y_error;
-		double * _dut_align_z_error;
-		double * _dut_align_a_error;
-		double * _dut_align_b_error;
-		double * _dut_align_c_error;
-		double * _dut_pre_align_x;
-		double * _dut_pre_align_y;
-		double * _dut_pre_align_z;
-		double * _dut_pre_align_a;
-		double * _dut_pre_align_b;
-		double * _dut_pre_align_c;
-		double _coordinator_x;
-		double _coordinator_y;
-		double _coordinator_z;
-
-		bool _alignmentloaded;
-		bool _pre_alignmentloaded;
-		bool _dutplanevectors;
-
-		// define axis for rotations:
-		TVector3 _Xaxis;
-		TVector3 _Yaxis;
-		TVector3 _Zaxis;
-
-		//! Alignment collection name
-		std::vector<std::string> _alignmentCollectionName;
-
-		//! Prealignment collection name
-		std::vector<std::string> _pre_alignmentCollectionName;
-
-		std::string _outputHitCollectionName;
-
-		std::string _outputTrackCollectionName;
-
-		private:
-
-		TVector3 _dutplane;
-		TVector3 _normalvector;
-
-		//! Run number
-		int _iRun;
-
-		//! Event number
-		int _iEvt;
-
-		//! Excluded planes
-		int _nExcludePlanes;
-
-		//! Statistics
-		int _nMilleDataPoints;
-		int _nMilleTracks;
-		int _nGoodMilleTracks;
-
-		//! The function to get the alignment from file
-		void getAlignment (LCEvent * event);
-
-		//! The function to get the prealignment from file
-		void getPreAlignment (LCEvent * event);
-
-		void getCoordinatorAlignment (LCEvent * event);
-
-		void getDUTNormal();
-
-		//! Silicon planes parameters as described in GEAR
-		/*! This structure actually contains the following:
-		 *  @li A reference to the telescope geoemtry and layout
-		 *  @li An integer number saying if the telescope is w/ or w/o DUT
-		 *  @li An integer number saying the number of planes in the
-		 *  telescope.
-		 *
-		 *  This object is provided by GEAR during the init() phase and
-		 *  stored here for local use.
-		*/
-		gear::SiPlanesParameters * _siPlanesParameters;
-
-		//! Silicon plane layer layout
-		/*! This is the real geoemetry description. For each layer
-		 *  composing the telescope the relevant information are
-		 *  available.
-		 *
-		 *  This object is taken from the _siPlanesParameters during the
-		 *  init() phase and stored for local use
-		 */
-		gear::SiPlanesLayerLayout * _siPlanesLayerLayout;
-
-		#if defined(USE_AIDA) || defined(MARLIN_USE_AIDA)
-		//! AIDA histogram map
-		/*! Instead of putting several pointers to AIDA histograms as
-		 *  class members, histograms are booked in the init() method and
-		 *  their pointers are inserted into this map keyed by their
-		 *  names.
-		 *  The histogram filling can proceed recalling an object through
-		 *  its name
-		 */
-		std::map<std::string, AIDA::IBaseHistogram * > _aidaHistoMap;
-		#endif
-
-		int _nPlanes;
-
-		std::vector<std::vector<double> > _xPos;
-		std::vector<std::vector<double> > _yPos;
-		std::vector<std::vector<double> > _zPos;
-
-		std::vector<double> _siPlaneZPosition;
-
-	};
+		    HitsInPlane ( )
+		    {
+			measuredX = 0.0;
+			measuredY = 0.0;
+			measuredZ = 0.0;
+		    }
+		    HitsInPlane ( double x, double y, double z )
+		    {
+			measuredX = x;
+			measuredY = y;
+			measuredZ = z;
+		    }
+		    bool operator < ( const HitsInPlane& b ) const
+		    {
+			return ( measuredZ < b.measuredZ );
+		    }
+		    double measuredX;
+		    double measuredY;
+		    double measuredZ;
+	    };
+
+	    virtual Processor * newProcessor ( )
+	    {
+		return new EUTelMilleGBL;
+	    }
+
+	    EUTelMilleGBL ();
+
+	    virtual void init ();
+
+	    virtual void processRunHeader ( LCRunHeader * run );
+
+	    virtual void processEvent ( LCEvent * evt );
+
+	    virtual void end ( );
+
+	    virtual bool isPointInPlane ( TVector3 point );
+
+	    void bookHistos ( );
+
+	    bool _BField;
+
+	protected:
+
+	    bool _alignmentloaded;
+	    bool _dthistos;
+	    bool _dutplanevectors;
+	    bool _pre_alignmentloaded;
+
+	    double _chi2ndfCut;
+	    double _coordinator_x;
+	    double _coordinator_y;
+	    double _coordinator_z;
+	    double _driCut;
+	    double _driCutREFx;
+	    double _driCutREFy;
+	    double _eBeam;
+	    double _probCut;
+	    double _resdutx;
+	    double _resduty;
+	    double _resrefx;
+	    double _resrefy;
+	    double _resx;
+	    double _resy;
+	    double _sixCut;
+	    double _slopecutDUTx;
+	    double _slopecutDUTy;
+	    double _triCut;
+	    double _triCutDUTx;
+	    double _triCutDUTy;
+
+	    double * _dut_align_x;
+	    double * _dut_align_y;
+	    double * _dut_align_z;
+	    double * _dut_align_a;
+	    double * _dut_align_b;
+	    double * _dut_align_c;
+	    double * _dut_align_x_error;
+	    double * _dut_align_y_error;
+	    double * _dut_align_z_error;
+	    double * _dut_align_a_error;
+	    double * _dut_align_b_error;
+	    double * _dut_align_c_error;
+	    double * _dut_pre_align_x;
+	    double * _dut_pre_align_y;
+	    double * _dut_pre_align_z;
+	    double * _dut_pre_align_a;
+	    double * _dut_pre_align_b;
+	    double * _dut_pre_align_c;
+
+	    int _alignMode;
+	    int _chi2ndfCutCount;
+	    int _cutfailsx;
+	    int _cutfailsy;
+	    int _doDUTAlignment;
+	    int _doPreAlignment;
+	    int _dutFitMethod;
+	    int _generatePedeSteerfile;
+	    int _manualDUTid;
+	    int _maxTrackCandidatesTotal;
+	    int _probCutCount;
+	    int _requireDUTHit;
+	    int _runPede;
+	    int _useREF;
+
+	    IntVec _FixParameter;
+
+	    std::string _alignmentConstantCollectionName;
+	    std::string _alignmentConstantLCIOFile;
+	    std::string _binaryFilename;
+	    std::string _coordinatorPreAlignmentCollectionName;
+	    std::string _outputHitCollectionName;
+	    std::string _outputTrackCollectionName;
+	    std::string _pedeSteerfileName;
+
+	    TVector3 _Xaxis;
+	    TVector3 _Yaxis;
+	    TVector3 _Zaxis;
+
+	    std::vector < int > _excludePlanes;
+	    std::vector < int > _excludePlanes_sensorIDs;
+	    std::vector < int > _FixedPlanes;
+	    std::vector < int > _FixedPlanes_sensorIDs;
+	    std::vector < int > _orderedSensorID;
+	    std::vector < int > _activeSensorID;
+	    std::vector < int > _activeSensorZ;
+
+	    std::vector < std::string > _alignmentCollectionName;
+	    std::vector < std::string > _hitCollectionName;
+	    std::vector < std::string > _pre_alignmentCollectionName;
+
+	private:
+
+	    int _iEvt;
+	    int _nExcludePlanes;
+	    int _nGoodMilleTracks;
+	    int _nMilleDataPoints;
+	    int _nMilleTracks;
+	    int _nPlanes;
+
+	    gear::SiPlanesParameters * _siPlanesParameters;
+	    gear::SiPlanesLayerLayout * _siPlanesLayerLayout;
+
+	    #if defined(USE_AIDA) || defined(MARLIN_USE_AIDA)
+	    std::map < std::string, AIDA::IBaseHistogram * > _aidaHistoMap;
+	    #endif
+
+	    TVector3 _dutplane;
+	    TVector3 _normalvector;
+
+	    std::vector < double > _siPlaneZPosition;
+	    std::vector < std::vector < double > > _xPos;
+	    std::vector < std::vector < double > > _yPos;
+	    std::vector < std::vector < double > > _zPos;
+
+	    void getAlignment ( LCEvent * event );
+	    void getPreAlignment ( LCEvent * event );
+	    void getCoordinatorAlignment ( LCEvent * event );
+	    void getDUTNormal ( );
+
+    };
 
 	//! A global instance of the processor
 	EUTelMilleGBL gEUTelMilleGBL;
