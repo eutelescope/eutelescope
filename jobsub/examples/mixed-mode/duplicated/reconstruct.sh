37f6f4f38a8b15f9a32495e6becba6da43617ae7
Jan Dreyling-Eschweiler <dreyling@naf-atlas11.desy.de>
2018-10-22 13:24:18 +0200

    mixed-mode example incl. reference FEI4 plane and testing EUTelGBL

---
+++ jobsub/examples/mixed-mode/duplicated/reconstruct.sh
@@ -0,0 +1,23 @@
+#jobsub -c config.cfg -csv runlist.csv -g noisypixelmasker 497
+jobsub -c config.cfg -csv runlist.csv -g clustering 497
+jobsub -c config.cfg -csv runlist.csv -g hitmaker 497
+#jobsub -c config.cfg -csv runlist.csv -g aligngbl1 497
+#jobsub -c config.cfg -csv runlist.csv -g aligngbl2 497
+jobsub -c config.cfg -csv runlist.csv -g aligngbl3 497
+jobsub -c config.cfg -csv runlist.csv -g gbl 497
+
+#jobsub -c config.cfg -csv runlist.csv -g noisypixelmasker 498
+jobsub -c config.cfg -csv runlist.csv -g clustering 498
+jobsub -c config.cfg -csv runlist.csv -g hitmaker 498
+#jobsub -c config.cfg -csv runlist.csv -g aligngbl1 498
+#jobsub -c config.cfg -csv runlist.csv -g aligngbl2 498
+jobsub -c config.cfg -csv runlist.csv -g aligngbl3 498
+jobsub -c config.cfg -csv runlist.csv -g gbl 498
+
+#jobsub -c config.cfg -csv runlist.csv -g noisypixelmasker 499
+jobsub -c config.cfg -csv runlist.csv -g clustering 499
+jobsub -c config.cfg -csv runlist.csv -g hitmaker 499
+#jobsub -c config.cfg -csv runlist.csv -g aligngbl1 499
+#jobsub -c config.cfg -csv runlist.csv -g aligngbl2 499
+jobsub -c config.cfg -csv runlist.csv -g aligngbl3 499
+jobsub -c config.cfg -csv runlist.csv -g gbl 499
