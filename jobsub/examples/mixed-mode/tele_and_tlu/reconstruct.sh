0afcc78554b5971316434c42a1b41122c6a66e57
Jan Dreyling-Eschweiler <jan.dreyling-eschweiler@desy.de>
2018-10-24 14:24:55 +0200

    tele_and_tlu examples

--- jobsub/examples/mixed-mode/tele_and_tlu/reconstruct.sh
+++ jobsub/examples/mixed-mode/tele_and_tlu/reconstruct.sh
@@ -1,10 +1,25 @@
-jobsub -c config.cfg -csv runlist.csv -g noisypixelmasker 497
-jobsub -c config.cfg -csv runlist.csv -g clustering 497
-jobsub -c config.cfg -csv runlist.csv -g hitmaker 497
-jobsub -c config.cfg -csv runlist.csv -g aligngbl1 497
-jobsub -c config.cfg -csv runlist.csv -g aligngbl2 497
-jobsub -c config.cfg -csv runlist.csv -g aligngbl3 497
-jobsub -c config.cfg -csv runlist.csv -g gbl 497
+#jobsub -c config.cfg -csv runlist.csv -g noisypixelmasker 501
+#jobsub -c config.cfg -csv runlist.csv -g clustering 501
+#jobsub -c config.cfg -csv runlist.csv -g hitmaker 501
+
+#jobsub -c config.cfg -csv runlist.csv -g noisypixelmasker 502
+jobsub -c config.cfg -csv runlist.csv -g clustering 502
+jobsub -c config.cfg -csv runlist.csv -g hitmaker 502
+
+#jobsub -c config.cfg -csv runlist.csv -g noisypixelmasker 503
+jobsub -c config.cfg -csv runlist.csv -g clustering 503
+jobsub -c config.cfg -csv runlist.csv -g hitmaker 503
+
+#jobsub -c config.cfg -csv runlist.csv -g noisypixelmasker 504
+jobsub -c config.cfg -csv runlist.csv -g clustering 504
+jobsub -c config.cfg -csv runlist.csv -g hitmaker 504
+
+exit 1
+
+jobsub -c config.cfg -csv runlist.csv -g aligngbl1 501
+jobsub -c config.cfg -csv runlist.csv -g aligngbl2 501
+jobsub -c config.cfg -csv runlist.csv -g aligngbl3 501
+jobsub -c config.cfg -csv runlist.csv -g gbl 501
 
 jobsub -c config.cfg -csv runlist.csv -g noisypixelmasker 498
 jobsub -c config.cfg -csv runlist.csv -g clustering 498
