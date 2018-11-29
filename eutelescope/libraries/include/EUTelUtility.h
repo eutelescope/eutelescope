29ceecb520ca0e9824ea9c7617a70b97c182102b
Cyril Becot <cyril.becot@cern.ch>
2018-11-29 10:44:29 +0100

    Actually - don't use int for alignMode

--- eutelescope/libraries/include/EUTelUtility.h
+++ eutelescope/libraries/include/EUTelUtility.h
@@ -39,7 +39,7 @@ namespace eutelescope {
 
   namespace Utility {
 
-	enum class alignMode : int { XYShiftsRotZ, XYShifts, XYShiftsAllRot, XYZShiftsRotZ, XYZShiftsRotXYZ};
+	enum class alignMode { XYShiftsRotZ, XYShifts, XYShiftsAllRot, XYZShiftsRotZ, XYZShiftsRotXYZ};
 
     const long double PI = 3.141592653589793238L;
 
