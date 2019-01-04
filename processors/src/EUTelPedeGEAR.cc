6c1814c2ec87c532e7f38b2a9a74f4a813754046
jha92 <jan-hendrik.arling@desy.de>
2019-01-04 13:48:42 +0100

    small (style) changes in EUTelPedeGEAR

--- processors/src/EUTelPedeGEAR.cc
+++ processors/src/EUTelPedeGEAR.cc
@@ -13,7 +13,6 @@
 #include "EUTelExceptions.h"
 #include "EUTelPStream.h"
 #include "EUTelRunHeaderImpl.h"
-//#include "EUTelCDashMeasurement.h"
 #include "EUTelGeometryTelescopeGeoDescription.h"
 #include "EUTelUtility.h"
 
@@ -39,7 +38,6 @@ using namespace eutelescope;
 
 EUTelPedeGEAR::EUTelPedeGEAR() : Processor("EUTelPedeGEAR") {
 
-  // modify processor description
   _description = "EUTelPedeGEAR calls PEDE to process a MILLE binary file and "
                  "create an updated GEAR file with the updated MILLEPEDE II "
                  "alignment constants.";
@@ -64,7 +62,7 @@ EUTelPedeGEAR::EUTelPedeGEAR() : Processor("EUTelPedeGEAR") {
 
   registerOptionalParameter("RotateOffsetVec",
                             "Apply the obtained rotation to the preexisting offset vector or not..",
-                            _rotateOldOffsetVec, true); //I can't align if this is set to false. Should we keep this option?
+                            _rotateOldOffsetVec, true); //FIXME: I can't align if this is set to false. Should we keep this option?
 
 
 }
@@ -100,41 +98,34 @@ void EUTelPedeGEAR::init() {
 void EUTelPedeGEAR::processRunHeader(LCRunHeader *rdr) {
   auto header = std::make_unique<EUTelRunHeaderImpl>(rdr);
   header->addProcessor(type());
-  // increment the run counter
+  //increment the run counter
   ++_iRun;
 }
 
 void EUTelPedeGEAR::processEvent(LCEvent * /*event*/) { /*NOP NOP NOP*/ }
 
 void EUTelPedeGEAR::end() {
-  // TODO:  check if steering file exists
+  
+  //check if steering file exists
   std::ifstream pedeSteerFile(_pedeSteerfileName.c_str());
-  if (pedeSteerFile.good()) {
+  if(pedeSteerFile.good()) {
     pedeSteerFile.close();
-    streamlog_out(MESSAGE2) << "Found pede steer file, continuing ..."
-                            << std::endl;
+    streamlog_out(MESSAGE2) << "Found pede steer file, continuing ..." << std::endl;
   } else {
     pedeSteerFile.close();
-    streamlog_out(ERROR5) << "Could not find pede steer file: "
-                          << _pedeSteerfileName << " EXITING!" << std::endl;
+    streamlog_out(ERROR5) << "Could not find pede steer file: " << _pedeSteerfileName << " EXITING!" << std::endl;
     return;
   }
 
   std::string command = "pede " + _pedeSteerfileName;
+  streamlog_out(MESSAGE5) << "Starting pede with " << _pedeSteerfileName.c_str() << std::endl;
 
-  streamlog_out(MESSAGE5) << "Starting pede...: " << command.c_str()
-                          << std::endl;
-
+  //run pede and create a streambuf that reads its stdout and stderr
+  redi::ipstream pede(command.c_str(), redi::pstreams::pstdout | redi::pstreams::pstderr);
   bool encounteredError = false;
 
-  // run pede and create a streambuf that reads its stdout and stderr
-  redi::ipstream pede(command.c_str(),
-                      redi::pstreams::pstdout | redi::pstreams::pstderr);
-
-  if (!pede.is_open()) {
-    streamlog_out(ERROR5)
-        << "Pede cannot be executed: command not found in the path"
-        << std::endl;
+  if(!pede.is_open()) {
+    streamlog_out(ERROR5) << "Pede cannot be executed: command not found in the path." << std::endl;
     encounteredError = true;
   } else {
     // output multiplexing: parse pede output in both stdout and stderr and echo
@@ -145,30 +136,30 @@ void EUTelPedeGEAR::end() {
     std::stringstream pedeerrors;
     bool finished[2] = {false, false};
 
-    while (!finished[0] || !finished[1]) {
-      if (!finished[0]) {
-        while ((n = pede.err().readsome(buf, sizeof(buf))) > 0) {
+    while(!finished[0] || !finished[1]) {
+      if(!finished[0]) {
+        while((n = pede.err().readsome(buf, sizeof(buf))) > 0) {
           streamlog_out(ERROR5).write(buf, n).flush();
           std::string error(buf, n);
           pedeerrors << error;
           encounteredError = true;
         }
-        if (pede.eof()) {
+        if(pede.eof()) {
           finished[0] = true;
-          if (!finished[1])
+          if(!finished[1])
             pede.clear();
         }
       }
 
-      if (!finished[1]) {
-        while ((n = pede.out().readsome(buf, sizeof(buf))) > 0) {
+      if(!finished[1]) {
+        while((n = pede.out().readsome(buf, sizeof(buf))) > 0) {
           streamlog_out(MESSAGE4).write(buf, n).flush();
           std::string output(buf, n);
           pedeoutput << output;
         }
-        if (pede.eof()) {
+        if(pede.eof()) {
           finished[1] = true;
-          if (!finished[0])
+          if(!finished[0])
             pede.clear();
         }
       }
@@ -177,40 +168,33 @@ void EUTelPedeGEAR::end() {
     // pede does not return exit codes on some errors (in V03-04-00)
     // check for some of those here by parsing the output
     const char *pch = strstr(pedeoutput.str().data(), "Too many rejects");
-    if (pch) {
-      streamlog_out(ERROR5)
-          << "Pede stopped due to the large number of rejects. " << std::endl;
+    if(pch) {
+      streamlog_out(ERROR5) << "Pede stopped due to the large number of rejects. " << std::endl;
       encounteredError = true;
     }
 
-    const char *pch0 =
-        strstr(pedeoutput.str().data(), "Sum(Chi^2)/Sum(Ndf) = ");
-    if (pch0 != nullptr) {
-      streamlog_out(DEBUG5)
-          << " Parsing pede output for final chi2/ndf result.. " << std::endl;
+    const char *pch0 = strstr(pedeoutput.str().data(), "Sum(Chi^2)/Sum(Ndf) = ");
+    if(pch0 != nullptr) {
+      streamlog_out(DEBUG5) << " Parsing pede output for final chi2/ndf result.. " << std::endl;
       // search for the equal sign after which the result for chi2/ndf is stated
       // within the next 80 chars
       //(with offset of 22 chars since pch points to beginning of "Sum(..."
       //string just found)
       const char *pch = static_cast<const char*>(memchr(pch0 + 22, '=', 180));
 
-      if (pch != nullptr) {
+      if(pch != nullptr) {
         char str[16];
         // now copy the numbers after the equal sign
         strncpy(str, pch + 1, 15);
         str[15] = '\0'; /* null character manually added */
-        // TODO: monitor the chi2/ndf in CDash when running tests
-        // CDashMeasurement meas_chi2ndf("chi2_ndf",atof(str));  cout <<
-        // meas_chi2ndf; // output only if DO_TESTING is set
-        streamlog_out(MESSAGE6) << "Final Sum(Chi^2)/Sum(Ndf) = " << str
-                                << std::endl;
+        streamlog_out(MESSAGE6) << "Final Sum(Chi^2)/Sum(Ndf) = " << str << std::endl;
       }
     }
 
     // wait for the pede execution to finish
     pede.close();
 
-    // check the exit value of pede / react to previous errors
+    //check the exit value of pede / react to previous errors
     if (pede.rdbuf()->status() == 0 && !encounteredError) {
       streamlog_out(MESSAGE7) << "Pede successfully finished" << std::endl;
     } else {
@@ -221,9 +205,7 @@ void EUTelPedeGEAR::end() {
       streamlog_out(ERROR5) << pedeerrors.str() << std::endl;
       // TODO: decide what to do now; exit? and if, how?
       streamlog_out(ERROR5) << "Will exit now" << std::endl;
-      // exit(EXIT_FAILURE); // FIXME: can lead to (ROOT?) seg faults - points
-      // to corrupt memory? run valgrind...
-      return; // does fine for now
+      return;
     }
 
     // reading back the millepede.res file and getting the results.
@@ -262,7 +244,7 @@ void EUTelPedeGEAR::end() {
           numpars = 6;
         }
         
-        int sensorID = 0; // should be done better
+        int sensorID = 0; //FIXME: should be done better
         double xOff = 0;
         double yOff = 0;
         double zOff = 0;
@@ -276,10 +258,10 @@ void EUTelPedeGEAR::end() {
         double betaErr = 0;
         double gammaErr = 0;
 
-        for (unsigned int iParam = 0; iParam < numpars; ++iParam) {
+        for(unsigned int iParam = 0; iParam < numpars; ++iParam) {
           std::getline(millepede, line);
 
-          if (line.empty()) {
+          if(line.empty()) {
             goodLine = false;
             continue;
           }
@@ -290,43 +272,93 @@ void EUTelPedeGEAR::end() {
 
           double buffer;
           // check that all parts of the line are non zero
-          while (tokenizer >> buffer) {
+          while(tokenizer >> buffer) {
             tokens.push_back(buffer);
           }
-          if ((tokens.size() == 3) || (tokens.size() == 6) || (tokens.size() == 5)) {
+
+          if((tokens.size() == 3) || (tokens.size() == 6) || (tokens.size() == 5)) {
             goodLine = true;
           } else {
             goodLine = false;
           }
 
-          if (_alignMode == Utility::alignMode::XYShifts) {
-            if (iParam == 0) {
-			  sensorID = (tokens[0] - 1) / 10; // should be done better
+	  //parameter 0
+	  if(iParam == 0) {
+	    sensorID = (tokens[0] - 1) / 10; //FIXME: should be done better                                                                                                                               
+	    xOff = tokens[1];
+	    if(tokens[2] == 0) xOffErr = tokens[4];
+	  }
+	  //parameter 1
+	  else if(iParam == 1) {
+	    yOff = tokens[1];
+	    if(tokens[2] == 0) yOffErr = tokens[4];
+	  }
+	  //parameter 2
+	  else if(iParam == 2) {
+	    if(_alignMode == Utility::alignMode::XYShiftsRotZ) {
+	      gamma = -tokens[1];
+	      if(tokens[2] == 0) gammaErr = tokens[4];
+	    }
+	    else if(_alignMode == Utility::alignMode::XYZShiftsRotZ) {
+	      gamma = -tokens[1];
+              if(tokens[2] == 0) gammaErr = tokens[4];
+	    }
+	    else if(_alignMode == Utility::alignMode::XYZShiftsRotXYZ) {
+	      zOff = tokens[1];
+              if(tokens[2] == 0) zOffErr = tokens[4];
+	    }
+	  }
+	  //parameter 3
+          else if(iParam == 3) {
+	    if(_alignMode == Utility::alignMode::XYZShiftsRotZ) {
+	      zOff = tokens[1];
+              if(tokens[2] == 0) zOffErr = tokens[4];
+	    }
+	    else if(_alignMode == Utility::alignMode::XYZShiftsRotXYZ) {
+	      alpha = -tokens[1];
+              if(tokens[2] == 0) alphaErr = tokens[4];
+	    }
+	  }
+	  //parameter 4 (only for XYZShiftsRotXYZ)
+	  else if(iParam == 4) {
+	    beta = -tokens[1];
+	    if(tokens[2] == 0) betaErr = tokens[4];
+	  }
+	  //parameter 5 (only for XYZShiftsRotXYZ)
+	  else if (iParam == 5) {
+              gamma = -tokens[1];
+              if(tokens[2] == 0) gammaErr = tokens[4];
+	  }
+      
+	  /* replaced by upper part - to be tested
+          if(_alignMode == Utility::alignMode::XYShifts) {
+            if(iParam == 0) {
+	      sensorID = (tokens[0] - 1) / 10; //FIXME: should be done better
               xOff = tokens[1];
               if(tokens[2] == 0) xOffErr = tokens[4];
             }
-            if (iParam == 1) {
+            if(iParam == 1) {
               yOff = tokens[1];
               if(tokens[2] == 0) yOffErr = tokens[4];
             }
           }
-          else if (_alignMode == Utility::alignMode::XYShiftsRotZ) {
-            if (iParam == 0) {
-			  sensorID = (tokens[0] - 1) / 10; // should be done better
+          else if(_alignMode == Utility::alignMode::XYShiftsRotZ) {
+            if(iParam == 0) {
+	      sensorID = (tokens[0] - 1) / 10; //FIXME: should be done better
               xOff = tokens[1];
               if(tokens[2] == 0) xOffErr = tokens[4];
             }
-            if (iParam == 1) {
+            if(iParam == 1) {
               yOff = tokens[1];
               if(tokens[2] == 0) yOffErr = tokens[4];
             }
-            if (iParam == 2) {
+            if(iParam == 2) {
               gamma = -tokens[1];
               if(tokens[2] == 0) gammaErr = tokens[4];
             }
-          } else if (_alignMode == Utility::alignMode::XYZShiftsRotZ){
-            if (iParam == 0) {
-			  sensorID = (tokens[0] - 1) / 10; // should be done better
+          } else if(_alignMode == Utility::alignMode::XYZShiftsRotZ){
+            if(iParam == 0) {
+	      sensorID = (tokens[0] - 1) / 10; // should be done better
               xOff = tokens[1];
               if(tokens[2] == 0) xOffErr = tokens[4];
             }
@@ -368,66 +400,63 @@ void EUTelPedeGEAR::end() {
               gamma = -tokens[1];
               if(tokens[2] == 0) gammaErr = tokens[4];
             }
-          }
+	  }
+	  */
         }
 
-        // right place to add the constant to the collection
-        // Errors added to the output. Format should be improved, though
-        if (goodLine) {
-          std::cout << "Alignment on sensor " << sensorID << " determined to be: " << std::endl
-                    << "xOff: " << xOff << " +- " << xOffErr << std::endl
-                    << "yOff: " << yOff << " +- " << yOffErr << std::endl
-                    << "zOff: " << zOff << " +- " << zOffErr << std::endl
-                    << "alpha: " << alpha << "+- " << alphaErr << std::endl
-                    << "beta: " << beta << " +- " << betaErr << std::endl
-                    << "gamma: " << gamma  << " +- " << gammaErr << std::endl;
+        // add the constant to the collection, errors added to the output
+        if(goodLine) {
+          streamlog_out(MESSAGE6) << "Alignment on sensor " << sensorID << " determined to be: " << std::endl
+				  << "xOff: "  << xOff  << " +- " << xOffErr  << std::endl
+				  << "yOff: "  << yOff  << " +- " << yOffErr  << std::endl
+				  << "zOff: "  << zOff  << " +- " << zOffErr  << std::endl
+				  << "alpha: " << alpha << " +- " << alphaErr << std::endl
+				  << "beta: "  << beta  << " +- " << betaErr  << std::endl
+				  << "gamma: " << gamma << " +- " << gammaErr << std::endl;
 
-          // The old rotation matrix is well defined by GEAR file
+          //get old rotation matrix from GEAR file
           Eigen::Matrix3d rotOld = geo::gGeometry().rotationMatrixFromAngles(sensorID);
-          // The new rotation matrix is obtained via the alpha, beta, gamma from
-          // MillepedeII
+          //get new rotation matrix via the alpha, beta, gamma from MillepedeII
           Eigen::Matrix3d rotAlign = Utility::rotationMatrixFromAngles(alpha, beta, gamma);
-          // The corrected rotation is given by: rotAlign*rotOld, from this
-          // rotation we can extract the
-          // updated alpha', beta' and gamma'
+          //get corrected rotation by multiplying rotAlign*rotOld and extract updated alpha', beta' and gamma'
           Eigen::Vector3d newCoeff = Utility::getRotationAnglesFromMatrix(rotAlign * rotOld);
 
-          // std::cout << "Old rotation matrix: " << rotOld << std::endl;
-          // std::cout << "Align rotation matrix: " << rotAlign << std::endl;
-          // std::cout << "Updated coefficients: " << newCoeff*57.29 <<
-          // std::endl;
-          std::cout << "This results in the updated rotations (alpha', beta', "
-                       "gamma'): "
+	  //output of results
+	  streamlog_out(DEBUG5) << "Old rotation matrix: " << rotOld << std::endl;
+	  streamlog_out(DEBUG5) << "Align rotation matrix: " << rotAlign << std::endl;
+	  streamlog_out(MESSAGE6) << "Updated rotations (alpha', beta', gamma'): "
                     << newCoeff[0] << ", " << newCoeff[1] << ", " << newCoeff[2]
                     << std::endl;
 
           Eigen::Vector3d oldOffset;
           oldOffset << geo::gGeometry().getPlaneXPosition(sensorID),
-              geo::gGeometry().getPlaneYPosition(sensorID),
-              geo::gGeometry().getPlaneZPosition(sensorID);
+	    geo::gGeometry().getPlaneYPosition(sensorID),
+	    geo::gGeometry().getPlaneZPosition(sensorID);
 		
-		// Edo: is this useful somehow or just a residual of old alignment collection?
-		if(_rotateOldOffsetVec) {
-			oldOffset = rotAlign*oldOffset;          
-		}
-          geo::gGeometry().alignGlobalPos(sensorID, oldOffset[0] - xOff,
+	  //FIXME: is this useful somehow or just a residual of old alignment collection?
+	  if(_rotateOldOffsetVec) {
+	    oldOffset = rotAlign*oldOffset;          
+	  }
+          
+	  //transfer alignment to geometry
+	  geo::gGeometry().alignGlobalPos(sensorID, 
+					  oldOffset[0] - xOff,
                                           oldOffset[1] - yOff,
                                           oldOffset[2] - zOff);
           geo::gGeometry().alignGlobalRot(sensorID, rotAlign * rotOld);
-
+	  
           counter++;
         }
       }
     }
     millepede.close();
   }
+
+  //create new GEAR file with new alignment constants
   marlin::StringParameters *MarlinStringParams = marlin::Global::parameters;
-  std::string outputFilename =
-      (MarlinStringParams->getStringVal("GearXMLFile"))
-          .substr(0,
-                  (MarlinStringParams->getStringVal("GearXMLFile")).size() - 4);
-  std::cout << "GEAR Filename: " << outputFilename + _GEARFileSuffix + ".xml"
-            << std::endl;
+  std::string gearFileName = MarlinStringParams->getStringVal("GearXMLFile");
+  std::string outputFilename = gearFileName.substr(0,gearFileName.size() - 4);
+  streamlog_out(MESSAGE4) << "GEAR Filename: " << outputFilename + _GEARFileSuffix + ".xml" << std::endl;
   geo::gGeometry().writeGEARFile(outputFilename + _GEARFileSuffix + ".xml");
   streamlog_out(MESSAGE2) << std::endl << "Successfully finished" << std::endl;
 }
