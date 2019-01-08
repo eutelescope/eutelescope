964bc8755a0ab0f11c181378e56dcdfb7eb21012
jha92 <jan-hendrik.arling@desy.de>
2019-01-08 08:00:22 +0100

    refactor EUTelPreAlignment to EUTelPreAligner
    For checking the history of the	renamed	files, use
    	git log -- processors/src/EUTelPreAlignment.cc
    	git log --follow processors/include/EUTelPreAligner.h

--- processors/include/EUTelPreAligner.h
+++ processors/include/EUTelPreAligner.h
@@ -6,11 +6,8 @@
  *   header with author names in all development based on this file.
  *
  */
-#ifndef EUTELPREALIGNMENT_H
-#define EUTELPREALIGNMENT_H
-
-// ROOT includes
-#include "TVector3.h"
+#ifndef EUTELPREALIGNER_H
+#define EUTELPREALIGNER_H
 
 // marlin includes ".h"
 #include "marlin/Processor.h"
@@ -37,7 +34,10 @@
 #include <vector>
 
 namespace eutelescope {
+
+  //class implementation of PreAligner
   class PreAligner {
+  
   private:
     float pitchX, pitchY;
     std::vector<int> histoX, histoY;
@@ -45,103 +45,125 @@ namespace eutelescope {
     float range;
     float zPos;
     int iden;
+    
+    //! function to get maximum bin of given histogram
     float getMaxBin(std::vector<int> &histo) {
       int maxBin(0), maxVal(0);
-      for (size_t ii = 0; ii < histo.size(); ii++) {
-        if (histo.at(ii) > maxVal) {
-          maxBin = ii;
-          maxVal = histo.at(ii);
-        }
+      //loop over bins
+      for(size_t ibin = 0; ibin < histo.size(); ibin++) {
+	if(histo.at(ibin) > maxVal) {
+	  maxBin = ibin;
+	  maxVal = histo.at(ibin);
+	}
       }
-      // Get weighted position from 3 neighboring bins
-      // as long as we are not on the edges of our histogram:
-      if (maxBin == 0 || maxBin == static_cast<int>(histo.size())) {
+      
+      //check if maxBin is not at the edges
+      if(maxBin == 0 || maxBin == static_cast<int>(histo.size())) {
         streamlog_out(WARNING3)
-            << "At least one sensor frame might be empty or heavily "
-               "misaligned. Please check the GEAR file!"
-            << " MaxBin: " << maxBin << " histo.size(): " << histo.size()
-            << std::endl;
+	  << "At least one sensor frame might be empty or heavily "
+	  "misaligned. Please check the GEAR file!"
+	  << " MaxBin: " << maxBin << " histo.size(): " << histo.size()
+	  << std::endl;
         return static_cast<float>(maxBin);
       }
+      	
+      //get weighted position from three neighboring bins:
       float weight(0.0);
       double pos1(0.0);
       double pos2(0.0);
       double pos3(0.0);
-
       try {
-        // use logarithms to be on the safe side even with large number of
-        // bin entries
-        pos1 = log(maxBin - 1) + log(histo.at(maxBin - 1));
-        pos2 = log(maxBin) + log(histo.at(maxBin));
-        pos3 = log(maxBin + 1) + log(histo.at(maxBin + 1));
-        weight = log((histo.at(maxBin - 1)) + (histo.at(maxBin)) +
+	//use logarithms to be safe even with large number of bin entries
+	pos1 = log(maxBin - 1) + log(histo.at(maxBin - 1));
+	pos2 = log(maxBin) + log(histo.at(maxBin));
+	pos3 = log(maxBin + 1) + log(histo.at(maxBin + 1));
+	weight = log((histo.at(maxBin - 1)) + (histo.at(maxBin)) +
                      (histo.at(maxBin + 1)));
-      } catch (...) {
-        streamlog_out(ERROR)
-            << "Could not execute prealignment bin content retrieval. The "
-               "sensor frame might be empty or heavily misaligned. Please "
-               "check the GEAR file!"
-            << std::endl;
+      } catch(...) {
+	streamlog_out(ERROR) 
+	  << "Could not execute prealignment bin content retrieval. The "
+	  "sensor frame might be empty or heavily misaligned. Please "
+	  "check the GEAR file!"
+	  << std::endl;
       }
-      return (exp(pos1 - weight) + exp(pos2 - weight) + exp(pos3 - weight));
+      return (exp(pos1-weight) + exp(pos2-weight) + exp(pos3-weight));
     }
-
+    
   public:
-    PreAligner(float pitchX, float pitchY, float zPos, int iden)
-        : pitchX(pitchX), pitchY(pitchY), minX(-40.0), maxX(40),
-          range(maxX - minX), zPos(zPos), iden(iden) {
+  PreAligner(float pitchX, float pitchY, float zPos, int iden)
+    : pitchX(pitchX), pitchY(pitchY), minX(-40.0), maxX(40),
+      range(maxX - minX), zPos(zPos), iden(iden) {
+      
       histoX.assign(int(range / pitchX), 0);
       histoY.assign(int(range / pitchY), 0);
     }
+    
     void *current() { return this; }
-    float getZPos() const { return (zPos); }
-    int getIden() const { return (iden); }
-    void addPoint(float x, float y) {
-      // Add to histo if within bounds, throw away data that is out of bounds
-      try {
-        histoX.at(static_cast<int>((x - minX) / pitchX)) += 1;
-      } catch (std::out_of_range &e) {
-        ;
-      }
-      try {
-        histoY.at(static_cast<int>((y - minX) / pitchY)) += 1;
-      } catch (std::out_of_range &e) {
-        ;
-      }
+    
+    float getZPos() const { 
+      return (zPos); 
     }
-    float getPeakX() { return ((getMaxBin(histoX) * pitchX) + minX); }
-    float getPeakY() { return ((getMaxBin(histoY) * pitchY) + minX); }
-
-  }; // class PreAligner
-
-  class EUTelPreAlign : public marlin::Processor {
-
+    
+    int getIden() const { 
+      return (iden); 
+    }
+    
+    //add point if within bounds, throw away data that is out of bounds
+    void addPoint(float x, float y) {
+      try {
+	histoX.at(static_cast<int>((x - minX) / pitchX)) += 1;
+      } catch(std::out_of_range &e) {;}
+      try {
+	histoY.at(static_cast<int>((y - minX) / pitchY)) += 1;
+      } catch(std::out_of_range &e) {;}
+    }	
+    
+    float getPeakX() { 
+      return ((getMaxBin(histoX) * pitchX) + minX); 
+    }
+    
+    float getPeakY() { 
+      return ((getMaxBin(histoY) * pitchY) + minX); 
+    }
+  };
+  
+  
+  class EUTelPreAligner : public marlin::Processor {
+    
   public:
-    virtual Processor *newProcessor() { return new EUTelPreAlign; }
-    //! Default constructor
-    EUTelPreAlign();
+    //! Returns a new instance of EUTelPreAligner
+    virtual Processor *newProcessor() { 
+      return new EUTelPreAligner; 
+    }
+    
+    //! default constructor
+    EUTelPreAligner();
+    
     virtual void init();
+    
     virtual void processRunHeader(LCRunHeader *run);
+    
     virtual void processEvent(LCEvent *evt);
+    
     virtual void end();
-
+    
+    //! Histogram booking
+    void bookHistos();
+    
   private:
-
-    //! How many events are needed to get reasonable correlation plots
-    /*! (and Offset DB values)
-     *
-     */
-    int _events;
-
+    //! How many events are needed to get reasonable plots (correlation & offset)
+    int _requiredEvents;
+    
     //! Sensor ID vector
     std::vector<int> _sensorIDVec;
-
+    
     //! map for sensor ID to position along Z id
-    /*!
-     */
     std::map<int, int> _sensorIDtoZOrderMap;
+    
+    //! fixed plane for prealignment
+    int _fixedID;
 
-    // _residual cuts [relative to the first upstream plane!]
+    //! Residual cuts [relative to the first upstream plane!]
     //! vector of correlation band cuts in X (upper limit)
     std::vector<float> _residualsXMax;
     //! vector of correlation band cuts in X (lower limit)
@@ -151,10 +173,11 @@ namespace eutelescope {
     //! vector of correlation band cuts in Y (lower limit)
     std::vector<float> _residualsYMin;
 
+    //! Minimal number of correlated hits
     int _minNumberOfCorrelatedHits;
 
     //! Boolean for turning histogram creation on and off
-    bool _fillHistos;
+    bool _histogramSwitch;
 
 #if defined(USE_AIDA) || defined(MARLIN_USE_AIDA)
     std::map<unsigned int, AIDA::IBaseHistogram *> _hitXCorr;
@@ -162,23 +185,19 @@ namespace eutelescope {
 #endif
 
   protected:
+    int _iRun;
+    int _iEvt;
     std::string _inputHitCollectionName;
     std::string _alignmentConstantLCIOFile;
     std::string _GEARFileSuffix;
     bool _dumpGEAR;
-
-    int _iRun;
-    int _iEvt;
-    int _fixedID;
-    float _fixedZ;
-    gear::SiPlanesParameters *_siPlanesParameters;
-    gear::SiPlanesLayerLayout *_siPlanesLayerLayout;
     std::vector<PreAligner> _preAligners;
     std::vector<int> _ExcludedPlanesXCoord;
     std::vector<int> _ExcludedPlanesYCoord;
     std::vector<int> _ExcludedPlanes;
   };
+  
   //! A global instance of the processor
-  EUTelPreAlign gEUTelPreAlign;
+  EUTelPreAligner gEUTelPreAligner;
 }
 #endif
